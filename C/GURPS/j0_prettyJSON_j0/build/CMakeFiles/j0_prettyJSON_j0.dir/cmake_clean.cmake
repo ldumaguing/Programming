ab8549239bfb055f7cc022cff73a94aefd6f68be
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-01-10 14:41:09 -0500

    bla

---
+++ C/GURPS/j0_prettyJSON_j0/build/CMakeFiles/j0_prettyJSON_j0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/j0_prettyJSON_j0.dir/main.c.o"
+  "j0_prettyJSON_j0"
+  "j0_prettyJSON_j0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/j0_prettyJSON_j0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
