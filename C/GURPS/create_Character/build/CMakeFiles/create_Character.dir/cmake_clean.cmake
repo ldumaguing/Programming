1680a41e23834f3705ea43ee750739d098d1bd7e
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-01-09 21:07:52 -0500

    bla

---
+++ C/GURPS/create_Character/build/CMakeFiles/create_Character.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create_Character.dir/main.c.o"
+  "create_Character"
+  "create_Character.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/create_Character.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/GURPS/create_Character/build/CMakeFiles/myne.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myne.dir/Myne/Human.c.o"
+  "CMakeFiles/myne.dir/Myne/arguments.c.o"
+  "CMakeFiles/myne.dir/Myne/secondaries.c.o"
+  "libmyne.a"
+  "libmyne.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
