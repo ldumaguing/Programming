b336c3baa3ad49086c0482b53f11824eb12add53
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-01-14 23:50:13 -0500

    2021.01a

---
+++ C/GURPS/s0_generateDamage_s1/Myne/myne.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int roll_3d6();
+void generateDamage(char *);
+int roll_d6_X_times(int);
---
+++ C/GURPS/s0_generateDamage_s1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Myne/myne.h"
+
+void define_DamageString(char *);
+
+// *******************************************************************************
+int main(int argc, char *argv[]) {
+   char DamageString[200];
+   define_DamageString(DamageString);
+
+   generateDamage(DamageString);
+
+   return 0;
+}
+
+// *******************************************************************************
+void define_DamageString(char *DmgStr) {
+   int counter = 0;
+   int c;
+   c = getchar();
+   while (c != EOF) {
+      DmgStr[counter] = c;
+      c = getchar();
+      counter++;
+   }
+   DmgStr[--counter] = '\0';
+}
+
