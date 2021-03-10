17e68abc65d064c7f7fd7c975d4349a769df9928
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-03-10 17:35:29 -0500

    2021.03a

--- C/GURPS/generateDamage/Myne/options.c
+++ C/GURPS/generateDamage/Myne/options.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "myne.h"
 
 void knockback(char *, char *, int, int);
 
@@ -20,4 +21,10 @@ void knockback(char *opts, char *dmgType, int dmg, int rolled) {
    if ((strcmp(dmgType, "cut") == 0) & (dmg > 0)) return;
    puts("knockback");
 
+   char buff[80];
+   char buff1[80];
+   getGrep(buff, "ST:[0-9]*", opts);
+   getGrep(buff1, "[0-9].*", buff);
+   int X = atoi(buff1);
+   X = X - 2;
 }
