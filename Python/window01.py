e8677410d58ccb98fe2280caae0264b073c20ff4
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-03-15 17:42:51 -0400

    2021.03b

--- Python/window01.py
+++ Python/window01.py
@@ -5,22 +5,31 @@ from subprocess import Popen
 p = Popen('hexagon getpoints 50.0 0.0 0.0', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 output, errors = p.communicate()
 X = output.split('\n')
-p1 = (int(double(X[0])), int(double(X[1])))
+p1 = (int(float(X[0])), int(float(X[1])))
 p2 = (int(float(X[2])), int(float(X[3])))
 p3 = (int(float(X[4])), int(float(X[5])))
 p4 = (int(float(X[6])), int(float(X[7])))
 p5 = (int(float(X[8])), int(float(X[9])))
 p6 = (int(float(X[10])), int(float(X[11])))
 p7 = (int(float(X[12])), int(float(X[13])))
-
-
+w = p7[0]
+h = p1[1]
 
 pygame.init()
 
 window = pygame.display.set_mode((500, 400))
 print(p7)
 while True:
-   pygame.draw.lines(window, (255,255,255), False, (p1, p2, p3, p4, p5, p6), 1)
-   pygame.draw.line(window, (255,255,255), p5, p7, 1)
+   for x in range(5):
+      p1a = (p1[0] + (x * w), p1[1])
+      p2a = (p2[0] + (x * w), p2[1])
+      p3a = (p3[0] + (x * w), p3[1])
+      p4a = (p4[0] + (x * w), p4[1])
+      p5a = (p5[0] + (x * w), p5[1])
+      p6a = (p6[0] + (x * w), p6[1])
+      p7a = (p7[0] + (x * w), p7[1])
+      pygame.draw.lines(window, (255,255,255), False, (p1a, p2a, p3a, p4a, p5a, p6a), 1)
+      pygame.draw.line(window, (255,255,255), p5a, p7a, 1)
+
    pygame.display.update()
 
