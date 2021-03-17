fe02d7bb92c7dd9301e2d2a9eca507dadd0a04a4
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-03-17 07:34:36 -0400

    2021.03b

--- Python/xxx.py
+++ Python/xxx.py
@@ -1,14 +1,15 @@
 import math
 import pygame
+from pygame.locals import *
 import subprocess
 from subprocess import Popen
 
 scl = '100.0'
 scale = int(float(scl))
-Xpx = 800
-Ypx = 600
+Xpx = 1366
+Ypx = 768
 
-p = Popen('hexagonCol getpoints ' + scl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+p = Popen('hexagonRow getpoints ' + scl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 output, errors = p.communicate()
 X = output.split('\n')
 p1 = (int(float(X[0])), int(float(X[1])))
@@ -18,12 +19,18 @@ p4 = (int(float(X[6])), int(float(X[7])))
 p5 = (int(float(X[8])), int(float(X[9])))
 p6 = (int(float(X[10])), int(float(X[11])))
 p7 = (int(float(X[12])), int(float(X[13])))
-w = p7[0]
-h = p1[1]
+w = p1[0]
+h = p7[1]
 
 pygame.init()
 
-window = pygame.display.set_mode((Xpx, Ypx))
+starship = pygame.image.load('fedCA2.png')
+
+
+
+
+
+window = pygame.display.set_mode((Xpx, Ypx), 0, 32)
 print(p7)
 countX = math.ceil(Xpx / w)
 countY = math.ceil(Ypx / h)
@@ -39,6 +46,9 @@ for x in range(countX):
       p7a = (p7[0] + (x * w), p7[1] + (y * h))
       pygame.draw.lines(window, (255,255,255), False, (p1a, p2a, p3a, p4a, p5a, p6a), 1)
       pygame.draw.line(window, (255,255,255), p5a, p7a, 1)
+
+window.blit(starship, (0, 0))
+
 
 while True:
    pygame.display.update()
