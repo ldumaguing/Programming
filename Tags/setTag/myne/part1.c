570668b34bf0a2a59a14f2cfcfa34ec5def9b348
Ayeka Jurai <ayeka@bsd.free.dom>
2021-06-19 22:29:02 -0400

    2021.06b

--- Tags/setTag/myne/part1.c
+++ Tags/setTag/myne/part1.c
@@ -4,6 +4,7 @@
 #include "myne.h"
 
 int hasTag ( struct json_object *, char * );
+void plus_minus ( struct json_object *, int, char *, char * );
 
 // *******************************************************************************
 void set ( char *argv[] ) {
@@ -17,7 +18,6 @@ void set ( char *argv[] ) {
         if ( i >= 0 ) {
             char buff[80];
             sprintf ( buff, "%s", argv[2] );
-
             char *key, *value;
             char *buff_p = buff;
             key = strtok_r ( buff_p, ":", &buff_p );
@@ -25,16 +25,39 @@ void set ( char *argv[] ) {
             if ( value == NULL ) return;
 
             if ( hasPlusMinus ( value ) ) {
-                printf ( "hasPlusMinus\n" );
+                plus_minus ( Tags, i, key, value );
+                saveObj ( theObj, argv );
                 return;
             }
 
+            // ********** Replace value
             sprintf ( buff, "%s:%s", key, value );
             json_object_array_put_idx ( Tags, i, json_object_new_string ( buff ) );
 
             saveObj ( theObj, argv );
         }
     }
+}
+
+// *******************************************************************************
+void plus_minus ( struct json_object *Tags, int i, char *key, char *value ) {
+    // ********** increment/decrement value
+    struct json_object *aTag;
+    aTag = json_object_array_get_idx ( Tags, i );
+
+    char buff[80];
+    sprintf ( buff, "%s", json_object_get_string ( aTag ) );
+    char *K, *V;
+    char *buff_p = buff;
+    K = strtok_r ( buff_p, ":", &buff_p );
+    V = strtok_r ( buff_p, ":", &buff_p );
+
+    int v = atoi ( V );
+    int value_i = atoi ( value );
+    int x = v + value_i;
+
+    sprintf ( buff, "%s:%d", key, x );
+    json_object_array_put_idx ( Tags, i, json_object_new_string ( buff ) );
 }
 
 // *******************************************************************************
