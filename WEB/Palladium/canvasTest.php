fca574dbc4e0a315cd958ca6925946daf11dc86d
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-07-08 21:05:34 -0400

    2021.07a

--- WEB/Palladium/canvasTest.php
+++ WEB/Palladium/canvasTest.php
@@ -5,7 +5,11 @@
 </head>
 <body onload="pageLoaded();">
    <canvas width="1600" height="900" id="canvas01" style="border: 1px solid black;">
-      <img src="images/Bank.png" id="u0001">
+      <img src="images/Bank.png" id="0001">
+      <img src="images/Jimmy.png" id="0002">
+      <img src="images/Nick.png" id="0003">
+      <img src="images/Z1.png" id="0004">
+      <img src="images/Z2.png" id="0005">
    </canvas>
 </body>
 </html>
@@ -15,8 +19,10 @@ function pageLoaded() {
    var canvas01 = document.getElementById("canvas01");
    var context = canvas01.getContext("2d");
 
-   var u0001 = document.getElementById("u0001");
-   context.drawImage(u0001, <?php echo "30, 30";  ?>);
+   var X = document.getElementById("0001");
+   context.drawImage(X, <?php echo "30, 30"; ?>);
+   X = document.getElementById("0002");
+   context.drawImage(X, <?php echo "160, 160"; ?>);
 }
 </script>
 
