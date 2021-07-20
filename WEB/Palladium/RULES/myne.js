c366f8b64841156278662a7e82035686f5b27261
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-07-20 00:12:18 -0400

    2021.07b

---
+++ WEB/Palladium/RULES/myne.js
@@ -0,0 +1,28 @@
+function assess() {
+   document.getElementById("IQ_7or8").style.display = "none";
+   document.getElementById("IQ_5or6").style.display = "none";
+   document.getElementById("IQ_3or4").style.display = "none";
+   document.getElementById("IQ_1or2").style.display = "none";
+   document.getElementById("MA_1or7").style.display = "none";
+   document.getElementById("MA_5or7").style.display = "none";
+   document.getElementById("MA_3or4").style.display = "none";
+
+   var X = parseInt(document.getElementById("IQ").value);
+   
+   if ((X == 7) | (X == 8)) document.getElementById("IQ_7or8").style.display = "";
+   if ((X == 5) | (X == 6)) document.getElementById("IQ_5or6").style.display = "";
+   if ((X == 3) | (X == 4)) document.getElementById("IQ_3or4").style.display = "";
+   if ((X == 1) | (X == 2)) document.getElementById("IQ_1or2").style.display = "";
+   
+   X = parseInt(document.getElementById("MA").value);
+   
+   if ((X >= 1) & (X <= 7)) document.getElementById("MA_1or7").style.display = "";
+   if ((X >= 5) & (X <= 7)) document.getElementById("MA_5or7").style.display = "";
+   if ((X == 3) | (X == 4)) document.getElementById("MA_3or4").style.display = "";
+   
+   
+   
+   
+   // document.getElementById("placeholder").innerHTML = X;
+}
+	
