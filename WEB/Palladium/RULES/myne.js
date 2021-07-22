96606b96d7e95421a0ea40e7f611651d370705fc
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-07-22 17:39:36 -0400

    2021.07b

--- WEB/Palladium/RULES/myne.js
+++ WEB/Palladium/RULES/myne.js
@@ -27,6 +27,7 @@ function assess() {
    document.getElementById("PE_1or7").style.display = "none";
    document.getElementById("PE_5or7").style.display = "none";
    document.getElementById("PE_3or4").style.display = "none";
+   document.getElementById("PE_1or2").style.display = "";
 
    var X = parseInt(document.getElementById("IQ").value);
    if ((X == 7) | (X == 8)) document.getElementById("IQ_7or8").style.display = "";
@@ -62,7 +63,7 @@ function assess() {
    if ((X >= 1) & (X <= 7)) document.getElementById("PE_1or7").style.display = "";
    if ((X >= 5) & (X <= 7)) document.getElementById("PE_5or7").style.display = "";
    if ((X == 3) | (X == 4)) document.getElementById("PE_3or4").style.display = "";
-   
+   if ((X == 1) | (X == 2)) document.getElementById("PE_1or2").style.display = "";
    
    
    
