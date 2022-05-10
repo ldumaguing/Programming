294a1c1189dea3c59bbdb45b23322a679d4c05b3
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-05-10 17:53:47 -0400

    Add files via upload

--- GURPS/Weapon.sql
+++ GURPS/Weapon.sql
@@ -1,8 +1,9 @@
-delete from Objects where id = 2000;
+delete from Objects where id = 10000;
 insert into Objects (id, className, classDef) values (
-2000,
+10000,
 'Weapon',
-'{}'
+JSON_OBJECT(),
+JSON_OBJECT()
 );
 
 
