bb89ce296a2c8229c6a49845e94c2f2eb89efeed
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-06-07 15:30:59 -0400

    Add files via upload

--- GURPS/Weapon4.sql
+++ GURPS/Weapon4.sql
@@ -859,38 +859,583 @@ update Objects set Definition =
 }}}}'
 where Id = 10099;
 
+-- ********************************************************************* Rifle-Musket, .577
+insert into Objects (Id, ClassType, Name, Definition) values (
+10100,
+'Weapon',
+'Rifle-Musket, .577',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 5,
+"Cost": 150,
+"Weight": "8.5/0.05"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "4d pi+",
+"Acc": 4,
+"Range": "700/2100",
+"RoF": 1,
+"Shots": "1(15)",
+"ST": "10†",
+"Bulk": -6,
+"Recoil": 3,
+"Notes": null
+}}}}'
+where Id = 10100;
 
+-- ********************************************************************* Cartridge Rifle, .45
+insert into Objects (Id, ClassType, Name, Definition) values (
+10101,
+'Weapon',
+'Cartridge Rifle, .45',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 5,
+"Cost": 200,
+"Weight": "6/0.1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "5d pi+",
+"Acc": 3,
+"Range": "600/2000",
+"RoF": 1,
+"Shots": "1(4)",
+"ST": "10†",
+"Bulk": -6,
+"Recoil": 3,
+"Notes": null
+}}}}'
+where Id = 10101;
 
+-- ********************************************************************* Lever-Action Carbine, .30
+insert into Objects (Id, ClassType, Name, Definition) values (
+10102,
+'Weapon',
+'Lever-Action Carbine, .30',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 5,
+"Cost": 300,
+"Weight": "7/0.3"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "5d pi",
+"Acc": 4,
+"Range": "450/3000",
+"RoF": 1,
+"Shots": "6+1(3i)",
+"ST": "10†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": null
+}}}}'
+where Id = 10102;
 
+-- ********************************************************************* Bolt-Action Rifle, 7.62mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10103,
+'Weapon',
+'Bolt-Action Rifle, 7.62mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 6,
+"Cost": 350,
+"Weight": "8.9/0.3"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "7d pi",
+"Acc": 5,
+"Range": "1000/4200",
+"RoF": 1,
+"Shots": "5+1(3)",
+"ST": "10†",
+"Bulk": -5,
+"Recoil": 4,
+"Notes": null
+}}}}'
+where Id = 10103;
 
+-- ********************************************************************* Self-Loading Rifle, 7.62mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10104,
+'Weapon',
+'Self-Loading Rifle, 7.62mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 6,
+"Cost": 600,
+"Weight": "10/0.5"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "7d pi",
+"Acc": 5,
+"Range": "1000/4200",
+"RoF": 3,
+"Shots": "8(3)",
+"ST": "10†",
+"Bulk": -5,
+"Recoil": 3,
+"Notes": null
+}}}}'
+where Id = 10104;
 
+-- ********************************************************************* Assault Rifle, 5.56mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10105,
+'Weapon',
+'Assault Rifle, 5.56mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 800,
+"Weight": "9/1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "5d pi",
+"Acc": 5,
+"Range": "500/3500",
+"RoF": 12,
+"Shots": "30+1(3)",
+"ST": "9†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": "1"
+}}}}'
+where Id = 10105;
 
+-- ********************************************************************* Assault Rifle, 7.62mmS
+insert into Objects (Id, ClassType, Name, Definition) values (
+10106,
+'Weapon',
+'Assault Rifle, 7.62mmS',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 300,
+"Weight": "10.5/1.8"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "5d+1 pi",
+"Acc": 4,
+"Range": "400/3000",
+"RoF": 10,
+"Shots": "30+1(3)",
+"ST": "10†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": "1"
+}}}}'
+where Id = 10106;
 
+-- ********************************************************************* Battle Rifle, 7.62mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10107,
+'Weapon',
+'Battle Rifle, 7.62mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 900,
+"Weight": "11/1.7"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "7d pi",
+"Acc": 5,
+"Range": "1000/4200",
+"RoF": 11,
+"Shots": "20+1(3)",
+"ST": "11†",
+"Bulk": -5,
+"Recoil": 3,
+"Notes": "1"
+}}}}'
+where Id = 10107;
 
+-- ********************************************************************* Assault Carbine, 5.56mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10108,
+'Weapon',
+'Assault Carbine, 5.56mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 8,
+"Cost": 900,
+"Weight": "7.3/1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "4d+2 pi",
+"Acc": 4,
+"Range": "400/3000",
+"RoF": 15,
+"Shots": "30+1(3)",
+"ST": "9†",
+"Bulk": -3,
+"Recoil": 2,
+"Notes": "1"
+}}}}'
+where Id = 10108;
 
+-- ********************************************************************* Dart Rifle, 11mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10109,
+'Weapon',
+'Dart Rifle, 11mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 8,
+"Cost": 1200,
+"Weight": "6.6/0.02"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "1d(0.2) pi-",
+"Acc": "5+1",
+"Range": "45/145",
+"RoF": 1,
+"Shots": "1(3)",
+"ST": "9†",
+"Bulk": -5,
+"Recoil": 2,
+"Notes": "2"
+}}}}'
+where Id = 10109;
 
+-- ********************************************************************* Sniper Rifle, .338
+insert into Objects (Id, ClassType, Name, Definition) values (
+10110,
+'Weapon',
+'Sniper Rifle, .338',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 8,
+"Cost": 5600,
+"Weight": "17.5/0.8"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "9d+1 pi",
+"Acc": "6+3",
+"Range": "1500/5500",
+"RoF": 1,
+"Shots": "4+1(3)",
+"ST": "11B†",
+"Bulk": -6,
+"Recoil": 4,
+"Notes": null
+}}}}'
+where Id = 10110;
 
+-- ********************************************************************* ICW, 6.8mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10111,
+'Weapon',
+'ICW, 6.8mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 9,
+"Cost": 7000,
+"Weight": "12/1.5"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "6d pi",
+"Acc": "6+3",
+"Range": "700/4000",
+"RoF": 15,
+"Shots": "25+1(3)",
+"ST": "10†",
+"Bulk": -5,
+"Recoil": 2,
+"Notes": "3,4"
+}}}}'
+where Id = 10111;
 
+-- ********************************************************************* Gauss Rifle, 4mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10112,
+'Weapon',
+'Gauss Rifle, 4mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 10,
+"Cost": 7100,
+"Weight": "8.5/1.4"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 43,
+"Damage": "6d+2(3) pi-",
+"Acc": "7+2",
+"Range": "1200/4800",
+"RoF": 12,
+"Shots": "60(3)",
+"ST": "10†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": "3"
+}}}}'
+where Id = 10112;
 
+-- ********************************************************************* Blunderbuss, 8G
+insert into Objects (Id, ClassType, Name, Definition) values (
+10113,
+'Weapon',
+'Blunderbuss, 8G',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 4,
+"Cost": 150,
+"Weight": "12/0.13"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 44,
+"Damage": "1d pi",
+"Acc": 1,
+"Range": "15/100",
+"RoF": "1x9",
+"Shots": "1(15)",
+"ST": "11†",
+"Bulk": -5,
+"Recoil": 1,
+"Notes": null
+}}}}'
+where Id = 10113;
 
+-- ********************************************************************* Double Shotgun, 10G
+insert into Objects (Id, ClassType, Name, Definition) values (
+10114,
+'Weapon',
+'Double Shotgun, 10G',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 5,
+"Cost": 450,
+"Weight": "10/0.1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 44,
+"Damage": "1d+2 pi",
+"Acc": 3,
+"Range": "50/125",
+"RoF": "2x9",
+"Shots": "2(3i)",
+"ST": "11†",
+"Bulk": -5,
+"Recoil": 1,
+"Notes": null
+}}}}'
+where Id = 10114;
 
+-- ********************************************************************* Pump Shotgun, 12G
+insert into Objects (Id, ClassType, Name, Definition) values (
+10115,
+'Weapon',
+'Pump Shotgun, 12G',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 6,
+"Cost": 240,
+"Weight": "8/0.7"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 44,
+"Damage": "1d+1 pi",
+"Acc": 3,
+"Range": "50/125",
+"RoF": "2x9",
+"Shots": "5(3i)",
+"ST": "10†",
+"Bulk": -5,
+"Recoil": 1,
+"Notes": null
+}}}}'
+where Id = 10115;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- ********************************************************************* Auto Shotgun, 12G
+insert into Objects (Id, ClassType, Name, Definition) values (
+10116,
+'Weapon',
+'Auto Shotgun, 12G',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 950,
+"Weight": "8.4/0.85"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 44,
+"Damage": "1d+1 pi",
+"Acc": 3,
+"Range": "50/125",
+"RoF": "3x9",
+"Shots": "6+1(3i)",
+"ST": "10†",
+"Bulk": -5,
+"Recoil": 1,
+"Notes": null
+}}}}'
+where Id = 10116;
 
 
 
