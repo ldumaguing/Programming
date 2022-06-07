d8210a72465cd88fec5b40976a41644718dfee09
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-06-07 12:46:41 -0400

    Add files via upload

--- GURPS/Weapon4.sql
+++ GURPS/Weapon4.sql
@@ -543,7 +543,7 @@ update Objects set Definition =
 "required skill group": 40,
 "Damage": "6d pi++",
 "Acc": 1,
-"Range": "1900",
+"Range": 1900,
 "RoF": 3,
 "Shots": "4(3i)",
 "ST": 9,
@@ -552,6 +552,351 @@ update Objects set Definition =
 "Notes": "1,2"
 }}}}'
 where Id = 10090;
+
+-- ********************************************************************* SMG, .45
+insert into Objects (Id, ClassType, Name, Definition) values (
+10091,
+'Weapon',
+'SMG, .45',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 6,
+"Cost": 2200,
+"Weight": "15.7/4.9"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 41,
+"Damage": "2d+1 pi+",
+"Acc": 3,
+"Range": "190/1750",
+"RoF": 13,
+"Shots": "50+1(5)",
+"ST": "11†",
+"Bulk": -4,
+"Recoil": 3,
+"Notes": "3"
+}}}}'
+where Id = 10091;
+
+-- ********************************************************************* SMG, 9mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10092,
+'Weapon',
+'SMG, 9mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 6,
+"Cost": 700,
+"Weight": "10.5/1.5"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 41,
+"Damage": "3d-1 pi",
+"Acc": 3,
+"Range": "160/1900",
+"RoF": "8!",
+"Shots": "32(3)",
+"ST": "10†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": "3"
+}}}}'
+where Id = 10092;
+
+-- ********************************************************************* Machine Pistol, 9mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10093,
+'Weapon',
+'Machine Pistol, 9mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 900,
+"Weight": "5.5/1.1"
+},
+"handling": 
+{
+"trigger": 
+{
+"one": 
+{
+"required skill group": 41,
+"Damage": "2d+2 pi",
+"Acc": 2,
+"Range": "160/1900",
+"RoF": 20,
+"Shots": "25+1(3)",
+"ST": 12,
+"Bulk": -3,
+"Recoil": 3,
+"Notes": "3"
+}}}}'
+where Id = 10093;
+
+-- ********************************************************************* SMG, 9mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10094,
+'Weapon',
+'SMG, 9mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 7,
+"Cost": 1200,
+"Weight": "7.5/1.2"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 41,
+"Damage": "3d-1 pi",
+"Acc": 4,
+"Range": "160/1900",
+"RoF": 13,
+"Shots": "30+1(3)",
+"ST": "10†",
+"Bulk": -4,
+"Recoil": 2,
+"Notes": "3"
+}}}}'
+where Id = 10094;
+
+-- ********************************************************************* PDW, 4.6mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10095,
+'Weapon',
+'PDW, 4.6mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 8,
+"Cost": 800,
+"Weight": "3.9/0.5"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 41,
+"Damage": "4d+1 pi-",
+"Acc": 3,
+"Range": "200/2000",
+"RoF": 15,
+"Shots": "20+1(3)",
+"ST": "7†",
+"Bulk": -3,
+"Recoil": 2,
+"Notes": null
+}}}}'
+where Id = 10095;
+
+-- ********************************************************************* Gause PDW, 4mm
+insert into Objects (Id, ClassType, Name, Definition) values (
+10096,
+'Weapon',
+'Gause PDW, 4mm',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 10,
+"Cost": 3600,
+"Weight": "4.6/1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 41,
+"Damage": "4d(3) pi-",
+"Acc": "6+1",
+"Range": "700/2900",
+"RoF": 16,
+"Shots": "80(3)",
+"ST": "9†",
+"Bulk": -3,
+"Recoil": 2,
+"Notes": "1"
+}}}}'
+where Id = 10096;
+
+-- ********************************************************************* Handgonne, .90
+insert into Objects (Id, ClassType, Name, Definition) values (
+10097,
+'Weapon',
+'Handgonne, .90',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 3,
+"Cost": 300,
+"Weight": "15/0.1"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 42,
+"Damage": "2d p++",
+"Acc": 0,
+"Range": "100/600",
+"RoF": 1,
+"Shots": "1(60)",
+"ST": "10†",
+"Bulk": -6,
+"Recoil": 4,
+"Notes": null
+}}}}'
+where Id = 10097;
+
+-- ********************************************************************* Matchlock Musket, .80
+insert into Objects (Id, ClassType, Name, Definition) values (
+10098,
+'Weapon',
+'Matchlock Musket, .80',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 4,
+"Cost": 150,
+"Weight": "20/0.05"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 42,
+"Damage": "4d pi++",
+"Acc": 2,
+"Range": "100/600",
+"RoF": 1,
+"Shots": "1(60)",
+"ST": "10R†",
+"Bulk": -6,
+"Recoil": 3,
+"Notes": null
+}}}}'
+where Id = 10098;
+
+-- ********************************************************************* Flintlock Musket, .75
+insert into Objects (Id, ClassType, Name, Definition) values (
+10099,
+'Weapon',
+'Flintlock Musket, .75',
+JSON_OBJECT()
+);
+update Objects set Definition =
+'{
+"stats": 
+{
+"TL": 4,
+"Cost": 200,
+"Weight": "13/0.05"
+},
+"handling": 
+{
+"trigger": 
+{
+"both": 
+{
+"required skill group": 42,
+"Damage": "4d pi++",
+"Acc": 2,
+"Range": "100/1500",
+"RoF": 1,
+"Shots": "1(15)",
+"ST": "10†",
+"Bulk": -6,
+"Recoil": 4,
+"Notes": null
+}}}}'
+where Id = 10099;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
