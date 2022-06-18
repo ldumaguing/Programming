e181bed9b4d56f3d39e4790e0c63409eaf142ffe
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-06-17 21:45:08 -0400

    2022.06b

--- GURPS/Humanoid-v2.sql
+++ GURPS/Humanoid-v2.sql
@@ -8,7 +8,32 @@ JSON_OBJECT()
 );
 
 -- ********************************************************************* Definition
-update Objects set Definition =
+update Objects set  Definition =
+'{
+"basic attributes": {
+"ST": 10,
+"DX": 10,
+"IQ": 10,
+"HT": 10
+},
+"secondary characteristics": {
+"damage": {
+"thrust": "",
+"swing": ""
+},
+"basic lift": 0,
+"HP": 0,
+"Will": 0,
+"Per": 0,
+"FP": 0,
+"basic speed": 0,
+"basic move": 0
+}
+}'
+where Id = 5000;
+
+-- ********************************************************************* Body
+update Objects set  Body =
 '{
 "head": {
 "skull": {},
@@ -21,8 +46,12 @@ update Objects set Definition =
 "groin": {}
 },
 "limbs": {
-"arms": {},
-"legs": {}
+"arms": {
+"hands": {}
+},
+"legs": {
+"feet": {}
+}
 }
 }'
 where Id = 5000;
