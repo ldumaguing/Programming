9042ed3af9d7ae357fd03032a44ca85427d4b22c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-08-10 17:25:17 -0400

    Add files via upload

---
+++ C/mysql/calc_Swing.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void instructions(void);
+
+void Swing_1(int);
+void Swing_2(int);
+void Swing_3(int);
+void Swing_4(int);
+void Swing_5(int);
+void Swing_6(int);
+
+// ***************************************************************************************
+int main (int argc, char* argv[]) {
+	if (argc <= 1) {
+		instructions();
+		return 0; }
+
+	int val = atoi(argv[1]);
+
+	if ((val >= 1) & (val <= 12))
+		Swing_1(val);
+	if ((val >= 13) & (val <= 24))
+		Swing_2(val);
+	if ((val >= 25) & (val <= 30))
+		Swing_3(val);
+	if ((val >= 31) & (val <= 38))
+		Swing_4(val);
+	if ((val >= 39) & (val <= 44))
+		Swing_5(val);
+	if ((val >= 45) & (val <= 109))
+		Swing_6(val);
+
+	return 0; }
+
+// =======================================================================================
+void Swing_6(int val) {
+	if ((val >= 45) & (val <= 49)) {
+		printf("7d+1\n");
+		return; }
+
+	int num = trunc(val / 10.0) + 3;
+	int modifier = 0;
+	
+	if ((val >= 50) & (val <= 54)) {
+		printf("8d-1");
+		return; }
+	if ((val >= 55) & (val <= 59)) {
+		printf("8d+1");
+		return; }
+
+	modifier = trunc(val / 5.0);
+	modifier = (modifier % 2) * 2;
+	if (modifier == 0)
+		printf("%dd\n", num);
+	else
+		printf("%dd+%d\n", num, modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Swing_5(int val) {
+	printf("7d-1\n"); }
+
+// ---------------------------------------------------------------------------------------
+void Swing_4(int val) {
+	int modifier = ceil(val / 2.0) - 17;
+	if (modifier < 0)
+		printf("6d%d\n", modifier);
+	if (modifier == 0)
+		printf("6d\n");
+	if (modifier > 0)
+		printf("6d+%d\n", modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Swing_3(int val) {
+	int modifier = ceil(val / 2.0) - 13;
+	if (val == 25)
+		modifier = -1;
+	if (modifier < 0)
+		printf("5d%d\n", modifier);
+	if (modifier == 0)
+		printf("5d\n");
+	if (modifier > 0)
+		printf("5d+%d\n", modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Swing_2(int val) {
+	int num = ceil(val / 4.0) - 2;
+	int modifier = ((val+3) % 4) - 1;
+	if (modifier < 0)
+		printf("%dd%d\n", num, modifier);
+	if (modifier == 0)
+		printf("%dd\n", num);
+	if (modifier > 0) 
+		printf("%dd+%d\n", num, modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Swing_1(int val) {
+	int modifier = round(val / 2.0) - 6;
+	if ((val >= 1) & (val <=9))
+		printf("1d%d\n", modifier);
+	if (val == 10)
+		printf("1d\n");
+	if (val == 11)
+		printf("1d+1\n");
+	if (val == 12)
+		printf("1d+2\n"); }
+
+// ***************************************************************************************
+void instructions() {
+	puts("calc_Swing   number");
+	puts("");
+}
---
+++ C/mysql/calc_Thrust.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void instructions(void);
+
+void Thrust_1(int);
+void Thrust_2(int);
+void Thrust_3(int);
+void Thrust_4(int);
+void Thrust_5(int);
+void Thrust_6(int);
+
+// ***************************************************************************************
+int main (int argc, char* argv[]) {
+	if (argc <= 1) {
+		instructions();
+		return 0; }
+
+	int val = atoi(argv[1]);
+
+	if ((val >= 1) & (val <= 18))
+		Thrust_1(val);
+	if ((val >= 19) & (val <= 34))
+		Thrust_2(val);
+	if ((val >= 35) & (val <= 44))
+		Thrust_3(val);
+	if ((val >= 45) & (val <= 59))
+		Thrust_4(val);
+	if ((val >= 60) & (val <= 109))
+		Thrust_5(val);
+	if (val >= 110)
+		Thrust_6(val);
+
+
+	return 0; }
+
+// =======================================================================================
+void Thrust_6(int val) {
+	int num = trunc(val / 10) + 1;
+	printf("%dd\n", num); }
+
+// ---------------------------------------------------------------------------------------
+void Thrust_5(int val) {
+	int num = trunc(val / 10) + 1;
+	int modifier = 0;
+	if ((val >= 60) & (val <= 64))
+		modifier = -1;
+	if ((val >= 65) & (val <= 69))
+		modifier = 1;
+	if (((val >= 75) & (val <= 79))
+		| ((val >= 85) & (val <= 89))
+		| ((val >= 95) & (val <= 99)))
+		modifier = 2;
+
+	if (modifier < 0)
+		printf("%dd%d\n", num, modifier);
+	if (modifier == 0)
+		printf("%dd\n", num);
+	if (modifier > 0)
+		printf("%dd+%d\n", num, modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Thrust_4(int val) {
+	if ((val >= 45) & (val <= 49))
+		printf("5d\n");
+	if ((val >= 50) & (val <= 54))
+		printf("5d+2\n");
+	if ((val >= 55) & (val <= 59))
+		printf("6d\n"); }
+
+// ---------------------------------------------------------------------------------------
+void Thrust_3(int val) {
+	if ((val == 35) | (val == 36))
+		printf("4d-1\n");
+	if ((val == 37) | (val == 38))
+		printf("4d\n");
+	if ((val >= 39) & (val < 45))
+		printf("4d+1\n"); }
+
+// ---------------------------------------------------------------------------------------
+void Thrust_2(int val) {
+	int num = 2;
+	if (val >= 27) num = 3;
+	int modifier = round(val / 2.0) - 11;
+	if (val >= 27) modifier = modifier - 4;
+	if (modifier < 0)
+		printf("%dd%d\n", num, modifier);
+	if (modifier == 0)
+		printf("%dd\n", num);
+	if (modifier > 0)
+		printf("%dd+%d\n", num, modifier); }
+
+// ---------------------------------------------------------------------------------------
+void Thrust_1(int val) {
+	int modifier = round(val / 2.0) - 7;
+	if (modifier < 0)
+		printf("1d%d\n", modifier);
+	if (modifier == 0)
+		printf("1d\n");
+	if (modifier > 0)
+		printf("1d+%d\n", modifier); }
+
+// ***************************************************************************************
+void instructions() {
+	puts("calc_Thrust   number");
+	puts("");
+}
