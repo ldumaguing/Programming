dc38510589f190e3593a971ec0683dcb8f6e02e1
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-08-15 22:18:24 -0400

    22:18

--- C/mysql/scan_Hero.c
+++ C/mysql/scan_Hero.c
@@ -8,35 +8,71 @@ MYSQL* open_DB();
 void default_scan(MYSQL*, int);
 void opt_wearing(MYSQL*, int);
 void opt_wearing1(MYSQL*, int);
+void opt_rearrange(MYSQL*, int, char**);
 
 // ***************************************************************************************
 int main (int argc, char* argv[]) {
-	if ((argc == 1)
-	| (argc > 3)) {
+	if (argc == 1) {
 		instructions();
 		return 0; }
 
 	MYSQL* conn = open_DB();
 	int HeroID = atoi(argv[1]);
+	if (HeroID <= 0) return 0;
 	if (argc == 2) {
 		default_scan(conn, HeroID);
 		mysql_close(conn);
 		return 0;}
 
+
 	char option[512];
 	sprintf(option, "%s", argv[2]);
 
 	if (strcmp(option, "wearing") == 0) {
-		opt_wearing(conn, HeroID);
-		mysql_close(conn);
-		return 0; }
+		opt_wearing(conn, HeroID); }
 
-
+	if (strcmp(option, "rearrange") == 0) {
+		opt_rearrange(conn, HeroID, argv); }
+		
 
 
 
 	mysql_close(conn);
 	return 0; }
+
+// ***************************************************************************************
+void opt_rearrange(MYSQL* conn, int HeroID, char* argv[]) {
+	int lay1 = atoi(argv[3]);
+	int lay2 = atoi(argv[4]);
+	char stmt[256];
+	// ********** get array length
+	sprintf(stmt, "SELECT JSON_LENGTH(Definition,"
+		" '$.\"armors\"') from TheWorld where Id = %d"
+	, HeroID);
+
+	if (mysql_query(conn, stmt)) {
+		puts("error SELECTing");
+		return; }
+
+	MYSQL_RES *result = mysql_store_result(conn);
+	if (result == NULL) puts("result is null");
+	MYSQL_ROW row;
+	if (result == NULL) {
+		puts("error RESULT");
+		return; }
+	row = mysql_fetch_row(result);
+	int len = atoi(row[0]);        // <----------- len
+	if (len <= 1) return;
+
+	int bigNum = (lay1 > lay2) ? lay1 : lay2;
+	printf("big: %d .. %d\n", bigNum, len);
+	if (len > bigNum) return;
+	
+	
+	
+	
+	
+}
 
 // ***************************************************************************************
 void opt_wearing(MYSQL* conn, int HeroID) {
@@ -76,6 +112,7 @@ void opt_wearing(MYSQL* conn, int HeroID) {
 			return; }
 		row = mysql_fetch_row(result);
 		ArmorID = atoi(row[0]);
+		printf("Layer %d: ", i + 1);
 		opt_wearing1(conn, ArmorID);
 		}}
 
@@ -152,4 +189,14 @@ void instructions() {
 	puts("scan_Hero   HeroID   [what]");
 	puts("   [what]:");
 	puts("    wearing");
+	puts("    rearrange   layer1   layer2");
+	puts("");
 }
+
+
+
+
+
+
+
+
