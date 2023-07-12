9c9376bba7619efd61c60ebecd0d67c2ca7b6f74
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2023-07-12 12:25:20 -0400

    Add files via upload

---
+++ Pico/Trump/fishes/fish0005.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+	printf("%x\n", 64>>0);
+	printf("%x\n", 64>>1);
+	printf("%x\n", 64>>2);
+	printf("%x\n", 64>>3);
+	printf("%x\n", 64>>4);
+	printf("%x\n", 64>>5);
+}
