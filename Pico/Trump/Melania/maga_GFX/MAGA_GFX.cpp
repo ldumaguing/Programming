18e3697e7a1c91a9176799e55365de2b8188a413
Lawrence Dumaguing <os2nlinux@gmail.com>
2023-07-18 23:01:58 -0400

    2023.07b

--- Pico/Trump/Melania/maga_GFX/MAGA_GFX.cpp
+++ Pico/Trump/Melania/maga_GFX/MAGA_GFX.cpp
@@ -12,10 +12,6 @@ uint16_t screenbuffer[ILI9341_SIZE] = { 0 };
 MAGA_GFX::MAGA_GFX() {
 }
 
-
-
-
-
 /**************************************************************************/
 /*!
    @brief    Draw a circle outline
@@ -25,61 +21,39 @@ MAGA_GFX::MAGA_GFX() {
     @param    color 16-bit 5-6-5 Color to draw with
 */
 /**************************************************************************/
-void MAGA_GFX::drawCircle(int16_t x0, int16_t y0, int16_t r,
-                              int color) {
-  int16_t f = 1 - r;
-  int16_t ddF_x = 1;
-  int16_t ddF_y = -2 * r;
-  int16_t x = 0;
-  int16_t y = r;
+void MAGA_GFX::drawCircle(int16_t x0, int16_t y0, int16_t r, int color) {
+	int16_t f = 1 - r;
+	int16_t ddF_x = 1;
+	int16_t ddF_y = -2 * r;
+	int16_t x = 0;
+	int16_t y = r;
 
+	writePixel(x0, y0 + r, color);
+	writePixel(x0, y0 - r, color);
+	writePixel(x0 + r, y0, color);
+	writePixel(x0 - r, y0, color);
 
-  writePixel(x0, y0 + r, color);
-  writePixel(x0, y0 - r, color);
-  writePixel(x0 + r, y0, color);
-  writePixel(x0 - r, y0, color);
+	while (x < y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
 
-  while (x < y) {
-    if (f >= 0) {
-      y--;
-      ddF_y += 2;
-      f += ddF_y;
-    }
-    x++;
-    ddF_x += 2;
-    f += ddF_x;
-
-    writePixel(x0 + x, y0 + y, color);
-    writePixel(x0 - x, y0 + y, color);
-    writePixel(x0 + x, y0 - y, color);
-    writePixel(x0 - x, y0 - y, color);
-    writePixel(x0 + y, y0 + x, color);
-    writePixel(x0 - y, y0 + x, color);
-    writePixel(x0 + y, y0 - x, color);
-    writePixel(x0 - y, y0 - x, color);
-  }
+		writePixel(x0 + x, y0 + y, color);
+		writePixel(x0 - x, y0 + y, color);
+		writePixel(x0 + x, y0 - y, color);
+		writePixel(x0 - x, y0 - y, color);
+		writePixel(x0 + y, y0 + x, color);
+		writePixel(x0 - y, y0 + x, color);
+		writePixel(x0 + y, y0 - x, color);
+		writePixel(x0 - y, y0 - x, color);
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /**************************************************************************/
 /*!
@@ -92,17 +66,11 @@ void MAGA_GFX::drawCircle(int16_t x0, int16_t y0, int16_t r,
    @param    color 16-bit 5-6-5 Color to fill with
 */
 /**************************************************************************/
-void MAGA_GFX::fillRect(int16_t x, int16_t y, int16_t w, int16_t h,
-                            int color) {
-
-  for (int16_t i = x; i < x + w; i++) {
-    writeFastVLine(i, y, h, color);
-  }
-
+void MAGA_GFX::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, int color) {
+	for (int16_t i = x; i < x + w; i++) {
+		writeFastVLine(i, y, h, color);
+	}
 }
-
-
-
 
 /**************************************************************************/
 /*!
@@ -114,12 +82,8 @@ void MAGA_GFX::fillRect(int16_t x, int16_t y, int16_t w, int16_t h,
    @param    color 16-bit 5-6-5 Color to fill with
 */
 /**************************************************************************/
-void MAGA_GFX::writeFastVLine(int16_t x, int16_t y, int16_t h,
-                                  int color) {
-  // Overwrite in subclasses if startWrite is defined!
-  // Can be just writeLine(x, y, x, y+h-1, color);
-  // or writeFillRect(x, y, 1, h, color);
-  drawFastVLine(x, y, h, color);
+void MAGA_GFX::writeFastVLine(int16_t x, int16_t y, int16_t h, int color) {
+	drawFastVLine(x, y, h, color);
 }
 
 /**************************************************************************/
@@ -132,30 +96,9 @@ void MAGA_GFX::writeFastVLine(int16_t x, int16_t y, int16_t h,
    @param    color 16-bit 5-6-5 Color to fill with
 */
 /**************************************************************************/
-void MAGA_GFX::writeFastHLine(int16_t x, int16_t y, int16_t w,
-                                  int color) {
-  // Overwrite in subclasses if startWrite is defined!
-  // Example: writeLine(x, y, x+w-1, y, color);
-  // or writeFillRect(x, y, w, 1, color);
-  drawFastHLine(x, y, w, color);
+void MAGA_GFX::writeFastHLine(int16_t x, int16_t y, int16_t w, int color) {
+	drawFastHLine(x, y, w, color);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /**************************************************************************/
 /*!
@@ -166,9 +109,8 @@ void MAGA_GFX::writeFastHLine(int16_t x, int16_t y, int16_t w,
 */
 /**************************************************************************/
 void MAGA_GFX::writePixel(int16_t x, int16_t y, int color) {
-  drawPixel(x, y, color);
+	drawPixel(x, y, color);
 }
-
 
 /**************************************************************************/
 /*!
@@ -180,22 +122,12 @@ void MAGA_GFX::writePixel(int16_t x, int16_t y, int color) {
     @param    color 16-bit 5-6-5 Color to draw with
 */
 /**************************************************************************/
-void MAGA_GFX::drawRect(int16_t x, int16_t y, int16_t w, int16_t h,
-                            int color) {
-  writeFastHLine(x, y, w, color);
-  writeFastHLine(x, y + h - 1, w, color);
-  writeFastVLine(x, y, h, color);
-  writeFastVLine(x + w - 1, y, h, color);
+void MAGA_GFX::drawRect(int16_t x, int16_t y, int16_t w, int16_t h, int color) {
+	writeFastHLine(x, y, w, color);
+	writeFastHLine(x, y + h - 1, w, color);
+	writeFastVLine(x, y, h, color);
+	writeFastVLine(x + w - 1, y, h, color);
 }
-
-
-
-
-
-
-
-
-
 
 /**************************************************************************/
 /*!
@@ -207,44 +139,43 @@ void MAGA_GFX::drawRect(int16_t x, int16_t y, int16_t w, int16_t h,
     @param    color 16-bit 5-6-5 Color to draw with
 */
 /**************************************************************************/
-void MAGA_GFX::writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-                             int color) {
-  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
-  if (steep) {
-    _swap_int16_t(x0, y0);
-    _swap_int16_t(x1, y1);
-  }
+void MAGA_GFX::writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int color) {
+	int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+	if (steep) {
+		_swap_int16_t(x0, y0);
+		_swap_int16_t(x1, y1);
+	}
 
-  if (x0 > x1) {
-    _swap_int16_t(x0, x1);
-    _swap_int16_t(y0, y1);
-  }
+	if (x0 > x1) {
+		_swap_int16_t(x0, x1);
+		_swap_int16_t(y0, y1);
+	}
 
-  int16_t dx, dy;
-  dx = x1 - x0;
-  dy = abs(y1 - y0);
+	int16_t dx, dy;
+	dx = x1 - x0;
+	dy = abs(y1 - y0);
 
-  int16_t err = dx / 2;
-  int16_t ystep;
+	int16_t err = dx / 2;
+	int16_t ystep;
 
-  if (y0 < y1) {
-    ystep = 1;
-  } else {
-    ystep = -1;
-  }
+	if (y0 < y1) {
+		ystep = 1;
+	} else {
+		ystep = -1;
+	}
 
-  for (; x0 <= x1; x0++) {
-    if (steep) {
-      writePixel(y0, x0, color);
-    } else {
-      writePixel(x0, y0, color);
-    }
-    err -= dy;
-    if (err < 0) {
-      y0 += ystep;
-      err += dx;
-    }
-  }
+	for (; x0 <= x1; x0++) {
+		if (steep) {
+			writePixel(y0, x0, color);
+		} else {
+			writePixel(x0, y0, color);
+		}
+		err -= dy;
+		if (err < 0) {
+			y0 += ystep;
+			err += dx;
+		}
+	}
 }
 
 /**************************************************************************/
@@ -258,18 +189,17 @@ void MAGA_GFX::writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
 */
 /**************************************************************************/
 void MAGA_GFX::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int color) {
-  // Update in subclasses if desired!
-  if (x0 == x1) {
-    if (y0 > y1)
-      _swap_int16_t(y0, y1);
-    drawFastVLine(x0, y0, y1 - y0 + 1, color);
-  } else if (y0 == y1) {
-    if (x0 > x1)
-      _swap_int16_t(x0, x1);
-    drawFastHLine(x0, y0, x1 - x0 + 1, color);
-  } else {
-    writeLine(x0, y0, x1, y1, color);
-  }
+	if (x0 == x1) {
+		if (y0 > y1)
+			_swap_int16_t(y0, y1);
+		drawFastVLine(x0, y0, y1 - y0 + 1, color);
+	} else if (y0 == y1) {
+		if (x0 > x1)
+			_swap_int16_t(x0, x1);
+		drawFastHLine(x0, y0, x1 - x0 + 1, color);
+	} else {
+		writeLine(x0, y0, x1, y1, color);
+	}
 }
 
 /**************************************************************************/
@@ -281,50 +211,49 @@ void MAGA_GFX::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int colo
    @param    color   color 16-bit 5-6-5 Color to draw line with
 */
 /**************************************************************************/
-void MAGA_GFX::drawFastVLine(int16_t x, int16_t y, int16_t h,
-                                int color) {
-  if (h < 0) { // Convert negative heights to positive equivalent
-    h *= -1;
-    y -= h - 1;
-    if (y < 0) {
-      h += y;
-      y = 0;
-    }
-  }
+void MAGA_GFX::drawFastVLine(int16_t x, int16_t y, int16_t h, int color) {
+	if (h < 0) { // Convert negative heights to positive equivalent
+		h *= -1;
+		y -= h - 1;
+		if (y < 0) {
+			h += y;
+			y = 0;
+		}
+	}
 
-  // Edge rejection (no-draw if totally off canvas)
-  if ((x < 0) || (x >= _width) || (y >= _height) || ((y + h - 1) < 0)) {
-    return;
-  }
+	// Edge rejection (no-draw if totally off canvas)
+	if ((x < 0) || (x >= _width) || (y >= _height) || ((y + h - 1) < 0)) {
+		return;
+	}
 
-  if (y < 0) { // Clip top
-    h += y;
-    y = 0;
-  }
-  if (y + h > _height) { // Clip bottom
-    h = _height - y;
-  }
+	if (y < 0) { // Clip top
+		h += y;
+		y = 0;
+	}
+	if (y + h > _height) { // Clip bottom
+		h = _height - y;
+	}
 
-  if (rotation == 0) {
-    drawFastRawVLine(x, y, h, color);
-  } else if (rotation == 1) {
-    int16_t t = x;
-    x = WIDTH - 1 - y;
-    y = t;
-    x -= h - 1;
-    drawFastRawHLine(x, y, h, color);
-  } else if (rotation == 2) {
-    x = WIDTH - 1 - x;
-    y = HEIGHT - 1 - y;
+	if (rotation == 0) {
+		drawFastRawVLine(x, y, h, color);
+	} else if (rotation == 1) {
+		int16_t t = x;
+		x = WIDTH - 1 - y;
+		y = t;
+		x -= h - 1;
+		drawFastRawHLine(x, y, h, color);
+	} else if (rotation == 2) {
+		x = WIDTH - 1 - x;
+		y = HEIGHT - 1 - y;
 
-    y -= h - 1;
-    drawFastRawVLine(x, y, h, color);
-  } else if (rotation == 3) {
-    int16_t t = x;
-    x = y;
-    y = HEIGHT - 1 - t;
-    drawFastRawHLine(x, y, h, color);
-  }
+		y -= h - 1;
+		drawFastRawVLine(x, y, h, color);
+	} else if (rotation == 3) {
+		int16_t t = x;
+		x = y;
+		y = HEIGHT - 1 - t;
+		drawFastRawHLine(x, y, h, color);
+	}
 }
 
 /**************************************************************************/
@@ -336,50 +265,49 @@ void MAGA_GFX::drawFastVLine(int16_t x, int16_t y, int16_t h,
    @param  color  Color 16-bit 5-6-5 Color to draw line with
 */
 /**************************************************************************/
-void MAGA_GFX::drawFastHLine(int16_t x, int16_t y, int16_t w,
-                                int color) {
-  if (w < 0) { // Convert negative widths to positive equivalent
-    w *= -1;
-    x -= w - 1;
-    if (x < 0) {
-      w += x;
-      x = 0;
-    }
-  }
+void MAGA_GFX::drawFastHLine(int16_t x, int16_t y, int16_t w, int color) {
+	if (w < 0) { // Convert negative widths to positive equivalent
+		w *= -1;
+		x -= w - 1;
+		if (x < 0) {
+			w += x;
+			x = 0;
+		}
+	}
 
-  // Edge rejection (no-draw if totally off canvas)
-  if ((y < 0) || (y >= _height) || (x >= _width) || ((x + w - 1) < 0)) {
-    return;
-  }
+	// Edge rejection (no-draw if totally off canvas)
+	if ((y < 0) || (y >= _height) || (x >= _width) || ((x + w - 1) < 0)) {
+		return;
+	}
 
-  if (x < 0) { // Clip left
-    w += x;
-    x = 0;
-  }
-  if (x + w >= _width) { // Clip right
-    w = _width - x;
-  }
+	if (x < 0) { // Clip left
+		w += x;
+		x = 0;
+	}
+	if (x + w >= _width) { // Clip right
+		w = _width - x;
+	}
 
-  if (rotation == 0) {
-    drawFastRawHLine(x, y, w, color);
-  } else if (rotation == 1) {
-    int16_t t = x;
-    x = WIDTH - 1 - y;
-    y = t;
-    drawFastRawVLine(x, y, w, color);
-  } else if (rotation == 2) {
-    x = WIDTH - 1 - x;
-    y = HEIGHT - 1 - y;
+	if (rotation == 0) {
+		drawFastRawHLine(x, y, w, color);
+	} else if (rotation == 1) {
+		int16_t t = x;
+		x = WIDTH - 1 - y;
+		y = t;
+		drawFastRawVLine(x, y, w, color);
+	} else if (rotation == 2) {
+		x = WIDTH - 1 - x;
+		y = HEIGHT - 1 - y;
 
-    x -= w - 1;
-    drawFastRawHLine(x, y, w, color);
-  } else if (rotation == 3) {
-    int16_t t = x;
-    x = y;
-    y = HEIGHT - 1 - t;
-    y -= w - 1;
-    drawFastRawVLine(x, y, w, color);
-  }
+		x -= w - 1;
+		drawFastRawHLine(x, y, w, color);
+	} else if (rotation == 3) {
+		int16_t t = x;
+		x = y;
+		y = HEIGHT - 1 - t;
+		y -= w - 1;
+		drawFastRawVLine(x, y, w, color);
+	}
 }
 
 /**************************************************************************/
@@ -392,12 +320,12 @@ void MAGA_GFX::drawFastHLine(int16_t x, int16_t y, int16_t w,
 */
 /**************************************************************************/
 void MAGA_GFX::drawFastRawVLine(int16_t x, int16_t y, int16_t h, int color) {
-  // x & y already in raw (rotation 0) coordinates, no need to transform.
-  uint16_t *buffer_ptr = screenbuffer + y * WIDTH + x;
-  for (int16_t i = 0; i < h; i++) {
-    (*buffer_ptr) = colors[color];
-    buffer_ptr += WIDTH;
-  }
+	// x & y already in raw (rotation 0) coordinates, no need to transform.
+	uint16_t *buffer_ptr = screenbuffer + y * WIDTH + x;
+	for (int16_t i = 0; i < h; i++) {
+		(*buffer_ptr) = colors[color];
+		buffer_ptr += WIDTH;
+	}
 }
 
 /**************************************************************************/
@@ -410,11 +338,11 @@ void MAGA_GFX::drawFastRawVLine(int16_t x, int16_t y, int16_t h, int color) {
 */
 /**************************************************************************/
 void MAGA_GFX::drawFastRawHLine(int16_t x, int16_t y, int16_t w, int color) {
-  // x & y already in raw (rotation 0) coordinates, no need to transform.
-  uint32_t buffer_index = y * WIDTH + x;
-  for (uint32_t i = buffer_index; i < buffer_index + w; i++) {
-    screenbuffer[i] = colors[color];
-  }
+	// x & y already in raw (rotation 0) coordinates, no need to transform.
+	uint32_t buffer_index = y * WIDTH + x;
+	for (uint32_t i = buffer_index; i < buffer_index + w; i++) {
+		screenbuffer[i] = colors[color];
+	}
 }
 
 /**************************************************************************/
