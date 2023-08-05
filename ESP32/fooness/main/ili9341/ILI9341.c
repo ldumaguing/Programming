51ae76ad8f26fa8195da12f2a9ef648091a7cb13
Lawrence Dumaguing <os2nlinux@gmail.com>
2023-08-05 02:24:52 -0400

    2023.08a

--- ESP32/fooness/main/ili9341/ILI9341.c
+++ ESP32/fooness/main/ili9341/ILI9341.c
@@ -1,25 +1,24 @@
 // ************************************************************************************** ILI9341.c
-uint16_t screenbuffer[ILI9341_SIZE] = { 0 };
+uint16_t screenbuffer[ILI9341_SIZE];
 
 volatile uint32_t* gpio_out_w1ts_reg = (volatile uint32_t*) GPIO_OUT_W1TS_REG;
 volatile uint32_t* gpio_out_w1tc_reg = (volatile uint32_t*) GPIO_OUT_W1TC_REG;
 volatile uint32_t* gpio_enable_reg = (volatile uint32_t*) GPIO_ENABLE_REG;
 
 static inline void init_pins() {
-	
-	*gpio_enable_reg = colorPins | controlPins;
-	*gpio_out_w1ts_reg = colorPins | controlPins;   // 1111 1111 : 11 11
-	sleep_ms(3000);
-	*gpio_out_w1tc_reg = colorPins;                 // 0000 0000 : 11 11
-	sleep_ms(3000);
+	*gpio_enable_reg = datumPins | controlPins;
+	*gpio_out_w1ts_reg = datumPins | controlPins;   // 1111 1111 : 11 11
+	sleep_ms(250);
+	*gpio_out_w1tc_reg = datumPins;                 // 0000 0000 : 11 11
+	sleep_ms(250);
 };
 
 static inline void sio_write(void *src, size_t len) {
 	char *x = (char *)src;
-	uint32_t colour;
+	uint32_t datum;
 	do {
-		colour = 0;
-		*gpio_out_w1tc_reg = colorPins;
+		datum = 0;
+		*gpio_out_w1tc_reg = datumPins;
 /*
 		if(*x&1) *gpio_out_w1ts_reg = (1 << ILI9341_D0);
 		if(*x&2) *gpio_out_w1ts_reg = (1 << ILI9341_D1);
@@ -30,32 +29,31 @@ static inline void sio_write(void *src, size_t len) {
 		if(*x&64) *gpio_out_w1ts_reg = (1 << ILI9341_D6);
 		if(*x&128) *gpio_out_w1ts_reg = (1 << ILI9341_D7);
 
-		colour |= ((*x&1) << ILI9341_D0);         // 17
-		colour |= ((*x&2) << (ILI9341_D1-1));     // 5
-		colour |= ((*x&4) << (ILI9341_D2-2));     // 18
-		colour |= ((*x&8) << (ILI9341_D3-3));     // 19
-		colour |= ((*x&16) << (ILI9341_D4-4));    // 21
-		colour |= ((*x&32) << (ILI9341_D5-5));    // 22
-		colour |= ((*x&64) << (ILI9341_D6-6));    // 23
-		colour |= ((*x&128) << (ILI9341_D7-7));   // 25
+		datum |= ((*x&1) << ILI9341_D0);         // 17
+		datum |= ((*x&2) << (ILI9341_D1-1));     // 5
+		datum |= ((*x&4) << (ILI9341_D2-2));     // 18
+		datum |= ((*x&8) << (ILI9341_D3-3));     // 19
+		datum |= ((*x&16) << (ILI9341_D4-4));    // 21
+		datum |= ((*x&32) << (ILI9341_D5-5));    // 22
+		datum |= ((*x&64) << (ILI9341_D6-6));    // 23
+		datum |= ((*x&128) << (ILI9341_D7-7));   // 25
 */
-		colour |= ((*x&1) << 17);     // 17
-		colour |= ((*x&2) << 4);      // 5
-		colour |= ((*x&4) << 16);     // 18
-		colour |= ((*x&8) << 16);     // 19
-		colour |= ((*x&16) << 17);    // 21
-		colour |= ((*x&32) << 17);    // 22
-		colour |= ((*x&64) << 17);    // 23
-		colour |= ((*x&128) << 18);   // 25
+		datum |= ((*x&1) << 17);     // 17 - 0
+		datum |= ((*x&2) << 4);      // 5  - 1
+		datum |= ((*x&4) << 16);     // 18 - 2
+		datum |= ((*x&8) << 16);     // 19 - 3
+		datum |= ((*x&16) << 17);    // 21 - 4
+		datum |= ((*x&32) << 17);    // 22 - 5
+		datum |= ((*x&64) << 17);    // 23 - 6
+		datum |= ((*x&128) << 18);   // 25 - 7
 
-		*gpio_out_w1ts_reg = colour;
+		*gpio_out_w1ts_reg = datum;
 		WR_STROBE;
 
 		len--;
 		x++;
 	} while (len > 0);
 }
-
 
 // ************************************************************************************************
 void ILI9341_init() {
@@ -72,15 +70,7 @@ void ILI9341_init() {
 
 	ILI9341_set_command(ILI9341_GAMMASET);
 	ILI9341_command_param(0x01);
-/*
-	// positive gamma correction
-	set_command(ILI9341_GMCTRP1);
-    write_data((const uint8_t[15]){ 0x0f, 0x31, 0x2b, 0x0c, 0x0e, 0x08, 0x4e, 0xf1, 0x37, 0x07, 0x10, 0x03, 0x0e, 0x09, 0x00 }, 15);
 
-	// negative gamma correction
-	set_command(ILI9341_GMCTRN1);
-	write_data((const uint8_t[15]){ 0x00, 0x0e, 0x14, 0x03, 0x11, 0x07, 0x31, 0xc1, 0x48, 0x08, 0x0f, 0x0c, 0x31, 0x36, 0x0f }, 15);
-*/
 	// memory access control
 	ILI9341_set_command(ILI9341_MADCTL);
 	ILI9341_command_param(0x48);
@@ -140,10 +130,4 @@ void ILI9341_write_data(void *buffer, int bytes) {
 	sio_write(buffer, bytes);
 	CS_IDLE;
 };
-
-// *********************************************************************************** Adafruit base
-void ILI9341_begin() {
-	ILI9341_init();
-};
-
 
--- ESP32/fooness/main/ili9341/ILI9341.h
+++ ESP32/fooness/main/ili9341/ILI9341.h
@@ -12,11 +12,7 @@ void ILI9341_init();
 void ILI9341_set_command(uint8_t cmd);
 void ILI9341_command_param(uint8_t data);
 void ILI9341_write_data(void *buffer, int bytes);
-void ILI9341_pin_reset();
 void ILI9341_render();
-
-// ************************************************** Adafruit base
-void ILI9341_begin();
 
 // *************************************************************************************************
 struct ILI9341 ili;
