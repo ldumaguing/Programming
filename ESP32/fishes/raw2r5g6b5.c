f667ddebccc1200fc4f2b49680a8793ecd68b7ea
Lawrence Dumaguing <os2nlinux@gmail.com>
2023-08-24 23:49:42 -0400

    2023.08b

--- ESP32/fishes/raw2r5g6b5.c
+++ ESP32/fishes/raw2r5g6b5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
 void instructions();
 
@@ -17,8 +18,26 @@ int main (int argc, char *argv[]) {
 		printf("Error! opening file");
 		exit(1);
 	}
+	int width = atoi(argv[2]);
+	printf("%d\n", width);
+	uint8_t buf;
+	
+	struct pixel { uint8_t r, g, b; };
+	struct pixel pix;
 
-
+	int counter = 0;
+	while(fread(&pix, sizeof(struct pixel), 1, fptr)) {
+		if(width == 0)
+			printf("%02x%02x%02x ", pix.r, pix.g, pix.b);
+		else {
+			printf("%02x%02x%02x ", pix.r, pix.g, pix.b);
+			counter++;
+			if(counter>=width) {
+				counter = 0;
+				printf("\n");
+			}
+		}
+	}
 
 
 	return 0;
