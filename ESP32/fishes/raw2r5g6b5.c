ca03780b41f69dde6d4e148e5fd225662c175bc9
Lawrence Dumaguing <os2nlinux@gmail.com>
2023-08-25 13:46:43 -0400

    2023.08b

--- ESP32/fishes/raw2r5g6b5.c
+++ ESP32/fishes/raw2r5g6b5.c
@@ -8,8 +8,15 @@ struct pixel { uint8_t r, g, b; } pix;
 void instructions();
 
 void toR5G6B5() {
+	uint16_t dot = 0;
 	// printf("%02x%02x%02x ", pix.r, pix.g, pix.b);
-	printf("%02x ", (int)((pix.r/255.0) * 100));
+	//printf("%02x.", (int)((pix.r/255.0)*31));
+	//printf("%02x.", (int)((pix.g/255.0)*63));
+	//printf("%02x ", (int)((pix.b/255.0)*31));
+	dot |= ((int)((pix.r/255.0)*31) << 11);
+	dot |= ((int)((pix.g/255.0)*63) << 5);
+	dot |= ((int)((pix.b/255.0)*31) << 0);
+	printf("%04x, ", dot);
 }
 
 // ***************************************************************************************
@@ -26,7 +33,6 @@ int main (int argc, char *argv[]) {
 		exit(1);
 	}
 	int width = atoi(argv[2]);
-	printf("%d\n", width);
 	uint8_t buf;
 
 	int counter = 0;
