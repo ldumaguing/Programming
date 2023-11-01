06e1219e0699eb5480a9a76f3d2f03969914f0d2
Lawrence Dumaguing <os2nlinux@gmail.com>
2023-10-31 21:06:13 -0400

    2023.10b

--- ESP32/Old/clear_screen/components/gfx/include/gfx.h
+++
@@ -1,5 +0,0 @@
-void func(void);
-
-void GFX_init(void);
-void GFX_clearScreen(uint16_t);
-void GFX_refreshScreen(void);
--- ESP32/Old/one_pixel/components/gfx/include/gfx.h
+++
@@ -1,5 +0,0 @@
-void GFX_init(void);
-void GFX_clearScreen(uint16_t);
-void GFX_refreshScreen(void);
-
-void GFX_drawPixel(int16_t, int16_t, uint16_t);
