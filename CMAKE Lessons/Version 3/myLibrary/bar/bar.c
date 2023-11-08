fa7431cc783a5a2d9db5bbebcd6d9fdf1bebf935
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2023-11-08 15:30:16 -0500

    Delete CMAKE Lessons/Version 3 directory

--- CMAKE Lessons/Version 3/myLibrary/bar/bar.c
+++
@@ -1,8 +0,0 @@
-#include "bar.h"
-#include "Arduino.h"
-#include <stdio.h>
-
-int bar() {
-    printf("bar: %f\n", PI);
-	return 1313;
-}
--- CMAKE Lessons/Version 3/myLibrary/foo/foo.c
+++
@@ -1,8 +0,0 @@
-#include "foo.h"
-#include "Arduino.h"
-#include <stdio.h>
-
-int foo() {
-    printf("foo: %f\n", PI);
-	return 444;
-}
