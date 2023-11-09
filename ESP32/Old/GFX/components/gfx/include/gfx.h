9bd871d306740a3836758ae9e94530aae6af8e28
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2023-11-09 15:18:41 -0500

    Add files via upload

---
+++ ESP32/Old/GFX/components/gfx/include/gfx.h
@@ -0,0 +1,25 @@
+void GFX_init();
+void GFX_fillScreen(uint16_t);
+void GFX_fillScreen2(uint16_t, uint16_t);
+void GFX_refreshScreen();
+
+void GFX_drawPixel(int16_t, int16_t, uint16_t);
+void GFX_drawFastRawHLine(int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawFastRawVLine(int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawFastVLine(int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawFastHLine(int16_t, int16_t, int16_t, uint16_t);
+void GFX_writeLine(int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawLine(int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawRect(int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawCircle(int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawTriangle(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_drawCircleHelper(int16_t, int16_t, int16_t, uint8_t, uint16_t);
+void GFX_drawRoundRect(int16_t, int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_fillCircleHelper(int16_t, int16_t, int16_t, uint8_t, int16_t, uint16_t);
+void GFX_fillCircle(int16_t, int16_t, int16_t, uint16_t);
+void GFX_fillRect(int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_fillRoundRect(int16_t, int16_t, int16_t, int16_t, int16_t, uint16_t);
+void GFX_fillTriangle(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint16_t);
+ 
+void GFX_drawImage(int, int, int, int, int16_t *);
+
