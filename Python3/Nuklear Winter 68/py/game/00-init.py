67c56c51fb30991294f27e676e6eb4c02c6adeb4
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-01-24 16:24:10 -0500

    Update 00-init.py

--- Python3/Nuklear Winter 68/py/game/00-init.py
+++ Python3/Nuklear Winter 68/py/game/00-init.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 import sys
+import re
 
+mapName = ""
+
+def mode_Board(f):
+	global mapName
+
+	while True:
+		line = f.readline()
+		if re.search("^\.$", line):
+			break
+		print("..." + line)
+		if re.search("Board na", line):
+			mapName = line
 
 f = open(sys.argv[1], "r")
 
@@ -8,5 +21,11 @@ while True:
 	line = f.readline()
 	if not line:
 		break
+	if re.search(">>>>> BOA", line):
+		mode_Board(f)
+	# print(line)
 
-	print(line)
+print("::::" + mapName)
+
+
+
