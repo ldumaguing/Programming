f51bfa4bc81a978f52eac8a906ad00a9d4f02d47
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-01-28 13:43:52 -0500

    Add files via upload

--- Python3/Nuklear Winter 68/py/game/placeUnit.py
+++ Python3/Nuklear Winter 68/py/game/placeUnit.py
@@ -4,7 +4,7 @@ import re
 import mysql.connector
 
 if len(sys.argv)!=4:
-	print("placeUnit.py [scenario] [unitID] [hexID]")
+	print("placeUnit.py [scenario] [unitID] [hexID]\n")
 	exit()
 
 scenario = sys.argv[1]
@@ -12,6 +12,36 @@ scenario = scenario[scenario.find('/')+1:scenario.find('.')]
 unitID = sys.argv[2]
 hexID = sys.argv[3]
 
-print(len(sys.argv))
+hexX = 0
+hexY = 0
+
+if re.search("[A-Z][A-Z]", hexID):
+	hexX = ord(hexID[0:1]) - ord('A') + 26
+	hexY = int(hexID[2:])
+else:
+	hexX = ord(hexID[0:1]) - ord('A')
+	hexY = int(hexID[1:])
+	
+print(unitID, hexX, hexY)
+
+stmt = "update gameData set j = JSON_REPLACE(j, '$.hexLoc[0]', " \
+	+ str(hexX) + ", '$.hexLoc[1]'," + str(hexY) + ") where name = '" + unitID + "'"
+
+print(stmt)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="ayeka",
+  password="",
+  database="NuklearWinter68"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute(stmt)
+mydb.commit()
+
+
+
+
 
 
