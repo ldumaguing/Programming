1e7a15a397ffa34d1d99de59dfd79ee6c6f5754b
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-01-29 17:10:23 -0500

    Update A_init.py

--- Python3/Nuklear Winter 68/py/game/A_init.py
+++ Python3/Nuklear Winter 68/py/game/A_init.py
@@ -3,6 +3,9 @@ import sys
 import re
 import mariadb as sql
 
+unit_id = 0
+
+# ******************************************************************************
 def mode_Board(f):
 	mapWidth = 0
 	mapHeight = 0
@@ -55,6 +58,55 @@ def mode_Board(f):
 			width = int(line[0:line.find(',')])
 			hexX_count = int(line[line.find(',')+1:line.find(' ')])
 
+# ******************************************************************************
+def save_unit(scenario, unit, count, faction, formation):
+	global unit_id
+
+	fields = "scenario, name, val, j"
+
+	for x in range(count):
+		unit_id += 1
+		id = "u" + str(unit_id)
+		values = "'" + scenario + "', '" + id + "', '" \
+			+ faction + "," + formation + ";" + unit + "'" + ", '{}'"
+		sql.insert(fields, values)
+
+def save_units(scenario, units, faction, formation):
+	unit = ""
+	count = 0
+	while re.search(",", units):
+		unit = units[units.find("x")+1:units.find(",")]
+		count = int(units[0:units.find("x")])
+		save_unit(scenario, unit, count, faction, formation)
+		units = units[units.find(",")+2:]
+
+	unit = units[units.find("x")+1:]
+	count = int(units[0:units.find("x")])
+	save_unit(scenario, unit, count, faction, formation)
+
+def formation(faction, line):
+	if line.find("AMs")<=0:
+		return
+
+	scenario = sql.get_current_scenario()
+
+	if line.find(" -")>0:
+		formation = line[0:line.find(" -")]
+		units = line[line.find("-")+2:line.find(" /")]
+	else:
+		formation = faction
+		units = line[0:line.find(" /")]
+
+	fields = "name, val, scenario"
+	save_units(scenario, units, faction, formation)
+
+	am = line[line.find("/ ")+2:line.find("AMs")]
+	am = "AM:" + formation + ";" + am
+	values = "'" + am + "', '" \
+		+ line[line.find("/ ")+2:line.find("AMs")] + "', '" \
+		+ scenario + "'"
+	sql.insert(fields, values)
+
 def mode_OOB(f):
 	faction = ""
 
@@ -69,10 +121,7 @@ def mode_OOB(f):
 		if re.search("BLACK", line):
 			faction = "Black Hand"
 
-		if re.search(" - ", line):
-			w_formation(faction, line)
-		else:
-			wo_formation(faction, line)
+		formation(faction, line)
 
 # ****************************************************************************************
 def main():
@@ -88,15 +137,45 @@ def main():
 			break
 		if re.search(">>>>> BOARD", line):
 			mode_Board(f)
-		"""
 		if re.search(">>>>> ORDER", line):
 			mode_OOB(f)
+		"""
 		if re.search(">>>>> OBJEC", line):
 			a1.mode_Objective(f, scenario)
 		"""
 	f.close()
 
 # ****************************************************************************************
+def unit_setup():
+	fields = "name, val"
+	conditions = "name regexp '^u'"
+	results = sql.select(fields, conditions)
+	for x in results:
+		uID = x[0]
+		val = x[1]
+		formation = val[val.find(",")+1:val.find(";")]
+		unit = val[val.find(";")+1:]
+		print(uID, val, formation, unit)
+		fields = "front, back"
+		conditions = "name = '" + unit + "' and formation = '" + formation + "'"
+		res0 = sql.select_v_units(fields, conditions)
+		for y in res0:
+			j = "JSON_INSERT(j,'$.front', 'images/" + y[0] + "',"
+			j += " '$.back', 'images/" + y[1] + "',"
+			j += " '$.flip', 'front',"
+			j += " '$.hexLoc', JSON_ARRAY(-1,-1),"
+			j += " '$.pixLoc', JSON_ARRAY(0,0),"
+			j += " '$.identity', JSON_ARRAY("
+			j += "'" + formation + "'" + ", '" + unit + "')"
+			j += ")"
+			sql.update_j(j, uID)
+			h = "<img id=\"" + uID + "\" src=\"images/" + y[0] + "\" hidden>"
+			sql.update_html(h, uID)
+
+# ****************************************************************************************
 main()
+unit_setup()
+
+
 
 
