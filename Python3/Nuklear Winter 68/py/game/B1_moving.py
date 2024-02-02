d022104f5dfe5e36bf67a3db134700ded1d98dd9
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-02-02 02:55:13 -0500

    2024.02a

---
+++ Python3/Nuklear Winter 68/py/game/B1_moving.py
@@ -0,0 +1,27 @@
+import sys
+import re
+import mariadb as sql
+
+scenario = ""
+
+def get_current_hexLoc(unitA):
+	fields = "JSON_VALUE(j, '$.hexLoc[0]')"
+	table = "gameData"
+	conditions = "name = '" + unitA + "'"
+	hexX = sql.select_one(fields, conditions, table)[0]
+	fields = "JSON_VALUE(j, '$.hexLoc[1]')"
+	hexY = sql.select_one(fields, conditions, table)[0]
+	return (int(hexX), int(hexY))
+
+
+# ****************************************************************************************
+def move(f):
+	global scenario
+	scenario = sql.get_current_scenario()
+
+	unitA = f[0:f.find(" ")]
+	direction = f[f.find(">")+2:].strip()
+
+	X = get_current_hexLoc(unitA)
+	print(X)
+
