33a1c38a23781b250471f583658683b51208f6e7
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-02-04 23:19:17 -0500

    Update create_path_with_multi_points.py

--- Python3/HexMap/create_path_with_multi_points.py
+++ Python3/HexMap/create_path_with_multi_points.py
@@ -3,6 +3,139 @@ import sys
 import math
 import re
 
+def conver2number(hexID):
+	hex_X = 0
+	hex_Y = 0
+	askey = ord('A')
+	X = re.search("[0-9]", hexID).start()
+	if X<2:
+		hex_X = ord(hexID[0:X]) - askey
+	else:
+		hex_X = ord(hexID[0]) - askey + 26
+
+	hex_Y = hexID[X:]
+	
+	return (str(hex_X).zfill(2) + str(hex_Y).zfill(2))
+
+def get_X(hexID):
+	return int(hexID[0:2]);
+	
+def get_Y(hexID):
+	return int(hexID[2:]);
+
+def evenXcol(X, Y, direction):
+	match direction:
+		case "A":
+			Y = Y-1;
+		case "B":
+			X = X+1;
+			Y = Y-1;
+		case "C":
+			X = X+1;
+		case "D":
+			Y = Y+1;
+		case "E":
+			X = X-1;
+		case "F":
+			X = X-1;
+			Y = Y-1;
+
+	return str(X).zfill(2) + str(Y).zfill(2);
+
+def oddXcol(X, Y, direction):
+	match direction:
+		case "A":
+			Y = Y-1;
+		case "B":
+			X = X+1;
+		case "C":
+			X = X+1;
+			Y = Y+1;
+		case "D":
+			Y = Y+1;
+		case "E":
+			X = X-1;
+			Y = Y+1;
+		case "F":
+			X = X-1;
+
+	return str(X).zfill(2) + str(Y).zfill(2);
+
+def stepDir(hexID, direction):
+	X = get_X(hexID);
+	Y = get_Y(hexID);
+	if X%2:
+		return oddXcol(X, Y, direction);
+	else:
+		return evenXcol(X, Y, direction);
+
+def get_angle(hexID_a, hexID_b):
+	Xa = get_X(hexID_a);
+	Ya = get_Y(hexID_a);
+	Xb = get_X(hexID_b);
+	Yb = get_Y(hexID_b);
+	
+	cartisian_X = math.cos(math.radians(30)) * 2.0;
+
+	real_Xa = cartisian_X * Xa;
+	real_Ya = Ya*2;
+	if Xa%2:
+		real_Ya = real_Ya+1;
+
+	real_Xb = cartisian_X * Xb;
+	real_Yb = Yb*2;
+	if Xb%2:
+		real_Yb = real_Yb+1;
+
+	X = real_Xb - real_Xa;
+	Y = real_Yb - real_Ya;
+	
+	if X==0:
+		if Y<0:
+			return 90;
+		return 270;
+	
+	angle = (math.atan(Y/X)*180.0)/math.pi;
+	angle = round(angle, ndigits=2);
+	if X<0 and Y<0:
+		return 180 - angle;
+	if X<0 and Y>=0:
+		return abs(angle - 180);
+	if X>=0 and Y>=0:
+		return 360 - angle;
+
+	return abs(angle);
+
+# ****************************************************************************************
+def create_path(hexID_a, hexID_b):
+	if re.search("^[A-Za-z]", hexID_a):
+		hexID_a = conver2number(hexID_a)
+	
+	if re.search("^[A-Za-z]", hexID_b):
+		hexID_b = conver2number(hexID_b)
+	
+	direction = "";
+
+	while True:
+		if hexID_a == hexID_b:
+			return;
+
+		angle = get_angle(hexID_a, hexID_b);
+		if angle>0 and angle<=60:
+			direction = "B";
+		if angle>60 and angle<=120:
+			direction = "A";
+		if angle>120 and angle<=180:
+			direction = "F";
+		if angle>180 and angle<=240:
+			direction = "E";
+		if angle>240 and angle<=300:
+			direction = "D";
+		if angle>300:
+			direction = "C";
+		hexID_a = stepDir(hexID_a, direction);
+		print(hexID_a);
+
 def remove_redundance(pts):
 	pts = re.split(",", pts)
 	priv = ""
@@ -14,18 +147,21 @@ def remove_redundance(pts):
 		priv = x
 	return re.sub(",$", "", newString)
 
-def create_path(starting, pts):
+def create_paths(starting, pts):
 	starting = re.sub("\s", "", starting)
 	pts = starting + "," + re.sub("\s", "", pts)
 	pts = remove_redundance(pts)
 	pts = re.split(",", pts)
 	print(pts, len(pts))
 	for x in range(len(pts)-1):
-		print(".", pts[x])
+		print(pts[x], pts[x+1])
+		create_path(pts[x], pts[x+1])
 
 # ***** MAIN *****************************************************************************
 starting = "A1   "
-points = "A1,  B1,D10,  D10, E15, F1"
+points = "A1,  B1,D10,D11,  D10, E15, F1"
 
-create_path(starting, points)
+create_paths(starting, points)
+
+
 
