4f982c757f04bf2be793ddfd3a79329cdced07ab
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-02-10 10:55:04 -0500

    2024.02a

--- Python3/Nuklear Winter 68/py/game/create_path_with_multi_points.py
+++ Python3/Nuklear Winter 68/py/game/create_path_with_multi_points.py
@@ -3,153 +3,28 @@ import sys
 import math
 import re
 import mariadb as sql
+import hexagon as hx
 
 scenario = ""
-
-def conver2number(hexID):
-	hex_X = 0
-	hex_Y = 0
-	askey = ord('A')
-	X = re.search("[0-9]", hexID).start()
-	if X<2:
-		hex_X = ord(hexID[0:X]) - askey
-	else:
-		hex_X = ord(hexID[0]) - askey + 26
-
-	hex_Y = hexID[X:]
-	hex_Y = int(hex_Y) - 1
-	
-	return (str(hex_X).zfill(2) + str(hex_Y).zfill(2))
-
-def get_X(hexID):
-	return int(hexID[0:2]);
-	
-def get_Y(hexID):
-	return int(hexID[2:]);
-
-def evenXcol(X, Y, direction):
-	match direction:
-		case "A":
-			Y = Y-1;
-		case "B":
-			X = X+1;
-			Y = Y-1;
-		case "C":
-			X = X+1;
-		case "D":
-			Y = Y+1;
-		case "E":
-			X = X-1;
-		case "F":
-			X = X-1;
-			Y = Y-1;
-
-	return str(X).zfill(2) + str(Y).zfill(2);
-
-def oddXcol(X, Y, direction):
-	match direction:
-		case "A":
-			Y = Y-1;
-		case "B":
-			X = X+1;
-		case "C":
-			X = X+1;
-			Y = Y+1;
-		case "D":
-			Y = Y+1;
-		case "E":
-			X = X-1;
-			Y = Y+1;
-		case "F":
-			X = X-1;
-
-	return str(X).zfill(2) + str(Y).zfill(2);
-
-def stepDir(hexID, direction):
-	X = get_X(hexID);
-	Y = get_Y(hexID);
-	if X%2:
-		return oddXcol(X, Y, direction);
-	else:
-		return evenXcol(X, Y, direction);
-
-def get_angle(hexID_a, hexID_b):
-	Xa = get_X(hexID_a);
-	Ya = get_Y(hexID_a);
-	Xb = get_X(hexID_b);
-	Yb = get_Y(hexID_b);
-	
-	cartisian_X = math.cos(math.radians(30)) * 2.0;
-
-	real_Xa = cartisian_X * Xa;
-	real_Ya = Ya*2;
-	if Xa%2:
-		real_Ya = real_Ya+1;
-
-	real_Xb = cartisian_X * Xb;
-	real_Yb = Yb*2;
-	if Xb%2:
-		real_Yb = real_Yb+1;
-
-	X = real_Xb - real_Xa;
-	Y = real_Yb - real_Ya;
-	
-	if X==0:
-		if Y<0:
-			return 90;
-		return 270;
-	
-	angle = (math.atan(Y/X)*180.0)/math.pi;
-	angle = round(angle, ndigits=2);
-	if X<0 and Y<0:
-		return 180 - angle;
-	if X<0 and Y>=0:
-		return abs(angle - 180);
-	if X>=0 and Y>=0:
-		return 360 - angle;
-
-	return abs(angle);
 
 # ****************************************************************************************
 def put_dot(hexDot):
 	# insert into gameData (scenario, name, j) values
 	# ('01-Day8', 'yellowDot', '{"hexLoc": [15,15]}' );
-	X = int(hexDot[0:2])
-	Y = int(hexDot[2:])
-	print("...", X, Y)
+	X = hexDot[0]
+	Y = hexDot[1]
 	stmt = "insert into gameData (scenario, name, j) values " \
 		+ "('" + scenario + "', 'yellowDot', '{\"hexLoc\": [" + str(X) + "," \
 		+ str(Y) + "]}' )"
 	sql.sql(stmt)
 
 def create_path(hexID_a, hexID_b):
-	if re.search("^[A-Za-z]", hexID_a):
-		hexID_a = conver2number(hexID_a)
-	
-	if re.search("^[A-Za-z]", hexID_b):
-		hexID_b = conver2number(hexID_b)
-	
-	direction = "";
-
-	while True:
-		if hexID_a == hexID_b:
-			return;
-
-		angle = get_angle(hexID_a, hexID_b);
-		if angle>0 and angle<=60:
-			direction = "B";
-		if angle>60 and angle<=120:
-			direction = "A";
-		if angle>120 and angle<=180:
-			direction = "F";
-		if angle>180 and angle<=240:
-			direction = "E";
-		if angle>240 and angle<=300:
-			direction = "D";
-		if angle>300:
-			direction = "C";
-		hexID_a = stepDir(hexID_a, direction);
-		put_dot(hexID_a);
+	hexLoc_a = hx.convert_id2loc(hexID_a)
+	hexLoc_b = hx.convert_id2loc(hexID_b)
+	path = hx.get_path(hexLoc_a, hexLoc_b)
+	for x in path:
+		#print(x)
+		put_dot(x)
 
 def remove_redundance(pts):
 	pts = re.split(",", pts)
@@ -167,7 +42,7 @@ def create_paths(starting, pts):
 	pts = starting + "," + re.sub("\s", "", pts)
 	pts = remove_redundance(pts)
 	pts = re.split(",", pts)
-	print(pts, len(pts))
+	#print(pts, len(pts))
 	for x in range(len(pts)-1):
 		create_path(pts[x], pts[x+1])
 
@@ -175,8 +50,8 @@ def create_paths(starting, pts):
 stmt = "delete from gameData where name = 'yellowDot' and html is null"
 sql.sql(stmt)
 
-starting = "A1   "
-points = "A1,D19,L1,M10,S2"
+starting = "   "
+points = "A1,CC5,O10"
 scenario = sql.get_current_scenario()
 create_paths(starting, points)
 
