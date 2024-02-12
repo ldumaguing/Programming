08291862fdf490539285ee996a0e8191d798c3f1
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-02-11 19:11:26 -0500

    Add files via upload

--- Python3/Nuklear Winter 68/py/game/commands.py
+++ Python3/Nuklear Winter 68/py/game/commands.py
@@ -23,7 +23,10 @@ def move(subject, obj, scenario):
 	if obj=="F":
 		n = hx.get_neighbor(currLoc, "F")
 
-	if mek.not_enough_movement(subject, obj, scenario):
+	#print("n:", n)
+	hexAhead = hx.convert_loc2id(n)
+
+	if mek.not_enough_movement(subject, obj, scenario, hexAhead):
 		print("***** Not enough movement points.")
 		return
 
--- Python3/Nuklear Winter 68/py/game/mechanics.py
+++ Python3/Nuklear Winter 68/py/game/mechanics.py
@@ -4,13 +4,21 @@ import mariadb as sql
 import hexagon as hx
 
 def is_on_hill(subject, scenario):
-	print(">>>", get_hexID(subject, scenario))
 	fields = "flags & (1<<1)"
 	conditions = "hexID = '" + get_hexID(subject, scenario) + "'"
 	table = "map"
-	if sql.select_one(fields, conditions, table)[0]:
-		return True
-	return False
+	if sql.select_one(fields, conditions, table)==None:
+		return False
+	return True
+
+def is_hill(hexID):
+	fields = "flags & (1<<1)"
+	conditions = "hexID = '" + hexID + "'"
+	table = "map"
+	if sql.select_one(fields, conditions, table)==None:
+		return False
+	return True
+
 
 def get_flip_status(subject, scenario):
 	fields = "JSON_VALUE(j, '$.flip')"
@@ -38,16 +46,17 @@ def get_hexID(subject, scenario):
 	return hx.convert_loc2id((sql.select_one(fields, conditions, table)[0],
 		sql.select_one(fields, conditions, table)[1]))
 
-def get_terrain_cost(hexID, scenario, subject):
+def get_terrain_cost(hexID, scenario, subject, hexAhead):
+	print("terrain cost")
 	fields = "flags"
-	conditions = "hexID = '" + hexID + "'"
+	conditions = "hexID = '" + hexAhead + "'"
 	table = "map"
 	r = sql.select_one(fields, conditions, table)
 	if r==None:
 		return 1
 	flags = r[0]
-	cost = 0
-	print("...........", is_on_hill(subject, scenario))
+	cost = 1
+
 
 	if sql.is_infantry(subject):
 		if flags & (1<<0):   # crop fields
@@ -63,8 +72,16 @@ def get_terrain_cost(hexID, scenario, subject):
 			cost += 2
 		if flags & (1<<6):   # woods
 			cost += 2
-	if flags & (1<<1):   # hill
+
+	#print("...........", is_on_hill(subject, scenario))
+	#print("...........", is_hill(hexAhead))
+	onHill = is_on_hill(subject, scenario)
+	hillAhead = is_hill(hexAhead)
+
+	if hillAhead & (not onHill):
+		print("pay hill")
 		cost += 1
+
 	if flags & (1<<2):   # sand
 		cost += 2
 	if flags & (1<<3):   # shellholes
@@ -74,14 +91,14 @@ def get_terrain_cost(hexID, scenario, subject):
 
 	return cost
 
-def not_enough_movement(subject, obj, scenario):
+def not_enough_movement(subject, obj, scenario, hexAhead):
 	# get current movement points
 	print(get_flip_status(subject, scenario))
 	movement = get_movement(subject, scenario,
 		get_flip_status(subject, scenario))
 
-	print(get_hexID(subject, scenario), "<<<")
-	print(      get_terrain_cost(get_hexID(subject, scenario), scenario, subject)                        )
+	print(get_hexID(subject, scenario), hexAhead, "<<<")
+	print(      get_terrain_cost(get_hexID(subject, scenario), scenario, subject, hexAhead)                        )
 	return True
 
 
