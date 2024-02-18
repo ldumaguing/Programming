3bf992b57ff6a3d5cc7049e6ed1772e1acdba473
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-02-17 22:09:09 -0500

    Add files via upload

---
+++ Python3/Nuklear Winter 68/py/list_units.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import mariadb as my
+import hexagon as hx
+
+# ****************************************************************************************
+scenario = my.get_current_scenario()
+
+stmt = "select name, val, json_query(j, '$.hexLoc') as hex from gameData" \
+	+ " where name regexp '^u' and scenario = '" + scenario \
+	+ "' order by val"
+print("********************************************************************************")
+
+results = my.sql_fetchall(stmt)
+
+for x in results:
+	#y = x[2]
+	#print(y.replace('[','').replace(']','').split(',')   )
+	#print(x[0], x[1], hx.convert_loc2id(x[2]))
+	y = x[2].replace('[','').replace(']','').split(',')
+	if int(y[0])<0:
+		print(x[0], "[]", x[1].replace(';',' --- '))
+	else:
+		print(x[0], "["+hx.convert_loc2id(y)+"]", x[1].replace(';',' --- '))
+
+print()
+
+
+
+
+
+
+
+
