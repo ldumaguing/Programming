3504be800deee179f0554a0bf32c9ef0f42c51f0
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-03-02 23:03:20 -0500

    Update foo.py

--- Python3/Nuklear Winter 68/py/foo.py
+++ Python3/Nuklear Winter 68/py/foo.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
-print(128^128)
-print(128&121)
-print(128|121)
-print(128^121)
+# slot = map0.get_slot(riverStat, direction, rivPlace)
+
+rivStat = 2129920  # destination hex
+direction = "C"    # unit's desired edge exit
+rivPlace = 8192    # unit's current river-place
+
+
+
+def get_slot(rivStat, direction, rivPlace):
+	pass
+
+
+
+
+get_slot(rivStat, direction, rivPlace)
+
+
 
