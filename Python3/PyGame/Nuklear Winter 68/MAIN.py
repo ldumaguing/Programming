f588c2a27ff8cd09bba328b9e5638f0e22625091
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-04-04 15:40:38 -0400

    Add files via upload

--- Python3/PyGame/Nuklear Winter 68/MAIN.py
+++ Python3/PyGame/Nuklear Winter 68/MAIN.py
@@ -6,6 +6,8 @@ scrn_w = 0
 scrn_h = 0
 screen = None
 running = True
+board_map_w = 0
+board_map_h = 0
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -14,6 +16,8 @@ f = open('config.json')
 data = json.load(f)
 scrn_w = data["screen_w"]
 scrn_h = data["screen_h"]
+board_map_w = data["board_map_w"]
+board_map_h = data["board_map_h"]
 
 board_map = pygame.image.load(data["board_map"])
 
@@ -29,17 +33,22 @@ f.close()
 # ****************************************************************************************
 drag_x = 0
 drag_y = 0
-drag_spd = 1
+drag_spd = 25
+zoom = 100
 while running:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+		if event.type == pygame.QUIT: running = False
+
+		if event.type == pygame.MOUSEWHEEL:
+			if event.y > 0: zoom += 5
+			else: zoom -= 5
+		if zoom < 5: zoom = 5
 
 	keys = pygame.key.get_pressed()
 	if keys[pygame.K_RSHIFT]:
-		drag_spd = 20
-	else:
 		drag_spd = 1
+	else:
+		drag_spd = 25
 	if keys[pygame.K_a]:
 		drag_x += 1*drag_spd
 	if keys[pygame.K_d]:
@@ -49,17 +58,24 @@ while running:
 	if keys[pygame.K_s]:
 		drag_y -= 1*drag_spd
 
+	if keys[pygame.K_LCTRL]:
+		if keys[pygame.K_LALT]:
+			if keys[pygame.K_q]:
+				running = False
 
+	if keys[pygame.K_q]:
+		drag_x = 0
+		drag_y = 0
+	if keys[pygame.K_e]:
+		zoom = 100
 
-
-
-
-
-
-	screen.blit(board_map, (drag_x, drag_y))
+	z = zoom / 100.0
+	screen.fill("purple")
+	fish = pygame.transform.scale(board_map, (board_map_w*z, board_map_h*z))
+	screen.blit(fish, (drag_x, drag_y))
 
 	pygame.display.flip()
-	clock.tick(25)
+	clock.tick(24)
 
 
 
