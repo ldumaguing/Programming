33238c8545032d505a6de6214b1842e5d618aa88
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-05-16 13:04:17 -0400

    Add files via upload

---
+++ vice/test2.s
@@ -0,0 +1,8 @@
+	lda #2
+	sta 53280
+	lda #0
+	sta 53281
+	lda #1
+	sta 646
+	rts
+
--- vice/test3.s
+++ vice/test3.s
@@ -2,6 +2,10 @@ chrout	= $ffd2
 
 	ldx #0
 loop:
+	lda #2
+	sta 53280
+	lda #0
+	sta 53281
 	lda text,x
 	beq done
 	jsr chrout
