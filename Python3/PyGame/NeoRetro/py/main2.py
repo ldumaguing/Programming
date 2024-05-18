3bb75ac6f2572c20ea040d3aa5117a1baaec50fe
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-05-17 20:33:35 -0400

    Update main2.py

--- Python3/PyGame/NeoRetro/py/main2.py
+++ Python3/PyGame/NeoRetro/py/main2.py
@@ -7,13 +7,15 @@ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 screen_dim = screen.get_size()
 
 # Draw stuff on the LCD surface
-lcd_dim = (320, 200)
+lcd_dim = (320, 240)
 lcd_surface = pygame.Surface(lcd_dim)
 lcd_surface.fill((255, 255, 255))
-pygame.draw.rect(lcd_surface, (255, 0, 255), pygame.Rect(50, 50, 269, 20))
+pygame.draw.rect(lcd_surface, (128, 128, 128), pygame.Rect(0, 0, 10, 10))
+pygame.draw.rect(lcd_surface, (255, 0, 255), pygame.Rect(1, 1, 10, 10))
+pygame.draw.rect(lcd_surface, (0, 0, 255), pygame.Rect(2, 2, 10, 10))
 
 scale = min(screen_dim[0]/lcd_dim[0], screen_dim[1]/lcd_dim[1])
-scale *= .85
+scale *= .98
 
 scale_lcd_dim = (lcd_dim[0]*scale, lcd_dim[1]*scale)
 
@@ -23,13 +25,14 @@ pygame.transform.scale(
 	scale_lcd_dim,
 	scale_surface)
 
-#x = screen_dim[0] - (scale_lcd_dim[0])
-#x = x - (x/2)
-x = (screen_dim[0] - scale_lcd_dim[0]) - ((screen_dim[0] - scale_lcd_dim[0])/2)
-y = (screen_dim[1] - scale_lcd_dim[1]) - ((screen_dim[1] - scale_lcd_dim[1])/2)
+
+upper_left_loc = (
+	(screen_dim[0] - scale_lcd_dim[0]) - ((screen_dim[0] - scale_lcd_dim[0])/2),
+	(screen_dim[1] - scale_lcd_dim[1]) - ((screen_dim[1] - scale_lcd_dim[1])/2)
+	)
 screen.blit(
 	scale_surface,
-	(x,y)
+	upper_left_loc
 	)
 
 pygame.display.flip()
