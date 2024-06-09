1006f1e3af885a29d7813872954a6c17741d97f9
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-06-08 22:33:46 -0400

    2024.06a

--- Python3/PyGame/Ukraine43/db/foo.py
+++ Python3/PyGame/Ukraine43/db/foo.py
@@ -1,4 +1,27 @@
 import sqlite3
+a1 = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Nuklear Winter '68</title>
+<!-- <meta http-equiv="refresh" content="3"> -->
+</head>
+<body onload="">
+"""
+
+a2 = """
+</body>
+</html>
+"""
+
+b1 = "../../../../../IMAGES/Ukraine43/"
+
+
+
+print(a1)
+
+
+
 
 con = sqlite3.connect("ukraine43.sqlite")
 cur = con.cursor()
@@ -7,6 +30,6 @@ cur = con.cursor()
 # print(res.fetchone())
 
 for row in cur.execute("SELECT * from ukraine43"):
-	print(row)
+	print("<img id=\"i" + str(row[0]) + "\" src=\"" + b1 + row[1] + "\"><br>")
 
-
+print(a2)
