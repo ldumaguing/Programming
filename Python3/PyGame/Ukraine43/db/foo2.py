2dc00c52f19cf2f064cd1c694a00b70fa2686a45
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-06-12 16:40:00 -0400

    Add files via upload

--- Python3/PyGame/Ukraine43/db/foo2.py
+++ Python3/PyGame/Ukraine43/db/foo2.py
@@ -31,8 +31,10 @@ cur1 = con.cursor()
 # res = cur.execute("SELECT * from ukraine43")
 # print(res.fetchone())
 
-for row in cur.execute("SELECT * from chits where flags & 1 order by id, name"):
-# for row in cur.execute("SELECT * from chits where (flags & 1) = 0 order by id, name"):
+for row in cur.execute("SELECT * from chits order by id, name"):
+# for row in cur.execute("SELECT * from chits where flags = 1 order by unitID, name"):   # non-mech
+# for row in cur.execute("SELECT * from chits where flags = 3 order by id, name"):
+# for row in cur.execute("SELECT * from chits where flags = 0 order by unitID, name"):
 	#print("<img id=\"i" + str(row[0]) + "\" src=\"" + b1 + row[1] + "\">" + str(row[0]) + "; " + row[1] + "<br>")
 	front = blank_img
 	rear = blank_img
@@ -52,7 +54,17 @@ for row in cur.execute("SELECT * from chits where flags & 1 order by id, name"):
 	print('<img src="' + front + '">')
 	print('<img src="' + rear + '">')
 	print('<img src="' + rem + '">')
-	print(row[1], "---", row[0])
+	# print(row[1], "---", row[0], ';', row[5])
+	print(row[0], ';', row[5], ';')
+	if ((row[7]^3)>>2)==1: print("Soviet Regular Army and Air Force")
+	if ((row[7]^3)>>2)==2: print("Soviet Guards")
+	if ((row[7]^3)>>2)==3: print("German Wehrmacht")
+	if ((row[7]^3)>>2)==4: print("German SS")
+	if ((row[7]^3)>>2)==5: print("German Luftwaffe")
+	if ((row[7]^3)>>2)==6: print("Hungarian")
+	if ((row[7]^3)>>2)==7: print("Romanian")
+	if ((row[7]^3)>>2)==8: print("Slovakian")
+	if ((row[7]^3)>>2)==9: print("Czechoslovakian")
 	print('<br>')
 	#print(front, rear)
 
