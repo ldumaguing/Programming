b32824d85d548fdafae1c6ee5c2df4d60653be0c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-07-24 12:43:49 -0400

    Add files via upload

--- Python3/Test_00/codes/reset.py
+++ Python3/Test_00/codes/reset.py
@@ -1,2 +1,11 @@
-print("yo from reset.py")
+import sqlite3 as sql
 
+print("yo from reset.py")
+print("yo from reset.py 2")
+
+conn = sql.connect("./Test_00.db")
+curs = conn.cursor()
+rows = curs.execute("SELECT * from units").fetchall()
+# print(rows)
+for row in rows:
+	print(row)
