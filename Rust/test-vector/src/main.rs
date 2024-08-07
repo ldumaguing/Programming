cb71536685aab8a28b3fc5c062a9eaceb2a363d0
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-08-07 01:30:09 -0400

    2024.08a

---
+++ Rust/test-vector/src/main.rs
@@ -0,0 +1,20 @@
+struct HexID(i32, i32);
+
+fn main() {
+    let mut v: Vec<HexID> = Vec::new();
+    v.push(HexID(0, 0));
+    v.push(HexID(1, 1));
+    v.push(HexID(2, 2));
+    println!("> push( HexID(0, 0) )");
+    println!("> push( HexID(1, 1) )");
+    println!("> push( HexID(2, 2) )");
+
+    // let x: &HexID = &v[1];
+    let x: &HexID = v.get(10).expect("***** Out of range. *****");
+    println!("> ({}, {})", x.0, x.1);
+    println!("> {} len", v.len());
+    v.pop();
+    println!("> pop");
+    println!("> {} len", v.len());
+    println!("> LIFO; i.e., HexID(2, 2) is gone.");
+}
