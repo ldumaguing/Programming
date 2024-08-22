dc022bc995c2f7270e84d89c54cec23a54cdca35
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-08-22 15:23:22 -0400

    Add files via upload

---
+++ Wretched/wretched.sql
@@ -0,0 +1,34 @@
+drop table if exists species;
+create table species (
+	id       integer primary key autoincrement,
+	name     text,
+	descript text
+);
+
+insert into species values
+(NULL, 'Adityan', 'Their history is profoundly connected to humanity''s past. If you are using the <b><i>Wretched Bastards</i></b> campaign setting, they were the creators of the Human race.'),
+(NULL, 'Android', 'These characters originated from diverse ways: Some were assembled, others cultured in laboratories, or forged through sophisticated blends of synthetic and organic elements.<p>There are various types of androids and bioroids, each with its unique features and specialised functions.'),
+(NULL, 'Bioroid', 'These characters originated from diverse ways: Some were assembled, others cultured in laboratories, or forged through sophisticated blends of synthetic and organic elements.<p>There are various types of androids and bioroids, each with its unique features and specialised functions.'),
+(NULL, 'Children of the Night', 'The Children of the Night are bizarre creatures living in underground cities or far enough from inhabited places to keep their existence hidden.<p>Their origins are still unknown, though they are believed to be hybrids, the result of humans mating with denizens of the Beyond. Children of the Night, are mutants and will acquire changes as they advance in level. See the mutation tables below.'),
+(NULL, 'Clone', 'These characters originated from diverse ways: Some were assembled, others cultured in laboratories, or forged through sophisticated blends of synthetic and organic elements.<p>There are various types of androids and bioroids, each with its unique features and specialised functions.'),
+(NULL, 'Deva', 'The Deva go by many names (Angels, Amesha Spenta, Ahura, Seraphim, Sabahot, Apsara; the Deva, in Vedic religion, are an acting power that manifests itself in natural and mental phenomena).<p>However, Deva, just like the other designations, are just labels given to these creatures by mortals, that have no relation to their true origin. The truth is Deva are minor entities from the Beyond. They are physical beings, seemingly human, with high charisma and handsome looks. Just like any denizen of the Beyond, they are not inherently good or evil.'),
+(NULL, 'Dolphar', ''),
+(NULL, 'Dwarf', ''),
+(NULL, 'Elf', ''),
+(NULL, 'Gnome', ''),
+(NULL, 'Gustora', ''),
+(NULL, 'Half-Elf', ''),
+(NULL, 'Half-Orc', ''),
+(NULL, 'Human', ''),
+(NULL, 'Incubus', ''),
+(NULL, 'Jegarni', ''),
+(NULL, 'Klarish', ''),
+(NULL, 'Loup-Garou', ''),
+(NULL, 'Mutants', ''),
+(NULL, 'Pherat', ''),
+(NULL, 'Seraphani', ''),
+(NULL, 'Succubus', ''),
+(NULL, 'Tzangi', ''),
+(NULL, 'Wampyr', '');
+
+
