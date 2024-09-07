f5e5bdeb156f429873e74117a56e6c893e0c854c
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-09-07 13:56:25 -0400

    2024.09a

---
+++ Rust/test-Box/src/main.rs
@@ -0,0 +1,17 @@
+trait Vehicle {
+    fn drive(&self);
+}
+
+struct Truck;
+
+impl Vehicle for Truck {
+    fn drive(&self) {
+        println!("Truck is driving");
+    }
+}
+
+fn main() {
+    let t: Box<dyn Vehicle>; // seems this defines a pointer
+    t = Box::new(Truck); // this assigns the pointer to point to a heap
+    t.drive();
+}
