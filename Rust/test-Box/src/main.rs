716dd92eee3e0d32f06d67cef0e6876768dd9430
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-09-10 11:37:26 -0400

    Update main.rs

--- Rust/test-Box/src/main.rs
+++ Rust/test-Box/src/main.rs
@@ -1,3 +1,40 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+/* ***********************************************************************
+fn main() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+*/
+
+/* ***********************************************************************
 trait Vehicle {
     fn drive(&self);
 }
@@ -15,3 +52,4 @@ fn main() {
     t = Box::new(Truck); // this assigns the pointer to point to a heap
     t.drive();
 }
+*/
