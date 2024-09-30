b684b5168a9e1e46c82c1a28c4f61f953df3cb1f
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-09-30 15:22:26 -0400

    Update joystick.rs

--- Rust/SDL2/rusteroids/tutorial19-mine/src/joystick.rs
+++ Rust/SDL2/rusteroids/tutorial19-mine/src/joystick.rs
@@ -1,3 +1,4 @@
+// ***** tutorial19-mine - joystick
 use sdl2::controller::Button;
 
 pub fn button_down(joystick_manager: &mut u16, button: Button) {
@@ -23,21 +24,22 @@ pub fn button_down(joystick_manager: &mut u16, button: Button) {
 
 pub fn button_up(joystick_manager: &mut u16, button: Button) {
     match button {
-        Button::DPadUp => *joystick_manager ^= 1 << 0,
-        Button::DPadDown => *joystick_manager ^= 1 << 1,
-        Button::DPadLeft => *joystick_manager ^= 1 << 2,
-        Button::DPadRight => *joystick_manager ^= 1 << 3,
-        Button::A => *joystick_manager ^= 1 << 4,
-        Button::B => *joystick_manager ^= 1 << 5,
-        Button::X => *joystick_manager ^= 1 << 6,
-        Button::Y => *joystick_manager ^= 1 << 7,
-        Button::Back => *joystick_manager ^= 1 << 8,
-        Button::Guide => *joystick_manager ^= 1 << 9,
-        Button::Start => *joystick_manager ^= 1 << 10,
-        Button::LeftStick => *joystick_manager ^= 1 << 11,
-        Button::RightStick => *joystick_manager ^= 1 << 12,
-        Button::LeftShoulder => *joystick_manager ^= 1 << 13,
-        Button::RightShoulder => *joystick_manager ^= 1 << 14,
+        Button::DPadUp => *joystick_manager &= !(1 << 0),
+        Button::DPadDown => *joystick_manager &= !(1 << 1),
+        Button::DPadLeft => *joystick_manager &= !(1 << 2),
+        Button::DPadRight => *joystick_manager &= !(1 << 3),
+        Button::A => *joystick_manager &= !(1 << 4),
+        Button::B => *joystick_manager &= !(1 << 5),
+        Button::X => *joystick_manager &= !(1 << 6),
+        Button::Y => *joystick_manager &= !(1 << 7),
+        Button::Back => *joystick_manager &= !(1 << 8),
+        Button::Guide => *joystick_manager &= !(1 << 9),
+        Button::Start => *joystick_manager &= !(1 << 10),
+        Button::LeftStick => *joystick_manager &= !(1 << 11),
+        Button::RightStick => *joystick_manager &= !(1 << 12),
+        Button::LeftShoulder => *joystick_manager &= !(1 << 13),
+        Button::RightShoulder => *joystick_manager &= !(1 << 14),
+
         _ => {}
     }
 }
