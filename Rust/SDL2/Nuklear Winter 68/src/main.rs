a20384368c8bb4c4bb4568b12241836e5186ee17
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-10-07 13:21:41 -0400

    Update main.rs

--- Rust/SDL2/Nuklear Winter 68/src/main.rs
+++ Rust/SDL2/Nuklear Winter 68/src/main.rs
@@ -18,10 +18,25 @@ struct State {
     ecs: World,
 }
 
-pub const SCREEN_WIDTH: u32 = 800;
-pub const SCREEN_HEIGHT: u32 = 600;
+// 2048x1152
+
+// ***** 720p
+pub const SCREEN_WIDTH: u32 = 1280;
+pub const SCREEN_HEIGHT: u32 = 720;
+
+// ***** Board Map
+pub const MAP_DIM: (u32, u32) = (6372, 4139);
+pub const HEX_0X0: (u32, u32) = (367, 215);
+const X_HEX_COUNT: i32 = 28;
+const Y_HEX_COUNT: i32 = 19;
+const HEX_LOW_RIGHT: (u32, u32) = (5095, 3920);
+pub const HEXAGON: (f32, f32) = (
+    (HEX_LOW_RIGHT.0 as f32 - HEX_0X0.0 as f32) / X_HEX_COUNT as f32,
+    (HEX_LOW_RIGHT.1 as f32 - HEX_0X0.1 as f32) / Y_HEX_COUNT as f32,
+);
 
 fn main() -> Result<(), String> {
+    println!("({}, {})", HEXAGON.0 * 10.0, HEXAGON.1 * 10.0);
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
