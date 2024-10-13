fa0a31446aac126db87b2004a6b18b75cfb06598
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-10-12 23:36:13 -0400

    2024.10a

--- Rust/SDL2/Nuklear Winter 68/src/game.rs
+++ Rust/SDL2/Nuklear Winter 68/src/game.rs
@@ -62,6 +62,11 @@ pub fn update(joystick_manager: &mut u16, gv: &mut GlobalVariables) {
         }
         if gv.cursor_loc.0 < 0 {
             gv.cursor_loc.0 = 0;
+            if !crate::joystick::is_button_pressed(joystick_manager, &Button::RightStick) {
+                gv.map_loc.0 -= 25;
+            } else {
+                gv.map_loc.0 -= 1;
+            }
         }
         if gv.cursor_loc.1 < 0 {
             gv.cursor_loc.1 = 0;
@@ -69,12 +74,35 @@ pub fn update(joystick_manager: &mut u16, gv: &mut GlobalVariables) {
         let x: i32 = SCREEN_DIM.0 as i32 - gv.cursor_dim.0 as i32;
         if gv.cursor_loc.0 > x {
             gv.cursor_loc.0 = x;
+            if !crate::joystick::is_button_pressed(joystick_manager, &Button::RightStick) {
+                gv.map_loc.0 += 25;
+            } else {
+                gv.map_loc.0 += 1;
+            }
         }
         let y: i32 = SCREEN_DIM.1 as i32 - gv.cursor_dim.1 as i32;
         if gv.cursor_loc.1 > y {
             gv.cursor_loc.1 = y;
         }
-    } else {
+        // ******************************
+        if gv.map_loc.0 < 0 {
+            gv.map_loc.0 = 0;
+        }
+        if gv.map_loc.1 < 0 {
+            gv.map_loc.1 = 0;
+        }
+        let x: i32 = gv.map_loc.0 + gv.map_screen_dim.0 as i32;
+        if MAP_DIM.0 < x as u32 {
+            gv.map_loc.0 = MAP_DIM.0 as i32 - gv.map_screen_dim.0 as i32;
+        }
+        let y: i32 = gv.map_loc.1 + gv.map_screen_dim.1 as i32;
+        if MAP_DIM.1 < y as u32 {
+            gv.map_loc.1 = MAP_DIM.1 as i32 - gv.map_screen_dim.1 as i32;
+        }
+    }
+
+    /*
+     else {
         // ***** map mode
         if !crate::joystick::is_button_pressed(joystick_manager, &Button::RightStick) {
             if crate::joystick::is_button_pressed(joystick_manager, &Button::DPadRight) {
@@ -118,4 +146,5 @@ pub fn update(joystick_manager: &mut u16, gv: &mut GlobalVariables) {
             gv.map_loc.1 = MAP_DIM.1 as i32 - gv.map_screen_dim.1 as i32;
         }
     }
+    */
 }
