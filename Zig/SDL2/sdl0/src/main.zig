2707c45de17bb74a43815e7428e7784c08e94194
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-10-21 01:11:34 -0400

    2024.10b

---
+++ Zig/SDL2/sdl0/src/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+// const SDL = @import("SDL2"); // Add this package by using sdk.getNativeModule
+const SDL = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
+pub fn main() !void {
+    if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_EVENTS | SDL.SDL_INIT_AUDIO) < 0)
+        sdlPanic();
+    defer SDL.SDL_Quit();
+
+    const window = SDL.SDL_CreateWindow(
+        "SDL2 Native Demo",
+        SDL.SDL_WINDOWPOS_CENTERED,
+        SDL.SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL.SDL_WINDOW_SHOWN,
+    ) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyWindow(window);
+
+    const renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RENDERER_ACCELERATED) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyRenderer(renderer);
+
+    mainLoop: while (true) {
+        var ev: SDL.SDL_Event = undefined;
+        while (SDL.SDL_PollEvent(&ev) != 0) {
+            if (ev.type == SDL.SDL_QUIT)
+                break :mainLoop;
+        }
+
+        _ = SDL.SDL_SetRenderDrawColor(renderer, 0xF7, 0xA4, 0x1D, 0xFF);
+        _ = SDL.SDL_RenderClear(renderer);
+
+        SDL.SDL_RenderPresent(renderer);
+    }
+}
+
+fn sdlPanic() noreturn {
+    const str = @as(?[*:0]const u8, SDL.SDL_GetError()) orelse "unknown error";
+    @panic(std.mem.sliceTo(str, 0));
+}
