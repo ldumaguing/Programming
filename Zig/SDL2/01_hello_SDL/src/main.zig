7005e589a4798dfd3dba0806d4bef32f2e53469f
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-10-26 21:47:56 -0400

    2024.10b

--- Zig/SDL2/01_hello_SDL/src/main.zig
+++ Zig/SDL2/01_hello_SDL/src/main.zig
@@ -9,7 +9,7 @@ pub fn main() !void {
     // ********** init SDL
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
         sdlPanic();
-    defer SDL.SDL_Quit();
+    SDL.SDL_Quit();
 
     // ********** create Window
     const window = SDL.SDL_CreateWindow(
@@ -20,17 +20,12 @@ pub fn main() !void {
         WINDOW_DIM[1],
         SDL.SDL_WINDOW_SHOWN,
     ) orelse sdlPanic();
-    defer _ = SDL.SDL_DestroyWindow(window);
-
-    // ********** create Renderer
-    const renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RENDERER_ACCELERATED) orelse sdlPanic();
-    defer _ = SDL.SDL_DestroyRenderer(renderer);
 
     // ********** The game loop
     mainLoop: while (true) {
         const screenSurface = SDL.SDL_GetWindowSurface(window);
-        defer _ = SDL.SDL_FillRect(screenSurface, null, SDL.SDL_MapRGB(screenSurface.*.format, 255, 0, 255));
-        defer _ = SDL.SDL_UpdateWindowSurface(window);
+        _ = SDL.SDL_FillRect(screenSurface, null, SDL.SDL_MapRGB(screenSurface.*.format, 255, 0, 255));
+        _ = SDL.SDL_UpdateWindowSurface(window);
 
         var ev: SDL.SDL_Event = undefined;
         while (SDL.SDL_PollEvent(&ev) != 0) {
