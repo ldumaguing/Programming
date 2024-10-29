1dd962a441087d86a26977bc60c031ab7fc816f5
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2024-10-29 12:42:45 -0400

    Update main.zig

--- Zig/test_struct/src/main.zig
+++ Zig/test_struct/src/main.zig
@@ -5,6 +5,13 @@ const Texture = struct {
     img_w: i32,
     img_h: i32,
 
+    fn new(x: i32, y: i32) Texture { // note: no self attribute
+        return Texture{
+            .img_w = x,
+            .img_h = y,
+        };
+    }
+
     fn foo1(self: Texture) void {
         _ = self;
         print("foo1\n", .{});
@@ -12,13 +19,6 @@ const Texture = struct {
 
     fn foo2(self: Texture) void {
         print("foo2 {}\n", .{self.img_w});
-    }
-
-    fn new(x: i32, y: i32) Texture {
-        return Texture{
-            .img_w = x,
-            .img_h = y,
-        };
     }
 
     fn loadFromFile(self: Texture, imgFile: []const u8) void {
