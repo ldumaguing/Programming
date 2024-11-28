93262e584332a2fe177683172749da095b72d529
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-11-28 18:44:20 -0500

    2024.11b

--- Zig/test_struct/src/main.zig
+++ Zig/test_struct/src/main.zig
@@ -2,11 +2,13 @@ const std = @import("std");
 const print = @import("std").debug.print;
 
 const Texture = struct {
+    name: []const u8,
     img_w: i32,
     img_h: i32,
 
-    fn new(x: i32, y: i32) Texture { // note: no self attribute
+    fn new(x: i32, y: i32, name: []const u8) Texture { // note: no self attribute
         return Texture{
+            .name = name,
             .img_w = x,
             .img_h = y,
         };
@@ -21,6 +23,10 @@ const Texture = struct {
         print("foo2 {}\n", .{self.img_w});
     }
 
+    fn foo3(self: Texture) void {
+        print("foo3 {s}\n", .{self.name});
+    }
+
     fn loadFromFile(self: Texture, imgFile: []const u8) void {
         _ = self;
         print(">>> {s}\n", .{imgFile});
@@ -28,10 +34,11 @@ const Texture = struct {
 };
 
 pub fn main() !void {
-    const textureFish = Texture.new(640, 480);
+    const textureFish = Texture.new(640, 480, "blablabla");
     print("({}, {})\n", .{ textureFish.img_w, textureFish.img_h });
 
     textureFish.loadFromFile("fish");
     textureFish.foo1();
     textureFish.foo2();
+    textureFish.foo3();
 }
