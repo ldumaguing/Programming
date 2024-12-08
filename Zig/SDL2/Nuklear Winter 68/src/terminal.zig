19b89f3d5531777f978e5da187830cdd4445d024
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-08 02:35:18 -0500

    2024.12a

--- Zig/SDL2/Nuklear Winter 68/src/terminal.zig
+++ Zig/SDL2/Nuklear Winter 68/src/terminal.zig
@@ -12,20 +12,16 @@ const gv = @import("main.zig").GV;
 const Texture = @import("Texture.zig").Texture;
 
 pub fn doTerminal() void {
-    print("arrow: ({},{})\n", .{ mb.arrow.x, mb.arrow.y });
-    print("map: ({},{})\n", .{ mb.map_image.x, mb.map_image.y });
     const corner_0 = [_]f64{ @as(f64, @floatFromInt(mb.CORNER_0[0])) * @as(f64, mb.scale), @as(f64, @floatFromInt(mb.CORNER_0[1])) * @as(f64, mb.scale) };
-    print("corner_0: ({d:.6},{d:.6})\n", .{ corner_0[0], corner_0[1] });
     const hexagon_dim = [_]f64{ (mb.HEXAGON_DIM[0] * mb.scale), (mb.HEXAGON_DIM[1] * mb.scale) };
     print("hexagon_dim: ({d:.6},{d:.6})\n", .{ hexagon_dim[0], hexagon_dim[1] });
 
-    // const arrow_loc_x: i32 = mb.arrow.x - mb.map_image.x - mb.CORNER_0[0];
-    // const arrow_loc_y: i32 = mb.arrow.y - mb.map_image.y - mb.CORNER_0[1];
-    // print("arrow loc: ({},{})\n", .{ arrow_loc_x, arrow_loc_y });
-    // print("***** scale: {d:.3}\n", .{mb.scale});
-    // const fish: i32 = mb.arrow.x - @as(i32, @intFromFloat(corner_0[0])) - mb.map_image.x;
     const arrow_loc = [_]i32{ (mb.arrow.x - @as(i32, @intFromFloat(corner_0[0])) - mb.map_image.x), (mb.arrow.y - @as(i32, @intFromFloat(corner_0[1])) - mb.map_image.y) };
     print("arrow loc: ({},{})\n", .{ arrow_loc[0], arrow_loc[1] });
+
+    // const fish: i32 = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[0])) / hexagon_dim[0]);
+    const hex_id = [_]i32{ (@intFromFloat(@as(f64, @floatFromInt(arrow_loc[0])) / hexagon_dim[0])), (@intFromFloat(@as(f64, @floatFromInt(arrow_loc[1])) / hexagon_dim[1])) };
+    print("hex id: ({},{})\n", .{ hex_id[0], hex_id[1] });
     print("\n", .{});
     mb.terminal.render(10, 10, null, null, 0.0, null, SDL.SDL_FLIP_NONE);
 
