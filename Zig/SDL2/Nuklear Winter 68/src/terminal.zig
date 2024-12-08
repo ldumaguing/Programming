231901779f201a1d21f06da5cda1e849c1f538c3
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-08 15:18:43 -0500

    2024.12a

--- Zig/SDL2/Nuklear Winter 68/src/terminal.zig
+++ Zig/SDL2/Nuklear Winter 68/src/terminal.zig
@@ -14,24 +14,23 @@ const Texture = @import("Texture.zig").Texture;
 pub fn doTerminal() void {
     const corner_0 = [_]f64{ @as(f64, @floatFromInt(mb.CORNER_0[0])) * @as(f64, mb.scale), @as(f64, @floatFromInt(mb.CORNER_0[1])) * @as(f64, mb.scale) };
     const hexagon_dim = [_]f64{ (mb.HEXAGON_DIM[0] * mb.scale), (mb.HEXAGON_DIM[1] * mb.scale) };
-    print("hexagon_dim: ({d:.6},{d:.6})\n", .{ hexagon_dim[0], hexagon_dim[1] });
+    // print("hexagon_dim: ({d:.6},{d:.6})\n", .{ hexagon_dim[0], hexagon_dim[1] });
 
     const arrow_loc = [_]i32{ (mb.arrow.x - @as(i32, @intFromFloat(corner_0[0])) - mb.map_image.x), (mb.arrow.y - @as(i32, @intFromFloat(corner_0[1])) - mb.map_image.y) };
-    print("arrow loc: ({},{})\n", .{ arrow_loc[0], arrow_loc[1] });
+    // print("arrow loc: ({},{})\n", .{ arrow_loc[0], arrow_loc[1] });
 
-    // const fish: i32 = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[0])) / hexagon_dim[0]);
     const hex_id_x: i32 = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[0])) / hexagon_dim[0]);
     var hex_id_y: i32 = 0;
     if (@mod(hex_id_x, 2) == 0) {
         hex_id_y = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[1])) / hexagon_dim[1]);
     } else {
-        const fish: i32 = @intFromFloat(corner_0[1]);
-        hex_id_y = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[1] + fish)) / hexagon_dim[1]);
-        print("... {}\n", .{fish});
+        const corner_y: i32 = @intFromFloat(corner_0[1]);
+        hex_id_y = @intFromFloat(@as(f64, @floatFromInt(arrow_loc[1] + corner_y)) / hexagon_dim[1]);
+        hex_id_y -= 1;
     }
     const hex_id = [_]i32{ hex_id_x, hex_id_y };
     print("hex id: ({},{})\n", .{ hex_id[0], hex_id[1] });
-    print("\n", .{});
+    // print("\n", .{});
     mb.terminal.render(10, 10, null, null, 0.0, null, SDL.SDL_FLIP_NONE);
 
     var textColor: SDL.SDL_Color = undefined;
