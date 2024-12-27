84d04a55447c116046761689457ad6a718cd5197
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-27 14:34:42 -0500

    2024.12b

--- Zig/test_mvzr/src/main.zig
+++ Zig/test_mvzr/src/main.zig
@@ -5,14 +5,14 @@ const mvzr = @import("mvzr.zig");
 
 pub fn main() !void {
     //const re: mvzr.Regex = mvzr.compile("f").?;
-    const regex: mvzr.Regex = mvzr.compile("23").?;
+    const regex: mvzr.Regex = mvzr.compile("m[a-z]*23").?;
     // const match: mvzr.Match = re.match("fish 123").?;
     // const did_match: bool = fi.isMatch("fish 123");
 
     // std.debug.print(">{}<\n", .{did_match});
     var iter: mvzr.Regex.RegexIterator = regex.iterator("By convention, main23.zig is where your main fun23ction lives in the ca23se that");
     while (iter.next()) |m| {
-        std.log.err("{s}\n", .{m});
+        std.log.err(">{s}<\n", .{m});
     }
 }
 
