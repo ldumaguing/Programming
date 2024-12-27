d4c6527398bb2fcc91fb41513920d789fe843e48
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-27 18:30:06 -0500

    2024.12b

--- Zig/fooness/src/main.zig
+++ Zig/fooness/src/main.zig
@@ -1,45 +1,42 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("fooness_lib");
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const file = std.fs.cwd().openFile("test.script", .{}) catch |err| {
+        std.log.err("Failed to open file: {s}", .{@errorName(err)});
+        return;
+    };
+    defer file.close();
+
+    var mode: i32 = 0;
+    while (file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize)) catch |err| {
+        std.log.err("Failed to read line: {s}", .{@errorName(err)});
+        return;
+    }) |line| {
+        defer allocator.free(line);
+
+        if (std.mem.indexOf(u8, line, "***** chit placement")) |_| {
+            mode = 1;
+        }
+        if (std.mem.indexOf(u8, line, "***** END")) |_| {
+            mode = 0;
+        }
+
+        switch (mode) {
+            0 => {
+                return;
+            },
+            1 => {
+                placement(line);
+            },
+            else => {},
+        }
+    }
+}
+
+// ************************************************************************************************
+fn placement(line: []u8) void {
+    std.debug.print("{s}\n", .{line});
+}
