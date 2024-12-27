9b2224827b3fc98bd9f59f16bacce0bf7f40b1d5
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-26 22:33:19 -0500

    2024.12b

--- Zig/test_mvzr/src/main.zig
+++ Zig/test_mvzr/src/main.zig
@@ -2,15 +2,18 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 const mvzr = @import("mvzr.zig");
-const re = mvzr.Regex;
 
 pub fn main() !void {
-    // const re: mvzr.Regex = mvzr.compile("f").?;
-    const fi: re = mvzr.compile("f").?;
+    //const re: mvzr.Regex = mvzr.compile("f").?;
+    const regex: mvzr.Regex = mvzr.compile("23").?;
     // const match: mvzr.Match = re.match("fish 123").?;
-    const did_match: bool = fi.isMatch("fish 123");
+    // const did_match: bool = fi.isMatch("fish 123");
 
-    std.debug.print(">{}<\n", .{did_match});
+    // std.debug.print(">{}<\n", .{did_match});
+    var iter: mvzr.Regex.RegexIterator = regex.iterator("By convention, main23.zig is where your main fun23ction lives in the ca23se that");
+    while (iter.next()) |m| {
+        std.log.err("{s}\n", .{m});
+    }
 }
 
 const std = @import("std");
