98bfef11a642bf6372b3de7ccc4a93635518b44f
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-28 00:57:35 -0500

    2024.12b

--- Zig/fooness/src/main.zig
+++ Zig/fooness/src/main.zig
@@ -42,10 +42,8 @@ pub fn main() !void {
 
 // ************************************************************************************************
 fn placement(line: []u8, id_counter: i32) !void {
-    _ = id_counter;
+    var buf: [32]u8 = undefined;
     const stmt_a = "insert into chit_status (id, chit_ID, hex_ID, flags) values (";
-    //var chit_ID: []u8 = undefined;
-    //var hex_ID: []u8 = undefined;
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -54,35 +52,34 @@ fn placement(line: []u8, id_counter: i32) !void {
 
     const regex1: mvzr.Regex = mvzr.compile("^[0-9]+").?;
     if (regex1.isMatch(line)) {
+        const str_id_counter = try std.fmt.bufPrint(&buf, "{}", .{id_counter});
         const match: mvzr.Match = regex1.match(line).?;
-        // const fish = match.slice;
         try stmt.concat(stmt_a);
+        try stmt.concat(str_id_counter);
+        try stmt.concat(", ");
         try stmt.concat(match.slice);
         try stmt.concat(", ");
-        // chit_ID = try std.fmt.allocPrint(allocator, "{s}", .{match.slice});
-        // defer allocator.free(chit_ID);
-        // std.debug.print("...b: {s}{s}\n", .{ fish, stmt.str() });
     }
-    std.debug.print("...b: {s}\n", .{stmt.str()});
 
-    // std.debug.print("b: {s}, {s}\n", .{ line, chit_ID.ptr });
+    const regex2: mvzr.Regex = mvzr.compile("[A-Z]+[0-9]+").?;
+    if (regex2.isMatch(line)) {
+        const match: mvzr.Match = regex2.match(line).?;
+        try stmt.concat("'");
+        try stmt.concat(match.slice);
+        try stmt.concat("', ");
+    }
 
-    // var arena1 = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena1.deinit();
-    // const allocator1 = arena1.allocator();
+    const regex3: mvzr.Regex = mvzr.compile("[FB]$").?;
+    if (regex3.isMatch(line)) {
+        const match: mvzr.Match = regex3.match(line).?;
+        if (std.mem.eql(u8, match.slice, "F")) {
+            try stmt.concat("0)");
+        } else {
+            try stmt.concat("1)");
+        }
+    }
 
-    // const regex2: mvzr.Regex = mvzr.compile("[A-Z]+[0-9]+").?;
-    // if (regex2.isMatch(line)) {
-    //     const match: mvzr.Match = regex2.match(line).?;
-
-    //     stmt_c = try std.fmt.allocPrint(allocator1, "{s}, {s}", .{ stmt_b, match.slice });
-    //     std.debug.print(":::{s}\n", .{stmt_c});
-    //     defer allocator1.free(stmt_c);
-    //     std.debug.print("...{s}\n", .{stmt_c});
-    // }
-
-    // const match: mvzr.Match = regex.match("fish 123").?;
-    // std.debug.print("{s}\n", .{match.slice});
-    // const did_match: bool = regex.isMatch("123 fish");
-    // std.debug.print("{}\n", .{did_match});
+    if (stmt.len() > 20) {
+        std.debug.print("{s}\n", .{stmt.str()});
+    }
 }
