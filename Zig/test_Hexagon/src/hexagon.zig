19d6ea9045f9117f10d0a6e0f35fdf3bd858a619
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-30 15:04:36 -0500

    2024.12b

--- Zig/test_Hexagon/src/hexagon.zig
+++ Zig/test_Hexagon/src/hexagon.zig
@@ -11,16 +11,28 @@ pub const Hexagon = struct {
     y: i32,
     z: i32,
 
+    pub fn foo(self: Hexagon, hex: Hexagon) void {
+        _ = self;
+        std.debug.print("foo: {}\n", .{hex.x});
+    }
+
     pub fn new(id: []const u8, z: i32) Hexagon {
         var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
         defer arena.deinit();
         std.debug.print("id: {s}\n", .{id});
 
         var X: i32 = 0;
-        const regex1: mvzr.Regex = mvzr.compile("^[A-Z]").?;
-        if (regex1.isMatch(id)) {
-            const match: mvzr.Match = regex1.match(id).?;
-            X = @as(i32, match.slice[0]) - letter_A;
+        const regex3: mvzr.Regex = mvzr.compile("^[A-Z][A-Z]").?;
+        if (regex3.isMatch(id)) {
+            std.debug.print("here\n", .{});
+            const match: mvzr.Match = regex3.match(id).?;
+            X = @as(i32, match.slice[0]) - letter_A + 26;
+        } else {
+            const regex1: mvzr.Regex = mvzr.compile("^[A-Z]").?;
+            if (regex1.isMatch(id)) {
+                const match: mvzr.Match = regex1.match(id).?;
+                X = @as(i32, match.slice[0]) - letter_A;
+            }
         }
 
         var Y: i32 = 0;
--- Zig/test_Hexagon/src/main.zig
+++ Zig/test_Hexagon/src/main.zig
@@ -3,6 +3,9 @@ const std = @import("std");
 const hexagon = @import("hexagon.zig");
 
 pub fn main() !void {
-    const hex0 = hexagon.Hexagon.new("F3", 30);
+    const hex0 = hexagon.Hexagon.new("AA3", 0);
+    const hex1 = hexagon.Hexagon.new("G4", 0);
     std.debug.print("{s}, {}, {}, {}\n", .{ hex0.id, hex0.x, hex0.y, hex0.z });
+
+    hex0.foo(hex1);
 }
