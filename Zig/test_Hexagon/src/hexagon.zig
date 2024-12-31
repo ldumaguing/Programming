41817fec4a991a9091544b311ea09746e9b7f256
Lawrence Dumaguing <os2nlinux@gmail.com>
2024-12-30 19:44:18 -0500

    2024.12b

--- Zig/test_Hexagon/src/hexagon.zig
+++ Zig/test_Hexagon/src/hexagon.zig
@@ -1,7 +1,7 @@
 // ***** hexagon.zig
 const std = @import("std");
 const mvzr = @import("mvzr.zig");
-// const String = @import("zig-string.zig").String;
+const String = @import("zig-string.zig").String;
 
 const letter_A: u8 = 'A';
 
@@ -14,6 +14,35 @@ pub const Hexagon = struct {
     pub fn foo(self: Hexagon, hex: Hexagon) void {
         _ = self;
         std.debug.print("foo: {}\n", .{hex.x});
+    }
+
+    pub fn new_XYZ(x: i32, y: i32, z: i32) Hexagon {
+        // // Use your favorite allocator
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+
+        // // Create your String
+        var myString = String.init(arena.allocator());
+        defer myString.deinit();
+        myString.concat("123fXish123") catch undefined;
+
+        std.debug.print("{s}\n", .{myString.str()});
+
+        var array: [32]u8 = undefined;
+        array[0] = 'a';
+        array[1] = myString.str()[4];
+        array[2] = 'c';
+        array[3] = 'd';
+        array[4] = 0;
+        const slice = array[0..4 :0];
+        std.debug.print("{s}: {}\n", .{ slice, slice.len });
+
+        return Hexagon{
+            .id = slice,
+            .x = x,
+            .y = y,
+            .z = z,
+        };
     }
 
     pub fn new(id: []const u8, z: i32) Hexagon {
--- Zig/test_Hexagon/src/main.zig
+++ Zig/test_Hexagon/src/main.zig
@@ -5,7 +5,10 @@ const hexagon = @import("hexagon.zig");
 pub fn main() !void {
     const hex0 = hexagon.Hexagon.new("AA3", 0);
     const hex1 = hexagon.Hexagon.new("G4", 0);
+    var hex_foo: hexagon.Hexagon = undefined;
     std.debug.print("{s}, {}, {}, {}\n", .{ hex0.id, hex0.x, hex0.y, hex0.z });
-
     hex0.foo(hex1);
+
+    hex_foo = hexagon.Hexagon.new_XYZ(10, 13, 0);
+    std.debug.print("{s} --- ({}, {})\n", .{ hex_foo.id, hex_foo.x, hex_foo.y });
 }
