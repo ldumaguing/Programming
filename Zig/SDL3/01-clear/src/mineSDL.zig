8f3b76f560cecb940c29f41e7fbb44ee4bdcfb79
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-02-06 00:12:40 -0500

    2025.02a

--- Zig/SDL3/01-clear/src/mineSDL.zig
+++ Zig/SDL3/01-clear/src/mineSDL.zig
@@ -11,7 +11,14 @@ const c = @cImport({
 });
 
 pub fn AppIterate(renderer: *c.SDL_Renderer) !void {
-    try m.errify(c.SDL_SetRenderDrawColor(renderer, 0xff, 0x00, 0xff, 0xff));
+    //try m.errify(c.SDL_SetRenderDrawColor(renderer, 0xff, 0x00, 0xff, 0xff));
+    const now: f64 = @as(f64, @floatFromInt(c.SDL_GetTicks())) / 1000.0;
+
+    const red: f32 = 0.5 + 0.5 * c.SDL_sinf(@floatCast(now));
+    const green: f32 = 0.5 + 0.5 * c.SDL_sinf(@floatCast(now + c.SDL_PI_D * 2.0 / 3.0));
+    const blue: f32 = 0.5 + 0.5 * c.SDL_sinf(@floatCast(now + c.SDL_PI_D * 4.0 / 3.0));
+    try m.errify(c.SDL_SetRenderDrawColorFloat(renderer, red, green, blue, c.SDL_ALPHA_OPAQUE_FLOAT));
+
     try m.errify(c.SDL_RenderClear(renderer));
     try m.errify(c.SDL_RenderPresent(renderer));
 }
