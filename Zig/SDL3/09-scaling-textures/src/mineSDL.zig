5ff805c8086a8f5353da22a06def29ad93263057
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-02-08 23:10:56 -0500

    2025.02a

---
+++ Zig/SDL3/09-scaling-textures/src/mineSDL.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+const m = @import("main.zig");
+
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+});
+
+pub fn AppIterate(renderer: *c.SDL_Renderer) !void {
+    _ = c.SDL_RenderPresent(renderer);
+}
