7931c6abf199ae1e1fad358fbf7a4b68db64bdc5
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-02-13 14:19:47 -0500

    Add files via upload

--- Zig/SDL3/17-read-pixels/src/mineSDL.zig
+++ Zig/SDL3/17-read-pixels/src/mineSDL.zig
@@ -39,49 +39,47 @@ pub fn AppIterate(renderer: *c.SDL_Renderer) !void {
     center.y /= 2.0;
     _ = c.SDL_RenderTextureRotated(renderer, m.texture, null, &dst_rect, rotation, &center, c.SDL_FLIP_NONE);
 
+    // ************************************** part 2
+    // *********************************************
     // this next whole thing is _super_ expensive. Seriously, don't do this in real life.
 
-    // Download the pixels of what has just been rendered. This has to wait for the GPU to finish rendering it and everything before it,
-    // and then make an expensive copy from the GPU to system RAM!
+    // Download the pixels of what has just been rendered. This has to wait for the GPU
+    // to finish rendering it and everything before it, and then make an expensive copy
+    // from the GPU to system RAM!
     surface = c.SDL_RenderReadPixels(renderer, null);
-    print("        {}, {}\n", .{ surface.*.w, surface.*.h });
-    // This is also expensive, but easier: convert the pixels to a format we want.
-    if ((surface != null) and (surface.*.format != c.SDL_PIXELFORMAT_RGBA8888) and (surface.*.format != c.SDL_PIXELFORMAT_BGRA8888)) {
-        // SDL_Surface *converted = SDL_ConvertSurface(surface, SDL_PIXELFORMAT_RGBA8888);
+
+    const testA: bool = (surface != null);
+    const testB: bool = (surface.*.format != c.SDL_PIXELFORMAT_RGBA8888);
+    const testC: bool = (surface.*.format != c.SDL_PIXELFORMAT_BGRA8888);
+    if (testA and (testB and testC)) {
         const converted: [*c]c.SDL_Surface = c.SDL_ConvertSurface(surface, c.SDL_PIXELFORMAT_RGBA8888);
-        defer c.SDL_DestroySurface(surface);
+        _ = c.SDL_DestroySurface(surface);
         surface = converted;
     }
 
-    if (surface != null) {
-        // Rebuild converted_texture if the dimensions have changed (window resized, etc).
-
-        const s_w: i32 = surface.*.w;
-        const s_h: i32 = surface.*.h;
-        // print("{}, {}\n", .{ s_w, s_h });
-        if ((s_w != m.converted_texture_width) or (s_h != m.converted_texture_height)) {
+    const test_S: bool = (surface != null);
+    if (test_S) {
+        const test_SW: bool = (surface.*.w != m.converted_texture_width);
+        const test_SH: bool = (surface.*.h != m.converted_texture_height);
+        if (test_SW or test_SH) {
             _ = c.SDL_DestroyTexture(m.converted_texture);
             m.converted_texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, surface.*.w, surface.*.h);
             if (m.converted_texture == null) {
-                c.SDL_Log("Couldn't (re)create conversion texture: %s", c.SDL_GetError());
-                return;
+                print("***** oops\n", .{});
             }
             m.converted_texture_width = surface.*.w;
             m.converted_texture_height = surface.*.h;
         }
-
-        // Turn each pixel into either black or white. This is a lousy technique but it works here.
-        // In real life, something like Floyd-Steinberg dithering might work
-        // better: https://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
-        // const s_w: u32 = @intCast(surface.*.w);
-        // const s_h: u32 = @intCast(surface.*.h);
-        // for (0..s_h) |y| {
-        //     _ = y;
-        //     for (0..s_w) |x| {
-        //         _ = x;
-        //     }
-        // }
     }
+
+    // Turn each pixel into either black or white. This is a lousy technique but it works here.
+    // In real life, something like Floyd-Steinberg dithering might work
+    // better: https://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
+    // for (0..@intCast(surface.*.h)) |y| {
+    //     for(0..@intCast(surface.*.w)) |x| {
+
+    //     }
+    // }
 
     _ = c.SDL_RenderPresent(renderer);
 }
