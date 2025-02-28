3d5688aa0931dc832378eb014f826e19612ab752
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-02-28 16:03:15 -0500

    Update DiSprite.zig

---
+++ Zig/SDL3/UA Di/05-sprite-clipping-and-stretching/src/DiSprite.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+    @cInclude("SDL3_image/SDL_image.h");
+});
+
+const m = @import("main.zig");
+
+// ****************************************************************************
