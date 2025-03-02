c212e9dcbae8e07224a8d4c227c5fac975e6eeb7
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-03-02 14:28:44 -0500

    2025.03a

--- Zig/SDL3/UA Di/sprite/src/DiSprite.zig
+++ Zig/SDL3/UA Di/sprite/src/DiSprite.zig
@@ -17,13 +17,46 @@ const DiTexture = @import("DiTexture.zig");
 id: i32,
 sprite_sheet: *DiTexture,
 clippage: c.SDL_FRect,
+stretch_x: f32,
+stretch_y: f32,
+scale_x: f32,
+scale_y: f32,
+flip: c.SDL_FlipMode,
+center: c.SDL_FPoint,
+angle: f32,
 
 // ****************************************************************************
 const DiSprite = @This();
 
 // ****************************************************************************
 pub fn new(id: i32, sprite_sheet: *DiTexture, clippage: c.SDL_FRect) DiSprite {
-    return .{ .id = id, .sprite_sheet = sprite_sheet, .clippage = clippage };
+    return .{
+        .id = id,
+        .sprite_sheet = sprite_sheet,
+        .clippage = clippage,
+        .stretch_x = -1.0,
+        .stretch_y = -1.0,
+        .scale_x = 1.0,
+        .scale_y = 1.0,
+        .flip = c.SDL_FLIP_NONE,
+        .center = null,
+        .angle = 0.0,
+    };
+}
+
+// **********
+pub fn render2(self: *DiSprite, x: f32, y: f32) void {
+    var dst_rect: c.SDL_FRect = undefined;
+    if ((self.stretch_x <= 0.0) or (self.stretch_y <= 0.0)) {
+        dst_rect.h = self.clippage.h * self.scale_y;
+        dst_rect.w = self.clippage.w * self.scale_x;
+    } else {
+        dst_rect.h = self.stretch_y;
+        dst_rect.w = self.stretch_x;
+    }
+    dst_rect.x = x;
+    dst_rect.y = y;
+    _ = c.SDL_RenderTextureRotated(m.gRenderer, self.sprite_sheet.texture, &self.clippage, &dst_rect, self.angle, &self.center, self.flip);
 }
 
 // **********
