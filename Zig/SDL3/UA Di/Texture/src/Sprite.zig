0bf55ad50c7822b1558be179e6677f21b7e83932
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-03-07 10:25:41 -0500

    Add files via upload

--- Zig/SDL3/UA Di/Texture/src/Sprite.zig
+++ Zig/SDL3/UA Di/Texture/src/Sprite.zig
@@ -14,8 +14,8 @@ const m = @import("main.zig");
 
 // ****************************************************************************
 id: i32,
-tile_sheet: *c.SDL_Texture,
-clippage: c.SDL_FRect, // defines a Tile
+sprite_sheet: *c.SDL_Texture,
+sprite0: c.SDL_FRect, // first chippage for the sprite
 stretch_x: f32,
 stretch_y: f32,
 scale_x: f32,
@@ -26,15 +26,15 @@ is_default_center: bool,
 angle: f32,
 
 // ****************************************************************************
-const Tile = @This();
+const Sprite = @This();
 
 // ****************************************************************************
-pub fn new(id: i32, tile_sheet: ?*c.SDL_Texture, clippage: c.SDL_FRect) Tile {
+pub fn new(id: i32, sprite_sheet: ?*c.SDL_Texture, sprite0: c.SDL_FRect) Sprite {
     const center: c.SDL_FPoint = .{ .x = 0.0, .y = 0.0 };
     return .{
         .id = id,
-        .tile_sheet = @ptrCast(tile_sheet),
-        .clippage = clippage,
+        .sprite_sheet = @ptrCast(sprite_sheet),
+        .sprite0 = sprite0,
         .stretch_x = -1.0,
         .stretch_y = -1.0,
         .scale_x = 1.0,
@@ -47,11 +47,11 @@ pub fn new(id: i32, tile_sheet: ?*c.SDL_Texture, clippage: c.SDL_FRect) Tile {
 }
 
 // **********
-pub fn render(self: *Tile, renderer: ?*c.SDL_Renderer, x: f32, y: f32) void {
+pub fn render(self: *Sprite, renderer: ?*c.SDL_Renderer, x: f32, y: f32) void {
     var dst_rect: c.SDL_FRect = undefined;
     if ((self.stretch_x <= 0.0) or (self.stretch_y <= 0.0)) { // if stretch varibles are negative, use scaling.
-        dst_rect.h = self.clippage.h * self.scale_y;
-        dst_rect.w = self.clippage.w * self.scale_x;
+        dst_rect.h = self.sprite0.h * self.scale_y;
+        dst_rect.w = self.sprite0.w * self.scale_x;
     } else {
         dst_rect.h = self.stretch_y;
         dst_rect.w = self.stretch_x;
@@ -59,8 +59,8 @@ pub fn render(self: *Tile, renderer: ?*c.SDL_Renderer, x: f32, y: f32) void {
     dst_rect.x = x;
     dst_rect.y = y;
     if (self.is_default_center) {
-        _ = c.SDL_RenderTextureRotated(renderer, self.tile_sheet, &self.clippage, &dst_rect, self.angle, null, self.flip);
+        _ = c.SDL_RenderTextureRotated(renderer, self.sprite_sheet, &self.sprite0, &dst_rect, self.angle, null, self.flip);
     } else {
-        _ = c.SDL_RenderTextureRotated(renderer, self.tile_sheet, &self.clippage, &dst_rect, self.angle, &self.center, self.flip);
+        _ = c.SDL_RenderTextureRotated(renderer, self.sprite_sheet, &self.sprite0, &dst_rect, self.angle, &self.center, self.flip);
     }
 }
