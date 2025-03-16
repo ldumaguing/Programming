27724dbcebf76ebcd9ddd4aa5bbc326ddc08077a
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-03-16 16:22:16 -0400

    2025.03b

--- Zig/SDL3/UA Di/NW68/src/Sheet.zig
+++ Zig/SDL3/UA Di/NW68/src/Sheet.zig
@@ -15,10 +15,6 @@ const m = @import("main.zig");
 // ****************************************************************************
 id: i32,
 texture: *c.SDL_Texture,
-stretch_x: f32,
-stretch_y: f32,
-scale_x: f32,
-scale_y: f32,
 loc_x: f32,
 loc_y: f32,
 
@@ -30,10 +26,6 @@ pub fn bindTexture2Sheet(id: i32, texture: ?*c.SDL_Texture) Sheet {
     return .{
         .id = id,
         .texture = @ptrCast(texture),
-        .stretch_x = -1.0,
-        .stretch_y = -1.0,
-        .scale_x = 1.0,
-        .scale_y = 1.0,
         .loc_x = 0.0,
         .loc_y = 0.0,
     };
@@ -49,11 +41,11 @@ pub fn render(self: *Sheet, renderer: ?*c.SDL_Renderer) void {
         m.boardgame_sheet.loc_x = m.WINDOW_CENTER_X - (orig_len_x * m.gScale_mult);
         m.boardgame_sheet.loc_y = m.WINDOW_CENTER_Y - (orig_len_y * m.gScale_mult);
 
-        dst_rect.h = @as(f32, @floatFromInt(self.texture.h)) * self.scale_y * m.gScale_mult;
-        dst_rect.w = @as(f32, @floatFromInt(self.texture.w)) * self.scale_x * m.gScale_mult;
+        dst_rect.h = @as(f32, @floatFromInt(self.texture.h)) * m.gScale_mult;
+        dst_rect.w = @as(f32, @floatFromInt(self.texture.w)) * m.gScale_mult;
     } else {
-        dst_rect.h = @as(f32, @floatFromInt(self.texture.h)) * self.scale_y * m.gScale_mult;
-        dst_rect.w = @as(f32, @floatFromInt(self.texture.w)) * self.scale_x * m.gScale_mult;
+        dst_rect.h = @as(f32, @floatFromInt(self.texture.h)) * m.gScale_mult;
+        dst_rect.w = @as(f32, @floatFromInt(self.texture.w)) * m.gScale_mult;
     }
 
     dst_rect.x = self.loc_x;
