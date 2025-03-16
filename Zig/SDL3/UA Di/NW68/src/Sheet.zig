b16b19ad78765a65b2f4d6a1eb3ca62f2b7680e7
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-03-16 13:50:28 -0400

    2025.03b

--- Zig/SDL3/UA Di/NW68/src/Sheet.zig
+++ Zig/SDL3/UA Di/NW68/src/Sheet.zig
@@ -43,15 +43,21 @@ pub fn bindTexture2Sheet(id: i32, texture: ?*c.SDL_Texture) Sheet {
 pub fn render(self: *Sheet, renderer: ?*c.SDL_Renderer) void {
     var gscale: f32 = 0.0;
     var gscale_prev: f32 = 0.0;
+
     if (m.gScale >= 0) {
         gscale = 1.0 + @as(f32, @floatFromInt(m.gScale));
         if (m.gScale != m.gScale_prev) {
+            print("a", .{});
             if ((m.gScale_prev == -1) and (m.gScale == 0)) {
+                print("b", .{});
                 gscale_prev = 1.0 / (1.0 - (@as(f32, @floatFromInt(m.gScale_prev)) * 0.25));
             } else {
+                print("c", .{});
                 if (m.gScale > m.gScale_prev) {
+                    print("d", .{});
                     gscale_prev = @as(f32, @floatFromInt(m.gScale));
                 } else {
+                    print("e", .{});
                     gscale_prev = gscale + 1.0;
                 }
             }
@@ -59,16 +65,19 @@ pub fn render(self: *Sheet, renderer: ?*c.SDL_Renderer) void {
             const orig_len_y: f32 = (m.WINDOW_CENTER_Y - m.boardgame_sheet.loc_y) / gscale_prev;
             m.boardgame_sheet.loc_x = m.WINDOW_CENTER_X - (orig_len_x * gscale);
             m.boardgame_sheet.loc_y = m.WINDOW_CENTER_Y - (orig_len_y * gscale);
+            print("\n", .{});
         }
     } else {
         gscale = 1.0 / (1.0 - (@as(f32, @floatFromInt(m.gScale)) * 0.25));
         if (m.gScale != m.gScale_prev) {
+            print("A", .{});
             gscale_prev = 1.0 / (1.0 - (@as(f32, @floatFromInt(m.gScale_prev)) * 0.25));
             const orig_len_x: f32 = (m.WINDOW_CENTER_X - m.boardgame_sheet.loc_x) / gscale_prev;
             const orig_len_y: f32 = (m.WINDOW_CENTER_Y - m.boardgame_sheet.loc_y) / gscale_prev;
             m.boardgame_sheet.loc_x = m.WINDOW_CENTER_X - (orig_len_x * gscale);
             m.boardgame_sheet.loc_y = m.WINDOW_CENTER_Y - (orig_len_y * gscale);
         }
+        print("\n", .{});
     }
 
     var dst_rect: c.SDL_FRect = undefined;
