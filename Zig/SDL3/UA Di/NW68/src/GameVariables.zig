3b1817109db3605de2726728708046a5a508ae3c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-03-25 12:14:10 -0400

    Add files via upload

---
+++ Zig/SDL3/UA Di/NW68/src/GameVariables.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+const mvzr = @import("mvzr.zig");
+
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+    @cInclude("SDL3_image/SDL_image.h");
+});
+
+pub var window: ?*c.SDL_Window = undefined;
+pub var renderer: ?*c.SDL_Renderer = undefined;
+
+// *************** Joystick
+pub var joystick: ?*c.SDL_Joystick = null;
+pub var button_bits: u16 = 0;
+pub var button_mods = [_]i32{0} ** 14;
+pub var d_pad: u16 = 0;
+
+// ***************
+pub var keybrd_bits: u16 = 0;
+pub var keybrd_dpad: u16 = 0;
+
+// ***************
+pub var all_bits: u16 = 0;
+pub var all_dpad: u16 = 0;
+
+// ***
