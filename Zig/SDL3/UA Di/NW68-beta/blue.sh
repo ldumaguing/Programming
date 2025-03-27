8c2395d66ab4d376f90fe08b35d8fd41c91d2bef
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-03-27 12:29:30 -0400

    Add files via upload

---
+++ Zig/SDL3/UA Di/NW68-beta/blue.sh
@@ -0,0 +1,59 @@
+convert '1st Ranger Chit front.png' \
+'1st Ranger CO back.png' \
+'1st Ranger CO front.png' \
+'1st Ranger M113 back.png' \
+'1st Ranger M113 front.png' \
+'1st Ranger M60 back.png' \
+'1st Ranger M60 front.png' \
+'1st Ranger Ranger back.png' \
+'1st Ranger Ranger front.png' \
+'2nd Ranger BRDM back.png' \
+'2nd Ranger BRDM front.png' \
+'2nd Ranger Chit front.png' \
+'2nd Ranger CO back.png' \
+'2nd Ranger CO front.png' \
+'2nd Ranger M113 back.png' \
+'2nd Ranger M113 front.png' \
+'2nd Ranger M60 back.png' \
+'2nd Ranger M60 front.png' \
+'2nd Ranger Ranger back.png' \
+'2nd Ranger Ranger front.png' \
+'4th G Chit front.png' \
+'4th G CO back.png' \
+'4th G CO front.png' \
+'4th G T64 back.png' \
+'4th G T64 front.png' \
+'58th Chit front.png' \
+'58th CO back.png' \
+'58th CO front.png' \
+'58th M113 back.png' \
+'58th M113 front.png' \
+'58th Rangers back.png' \
+'58th Rangers front.png' \
+'58th Spetsnaz back.png' \
+'58th Spetsnaz front.png' \
+'58th T62 back.png' \
+'58th T62 front.png' \
+'9K32 front.png' \
+'A6 front.png' \
+'blue control.png' \
+'BRDM back.png' \
+'BRDM front.png' \
+'Cowboys Chit front.png' \
+'Cowboys Huey back.png' \
+'Cowboys Huey front.png' \
+'M109 back.png' \
+'M109 front.png' \
+'M-60 front.png' \
+'M72Law front.png' \
+'Scud front.png' \
+'Vympel Chit front.png' \
+'Vympel Co back.png' \
+'Vympel Co front.png' \
+'Vympel Spetsnaz back.png' \
+'Vympel Spetsnaz front.png' \
+'ZSU23 back.png' \
+'ZSU23 front.png' \
+-append blue.PNG
+
+
---
+++ Zig/SDL3/UA Di/NW68-beta/ears.sh
@@ -0,0 +1,7 @@
+convert 'Finished.png' \
+'Moved.png' \
+'OOC.png' \
+'Shaken.png' \
+'TLR_Bevel.png' \
+'TLR_DS.png' \
+ -append ears.PNG
