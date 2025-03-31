c1f367b7c5e5ef3691f99bc50a08a67034f3b62d
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-03-31 12:18:53 -0400

    Add files via upload

---
+++ Zig/SDL3/UA Di/NW68-background-move/src/gameLoop.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+const g = @import("GameVariables.zig");
+
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+    @cInclude("SDL3_image/SDL_image.h");
+});
+
+pub fn draw_world() void {
+    _ = c.SDL_SetRenderDrawColor(g.renderer, 255, 5, 5, c.SDL_ALPHA_OPAQUE);
+    _ = c.SDL_RenderClear(g.renderer);
+
+    g.mapboard_sheet.render();
+    _ = c.SDL_RenderPresent(g.renderer);
+}
+
+pub fn update_world() void {
+    // ****************************** Global scale
+    if ((g.all_bits & 32) > 0) {
+        g.scale_rank += 1;
+        g.all_bits ^= 32;
+    }
+    if ((g.all_bits & 16) > 0) {
+        g.scale_rank -= 1;
+        g.all_bits ^= 16;
+    }
+    if (g.scale_rank >= 0) {
+        g.scale = 1.0 + @as(f32, @floatFromInt(g.scale_rank));
+    } else {
+        g.scale = 1.0 / (1.0 - @as(f32, @floatFromInt(g.scale_rank)));
+    }
+
+    // ****************************** move mapboard
+    if ((g.all_dpad & 1) > 0) {
+        g.mapboard_sheet.y += 10;
+    }
+    if ((g.all_dpad & 2) > 0) {
+        g.mapboard_sheet.x -= 10;
+    }
+    if ((g.all_dpad & 4) > 0) {
+        g.mapboard_sheet.y -= 10;
+    }
+    if ((g.all_dpad & 8) > 0) {
+        g.mapboard_sheet.x += 10;
+    }
+}
