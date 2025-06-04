1bc7a8beec9814169c5c22b13727a60b17ec673a
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-06-04 19:46:52 -0400

    2025.06a

--- Zig/SDL3/UA Di/NW68/src/main.zig
+++ Zig/SDL3/UA Di/NW68/src/main.zig
@@ -1,46 +1,91 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var window: ?*c.SDL_Window = undefined;
+    var renderer: ?*c.SDL_Renderer = undefined;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+    // const dimention = c.SDL_GetCurrentDisplayMode(c.SDL_GetPrimaryDisplay());
+    // g.desktop_w = @as(f32, @floatFromInt(dimention.*.w));
+    // g.desktop_h = @as(f32, @floatFromInt(dimention.*.h));
+    // g.desktop_w = 1280; // High Definition (HD)
+    // g.desktop_h = 720;
+    // g.desktop_w = 960; // anbernic.com
+    // g.desktop_h = 720;
+    const desktop_w = 800;
+    const desktop_h = 600;
+    // g.window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(g.desktop_w), @intFromFloat(g.desktop_h), c.SDL_WINDOW_BORDERLESS);
+    window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(desktop_w), @intFromFloat(desktop_h), 0);
+    // g.window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(g.desktop_w), @intFromFloat(g.desktop_h), c.SDL_WINDOW_FULLSCREEN);
+    renderer = c.SDL_CreateRenderer(window, null);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
+// ************************************************************************************************
 const std = @import("std");
+const print = @import("std").debug.print;
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
 const lib = @import("NW68_lib");
+
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+    @cInclude("SDL3_image/SDL_image.h");
+});
+
+// ************************************************************************************************
+fn fmtSdlDrivers(
+    current_driver: [*:0]const u8,
+    num_drivers: c_int,
+    getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
+) std.fmt.Formatter(formatSdlDrivers) {
+    return .{ .data = .{
+        .current_driver = current_driver,
+        .num_drivers = num_drivers,
+        .getDriver = getDriver,
+    } };
+}
+
+fn formatSdlDrivers(
+    context: struct {
+        current_driver: [*:0]const u8,
+        num_drivers: c_int,
+        getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
+    },
+    comptime _: []const u8,
+    _: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    var i: c_int = 0;
+    while (i < context.num_drivers) : (i += 1) {
+        if (i != 0) {
+            try writer.writeAll(", ");
+        }
+        const driver = context.getDriver(i).?;
+        try writer.writeAll(std.mem.span(driver));
+        if (std.mem.orderZ(u8, context.current_driver, driver) == .eq) {
+            try writer.writeAll(" (current)");
+        }
+    }
+}
+
+/// Converts the return value of an SDL function to an error union.
+pub inline fn errify(value: anytype) error{SdlError}!switch (@import("shims").typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@import("shims").typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
+}
