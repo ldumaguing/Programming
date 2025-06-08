e67dc4adffc4e728dae767cc6127fa96be75da3a
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-06-08 02:37:37 -0400

    2025.06a

--- Zig/SDL3/UA Di/NW68/src/main.zig
+++ Zig/SDL3/UA Di/NW68/src/main.zig
@@ -1,28 +1,5 @@
-pub fn main() !void {
-    var window: ?*c.SDL_Window = undefined;
-    var renderer: ?*c.SDL_Renderer = undefined;
-
-    // const dimention = c.SDL_GetCurrentDisplayMode(c.SDL_GetPrimaryDisplay());
-    // g.desktop_w = @as(f32, @floatFromInt(dimention.*.w));
-    // g.desktop_h = @as(f32, @floatFromInt(dimention.*.h));
-    // g.desktop_w = 1280; // High Definition (HD)
-    // g.desktop_h = 720;
-    // g.desktop_w = 960; // anbernic.com
-    // g.desktop_h = 720;
-    const desktop_w = 800;
-    const desktop_h = 600;
-    // g.window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(g.desktop_w), @intFromFloat(g.desktop_h), c.SDL_WINDOW_BORDERLESS);
-    window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(desktop_w), @intFromFloat(desktop_h), 0);
-    // g.window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(g.desktop_w), @intFromFloat(g.desktop_h), c.SDL_WINDOW_FULLSCREEN);
-    renderer = c.SDL_CreateRenderer(window, null);
-}
-
-// ************************************************************************************************
 const std = @import("std");
-const print = @import("std").debug.print;
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("NW68_lib");
+const NW68 = @import("NW68");
 
 const c = @cImport({
     @cDefine("SDL_DISABLE_OLD_NAMES", {});
@@ -32,6 +9,75 @@ const c = @cImport({
     @cInclude("SDL3/SDL_main.h");
     @cInclude("SDL3_image/SDL_image.h");
 });
+
+var window: ?*c.SDL_Window = undefined;
+var renderer: ?*c.SDL_Renderer = undefined;
+var desktop_w: f32 = 128.0;
+var desktop_h: f32 = 128.0;
+
+pub fn main() !void {
+    errdefer |err| if (err == error.SdlError) std.log.err("SDL error: {s}", .{c.SDL_GetError()});
+
+    std.log.debug("SDL build time version: {d}.{d}.{d}", .{
+        c.SDL_MAJOR_VERSION,
+        c.SDL_MINOR_VERSION,
+        c.SDL_MICRO_VERSION,
+    });
+
+    const version = c.SDL_GetVersion();
+    std.log.debug("SDL runtime version: {d}.{d}.{d}", .{
+        c.SDL_VERSIONNUM_MAJOR(version),
+        c.SDL_VERSIONNUM_MINOR(version),
+        c.SDL_VERSIONNUM_MICRO(version),
+    });
+
+    c.SDL_SetMainReady();
+
+    try errify(c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD | c.SDL_INIT_JOYSTICK));
+    defer c.SDL_Quit();
+
+    std.log.debug("SDL video drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentVideoDriver().?,
+        c.SDL_GetNumVideoDrivers(),
+        c.SDL_GetVideoDriver,
+    )});
+    std.log.debug("SDL audio drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentAudioDriver().?,
+        c.SDL_GetNumAudioDrivers(),
+        c.SDL_GetAudioDriver,
+    )});
+
+    errify(c.SDL_SetHint(c.SDL_HINT_RENDER_VSYNC, "1")) catch {};
+
+    // [ set window and renderer ******************************************************************
+    const desktop_dim = c.SDL_GetCurrentDisplayMode(c.SDL_GetPrimaryDisplay());
+    desktop_w = @as(f32, @floatFromInt(desktop_dim.*.w));
+    desktop_h = @as(f32, @floatFromInt(desktop_dim.*.h));
+    window = c.SDL_CreateWindow("Nuklear Winter '68", @intFromFloat(desktop_w), @intFromFloat(desktop_h), 0);
+    renderer = c.SDL_CreateRenderer(window, null);
+    defer c.SDL_DestroyRenderer(renderer);
+    defer c.SDL_DestroyWindow(window);
+    // ]
+
+    // [ game loop ********************************************************************************
+    main_loop: while (true) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_EVENT_KEY_DOWN, c.SDL_EVENT_KEY_UP => {
+                    switch (event.key.scancode) {
+                        c.SDL_SCANCODE_ESCAPE => {
+                            break :main_loop;
+                        },
+                        else => {},
+                    }
+                },
+                else => {},
+            }
+        }
+    }
+    // ]
+}
 
 // ************************************************************************************************
 fn fmtSdlDrivers(
