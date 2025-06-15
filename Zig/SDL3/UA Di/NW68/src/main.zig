4ed5ab598fb02dd550c2ac1c3fb80adcd64b034a
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-06-15 18:34:26 -0400

    2025.06a

--- Zig/SDL3/UA Di/NW68/src/main.zig
+++ Zig/SDL3/UA Di/NW68/src/main.zig
@@ -157,6 +157,7 @@ fn draw_world() void {
     _ = draw_mapboard();
     _ = draw_chit1();
     _ = draw_chit2();
+    _ = draw_chit3();
 
     // ***** draw X on window
     _ = c.SDL_RenderLine(renderer, 0, 0, gv.window_w, gv.window_h);
@@ -164,6 +165,47 @@ fn draw_world() void {
 
     // ***** show
     _ = c.SDL_RenderPresent(renderer);
+}
+
+// ------------------------------------------------------------------------------------------------
+fn draw_chit3() void {
+    // ***** chit info
+    const hex_ID = [_]i32{ 1, 1 };
+    const chit_index = 7;
+
+    // ***** create a surface
+    const a_surf: *c.SDL_Surface = c.SDL_CreateSurface(gv.chit_square_dim, gv.chit_square_dim, c.SDL_PIXELFORMAT_RGBA8888);
+    defer c.SDL_DestroySurface(a_surf);
+
+    // ***** clip one chit and put it on the surface
+    var a_rect: c.SDL_Rect = undefined;
+    a_rect.x = 0;
+    a_rect.y = chit_index * gv.chit_square_dim;
+    a_rect.w = gv.chit_square_dim;
+    a_rect.h = gv.chit_square_dim;
+    _ = c.SDL_BlitSurface(chits_surface, &a_rect, a_surf, null); // no scaling. the target surface truncates.
+
+    // convert the surface to a texture
+    const a_texture = c.SDL_CreateTextureFromSurface(renderer, a_surf);
+    defer c.SDL_DestroyTexture(a_texture);
+
+    // define a silly puddy rectangle and render it
+    var a_rectness: c.SDL_FRect = undefined;
+    const hex_ID_x: i32 = @intFromFloat(@as(f64, @floatFromInt(hex_ID[0])) * gv.Hex_Dim_ness[0]);
+    const hex_ID_y: i32 = @intFromFloat(@as(f64, @floatFromInt(hex_ID[1])) * gv.Hex_Dim_ness[1]);
+    const x: f32 = @as(f32, @floatFromInt(gv.Zero_Zero[0] - gv.map_loc[0] + hex_ID_x)) / gv.scaleness;
+    var y: f32 = 0.0;
+    if (@mod(hex_ID[0], 2) == 0) {
+        y = @as(f32, @floatFromInt(gv.Zero_Zero[1] - gv.map_loc[1] + hex_ID_y)) / gv.scaleness;
+    } else {
+        y = @as(f32, @floatFromInt(gv.Zero_Zero[1] - gv.map_loc[1] + hex_ID_y + @as(i32, @intFromFloat(gv.Half_Hex_Y_ness)))) / gv.scaleness;
+    }
+    const w_h_ness: f32 = @as(f32, @floatFromInt(gv.chit_square_dim)) / gv.scaleness;
+    a_rectness.x = x;
+    a_rectness.y = y;
+    a_rectness.w = w_h_ness;
+    a_rectness.h = w_h_ness;
+    _ = c.SDL_RenderTexture(renderer, a_texture, null, &a_rectness);
 }
 
 // ------------------------------------------------------------------------------------------------
