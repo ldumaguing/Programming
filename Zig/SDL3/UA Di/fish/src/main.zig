36dfeeafa588ae0b89274459777455446e79a773
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-06-22 22:34:28 -0400

    2025.06b

---
+++ Zig/SDL3/UA Di/fish/src/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+const fish = @import("fish");
+
+const c = @cImport({
+    @cInclude("sqlite3.h");
+    @cInclude("sqlite3ext.h");
+});
+
+pub fn main() !void {
+    var db: ?*c.sqlite3 = undefined;
+    var stmt: ?*c.sqlite3_stmt = undefined;
+
+    _ = c.sqlite3_open("fish.db", &db);
+    if (db == null) {
+        print("failed to open\n", .{});
+        return;
+    }
+
+    _ = c.sqlite3_prepare_v2(db, "select * from laptop", -1, &stmt, null);
+    var count: i32 = 0;
+    while (c.sqlite3_step(stmt) != c.SQLITE_DONE) {
+        count += 1;
+        print("{d}\n", .{count});
+    }
+}
