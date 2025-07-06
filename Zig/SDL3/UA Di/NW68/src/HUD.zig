3dc1888690f931dc95412b4c1653191793303e91
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-07-06 14:56:04 -0400

    2025.07a

--- Zig/SDL3/UA Di/NW68/src/HUD.zig
+++ Zig/SDL3/UA Di/NW68/src/HUD.zig
@@ -13,21 +13,24 @@ const c = @cImport({
 });
 
 pub fn mode() void {
-    const dst_rect = c.SDL_Rect{ .x = 0, .y = 60, .w = 260, .h = 340 }; // cutter
-    const dst_rectness = c.SDL_FRect{ .x = 60.0, .y = 60.0, .w = 260.0, .h = 340.0 }; // silly putty
-    var viewport: c.SDL_Rect = undefined;
+    const clipped = c.SDL_Rect{ .x = 0, .y = 60, .w = 260, .h = 340 }; // clipped
 
     // ***** create a surface
     const a_surf: *c.SDL_Surface = c.SDL_CreateSurface(260, 340, c.SDL_PIXELFORMAT_RGBA8888);
     defer c.SDL_DestroySurface(a_surf);
 
     // ***** clip the frame and put it on the surface
-    _ = c.SDL_BlitSurface(@ptrCast(m.frames_surface), &dst_rect, a_surf, null);
+    _ = c.SDL_BlitSurface(@ptrCast(m.frames_surface), &clipped, a_surf, null);
 
     // convert the surface to a texture
     const a_texture = c.SDL_CreateTextureFromSurface(@ptrCast(m.renderer), a_surf);
     defer c.SDL_DestroyTexture(a_texture);
 
-    _ = c.SDL_SetRenderViewport(@ptrCast(m.renderer), &viewport);
-    _ = c.SDL_RenderTexture(@ptrCast(m.renderer), a_texture, null, &dst_rectness);
+    if (false) { // not using viewport
+        // *** render texture
+        const fishness = c.SDL_FRect{ .x = 30.0, .y = 30.0, .w = 260.0, .h = 340.0 };
+        _ = c.SDL_RenderTexture(@ptrCast(m.renderer), a_texture, null, &fishness);
+    } else { // trying to use viewport; lol
+
+    }
 }
