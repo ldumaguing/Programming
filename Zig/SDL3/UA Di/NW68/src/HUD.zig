274e5d371037243b73b2177d054e4a279cca3921
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-07-06 23:17:22 -0400

    2025.07a

--- Zig/SDL3/UA Di/NW68/src/HUD.zig
+++ Zig/SDL3/UA Di/NW68/src/HUD.zig
@@ -13,10 +13,12 @@ const c = @cImport({
 });
 
 pub fn mode() void {
-    const clipped = c.SDL_Rect{ .x = 0, .y = 60, .w = 260, .h = 340 }; // clipped
+    const frame_TopLeft = [_]i32{ 0, 60 };
+    const frame_dim = [_]i32{ 372, 596 };
+    const clipped = c.SDL_Rect{ .x = frame_TopLeft[0], .y = frame_TopLeft[1], .w = frame_dim[0], .h = frame_dim[1] }; // clipped
 
     // ***** create a surface
-    const a_surf: *c.SDL_Surface = c.SDL_CreateSurface(260, 340, c.SDL_PIXELFORMAT_RGBA8888);
+    const a_surf: *c.SDL_Surface = c.SDL_CreateSurface(frame_dim[0], frame_dim[1], c.SDL_PIXELFORMAT_RGBA8888);
     defer c.SDL_DestroySurface(a_surf);
 
     // ***** clip the frame and put it on the surface
@@ -32,11 +34,15 @@ pub fn mode() void {
         _ = c.SDL_RenderTexture(@ptrCast(m.renderer), a_texture, null, &fishness);
     } else { // trying to use viewport; lol
         //const rect_0 = c.SDL_Rect{ .x = 0, .y = 0, .w = 260, .h = 340 };
-        const rect_0ness = c.SDL_FRect{ .x = gv.window_w - 260.0, .y = 0.0, .w = 260.0, .h = 340.0 };
+        const rect_0ness = c.SDL_FRect{ .x = gv.window_w - frame_dim[0], .y = 0.0, .w = frame_dim[0], .h = frame_dim[1] };
         _ = c.SDL_SetRenderViewport(@ptrCast(m.renderer), null);
         _ = c.SDL_RenderTexture(@ptrCast(m.renderer), a_texture, null, &rect_0ness);
 
         _ = c.SDL_SetRenderDrawColor(@ptrCast(m.renderer), 255, 255, 255, c.SDL_ALPHA_OPAQUE);
-        _ = c.SDL_RenderDebugText(@ptrCast(m.renderer), rect_0ness.x + 11.0, rect_0ness.y + 11.0, "Hello world!");
+        const font_scale: f32 = 2.0;
+        _ = c.SDL_SetRenderScale(@ptrCast(m.renderer), font_scale, font_scale); // this will affect a lot
+        _ = c.SDL_RenderDebugText(@ptrCast(m.renderer), (rect_0ness.x / font_scale) + (11 / font_scale), (11 / font_scale), "-123456789-123456789-1");
+        _ = c.SDL_SetRenderScale(@ptrCast(m.renderer), 1.0, 1.0); // normalize overall scale
+
     }
 }
