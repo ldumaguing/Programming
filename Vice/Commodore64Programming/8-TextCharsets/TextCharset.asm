5d37e6e63cd8f3c108c555ba5172c614f5b5edbc
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-07-27 17:22:44 -0400

    2025.07b

--- Vice/Commodore64Programming/8-TextCharsets/TextCharset.asm
+++ Vice/Commodore64Programming/8-TextCharsets/TextCharset.asm
@@ -2,34 +2,23 @@ BasicUpstart2(main)
 	* = $1000
 
 main:
-	jsr $e544
+	jsr $e544 // clear screen
 	lda #$0d
-	sta $d020
+	sta $d020 // border color
 	lda #$05
-	sta $d021
-	lda #$18
-	sta $d018
+	sta $d021 // background color
+	lda #$18  // 0001 100_ ( 1 * 1024 = $400; 4 * 2048 = $2000)
+	sta $d018 // VIC-II register
 
-	ldx #$00
-write:
+	ldx    #0
 	lda    msg,x
 	jsr    $ffd2
-	inx
-	cpx    #20
-	bne    write
-
-	ldx #$00
-setcolor:
-	lda #$07
-	sta $d800,x
-	inx
-	cpx #$20
-	bne setcolor
 
 loop:
 	jmp loop
 
-msg: .text "C64 AZWERTYUIOPLKJHGrial by digitalerr0r of Dark Codex"
+// msg: .text "C64 AZWERTYUIOPLKJHGrial by digitalerr0r of Dark Codex"
+msg: .byte 0,1,2,3
 
 	* = $2000
 .import binary "fish.bin"
