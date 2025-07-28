a0199c0cfeaa87aff48b7556fe8b39d064cb9dcb
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-07-27 21:45:54 -0400

    2025.07b

--- Vice/Commodore64Programming/8-TextCharsets/TextCharset.asm
+++ Vice/Commodore64Programming/8-TextCharsets/TextCharset.asm
@@ -5,20 +5,20 @@ main:
 	jsr $e544 // clear screen
 	lda #$0d
 	sta $d020 // border color
-	lda #$05
+	lda #$00
 	sta $d021 // background color
 	lda #$18  // 0001 100_ ( 1 * 1024 = $400; 4 * 2048 = $2000)
 	sta $d018 // VIC-II register
 
-	ldx    #0
-	lda    msg,x
-	jsr    $ffd2
+//	ldx    #2
+//	lda    msg,x
+//	jsr    $ffd2
 
-loop:
-	jmp loop
+	rts
 
 // msg: .text "C64 AZWERTYUIOPLKJHGrial by digitalerr0r of Dark Codex"
-msg: .byte 0,1,2,3
+// msg: .byte 0,1,2,3
 
 	* = $2000
+//.import c64 "scrap_writer_iii_17.64c"
 .import binary "fish.bin"
