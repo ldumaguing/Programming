8dec3a237db8b22a2db88f02c4c91cb99d70416f
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-08-10 17:47:37 -0400

    Update keyboardv2.s

--- Vice/Programming/Mine/keyboardv2.s
+++ Vice/Programming/Mine/keyboardv2.s
@@ -5,13 +5,13 @@ loop:
 
 	tax
 	and #$0f    ; 00001111 AND accumulator
-	sta $d020   ; store what was pressed into screen color
+	sta $d020   ; store what was pressed into border color
 	txa
 	lsr a       ; accumulator bit shift right once
 	lsr a
 	lsr a
 	lsr a
-	sta $d021   ; store what was press into border color
+	sta $d021   ; store what was press into background color
 	jmp loop    ; continue loop
 
 ; ***** same background and foreground colors; so, bit shifting works
