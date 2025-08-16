b6c7ae3e3560b04615a4eecd6bde5be739731335
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-08-15 22:58:25 -0400

    2025.08a

--- Vice/Programming/Mine/highres.s
+++ Vice/Programming/Mine/highres.s
@@ -13,7 +13,7 @@
 
    lda #0
    tax      ; x = 0
-   lda #$69 ; blue & red
+   lda #$01 ; black & white
 VidMem_loop:
    sta $400,x
    sta $500,x
@@ -27,6 +27,14 @@ VidMem_loop:
 Bank_loop:
    lda #$55
    sta $2000,x
+   sta $2100,x
+   sta $2200,x
+   sta $2300,x
+   lda #$aa
+   sta $2001,x
+   sta $2101,x
+   sta $2201,x
+   sta $2301,x
    inx
    inx
    bne Bank_loop
