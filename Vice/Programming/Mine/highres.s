1ab17b62265022c6ca52008b751bcdba1d4720d9
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-08-15 22:44:38 -0400

    2025.08a

---
+++ Vice/Programming/Mine/highres.s
@@ -0,0 +1,40 @@
+; cl65 -o highres -t c64 -I ~/cc65/include -L ~/cc65/lib -C c64-asm.cfg-MINE highres.s && mv highres ~/Vice/vicefs/
+; sys49152
+
+   ; ***** set bitmap mode
+   lda $d011
+   ora #32
+   sta $d011
+
+   ; ***** Video Matrix: $400
+   ; ***** Use upper half
+   lda #$18
+   sta $d018
+
+   lda #0
+   tax      ; x = 0
+   lda #$69 ; blue & red
+VidMem_loop:
+   sta $400,x
+   sta $500,x
+   sta $600,x
+   sta $700,x
+   inx
+   bne VidMem_loop
+
+   lda #0
+   tax
+Bank_loop:
+   lda #$55
+   sta $2000,x
+   inx
+   inx
+   bne Bank_loop
+
+loop:
+   jmp loop
+
+
+
+
+
