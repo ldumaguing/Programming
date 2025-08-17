54409215065b143ca743fcd3d01e58a63df7d6d0
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-08-17 12:27:48 -0400

    2025.08b

---
+++ Vice/Programming/Mine/highres_Bank0.s
@@ -0,0 +1,38 @@
+; cl65 -o test -u __EXEHDR__ -t c64 -I ~/cc65/include -L ~/cc65/lib -C c64-asm.cfg highres_Bank0.s && mv test ~/Vice/vicefs/
+; run
+
+   ; ***** set bitmap mode
+   lda $d011
+   ora #32
+   sta $d011
+
+   ; ***** Bank
+   lda $dd00
+   ora #3         ; Bank #0; $0000 to $3fff
+   sta $dd00
+
+   ; ***** (high nibble; 1) Video Matrix: $400
+   ; ***** (low nibble; 8)  Use upper half
+   lda #$18
+   sta $d018
+
+   ; ******************************************************************************** Start drawing
+   lda #$01       ; black & white
+   sta $400       ; a cell with the two colors
+
+   ldx #$0        ; index for img array
+   ldy #8         ; number of bytes to read from
+loop1:
+   lda img,x
+   sta $2000,x
+   inx
+   dey
+   bne loop1
+
+loop:
+   jmp loop
+
+img: .byte $28, $28, $ee, 0, $ee, $28, $28, 0
+
+
+
---
+++ Vice/Programming/Mine/highres_Bank1.s
@@ -0,0 +1,38 @@
+; cl65 -o test -u __EXEHDR__ -t c64 -I ~/cc65/include -L ~/cc65/lib -C c64-asm.cfg highres_Bank1.s && mv test ~/Vice/vicefs/
+; run
+
+   ; ***** set bitmap mode
+   lda $d011
+   ora #32
+   sta $d011
+
+   ; ***** Bank
+   lda $dd00
+   ora #2         ; Bank #1; $4000 to $7fff
+   sta $dd00
+
+   ; ***** (high nibble; 1) Video Matrix: $400
+   ; ***** (low nibble; 8)  Use upper half
+   lda #$78
+   sta $d018
+
+   ; ******************************************************************************** Start drawing
+   lda #$23
+   sta $1c00       ; a cell with the two colors
+
+   ldx #$0        ; index for img array
+   ldy #8         ; number of bytes to read from
+loop1:
+   lda img,x
+   sta $6000,x
+   inx
+   dey
+   bne loop1
+
+loop:
+   jmp loop
+
+img: .byte $28, $28, $ee, 0, $ee, $28, $28, 0
+
+
+
