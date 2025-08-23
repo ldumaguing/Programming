1009efabac5fadc9bbc582ad8fe0c490deece98c
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-08-22 21:35:49 -0400

    2025.08b

--- Vice/Programming/Mine/Tutorial-03.s
+++ Vice/Programming/Mine/Tutorial-03.s
@@ -1,5 +1,5 @@
-; cl65 -o test -u __EXEHDR__ -t c64 -I ~/cc65/include -L ~/cc65/lib -C c64-asm.cfg Tutorial-02.s && mv test ~/Vice/vicefs/ && rm *.o
-; cl65 -o test -u __EXEHDR__ -t c64 -C c64-asm.cfg Tutorial-02.s && mv test ~/Vice/vicefs/ && rm *.o
+; cl65 -o test -u __EXEHDR__ -t c64 -I ~/cc65/include -L ~/cc65/lib -C c64-asm.cfg Tutorial-03.s && mv test ~/Vice/vicefs/ && rm *.o
+; cl65 -o test -u __EXEHDR__ -t c64 -C c64-asm.cfg Tutorial-03.s && mv test ~/Vice/vicefs/ && rm *.o
 ; run
 
    Bank2 = $8000
