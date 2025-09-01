511be95d7ad61ee9e9171e9fce32fcf197f9640b
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-09-01 00:02:34 -0400

    2025.09a

--- Vice/Programming/Mine/Tutorial-05/bar.s
+++ Vice/Programming/Mine/Tutorial-05/bar.s
@@ -1,4 +1,6 @@
 .export _SetColor
+.export _SetColor2
+.export _SetColor3
 
 _SetColor:
    lda #$00
@@ -6,5 +8,17 @@ _SetColor:
 
    lda #$01
    sta $d021
+
+   rts
+
+_SetColor2:
+   sta $d020
+   sta $d021
+
+   rts
+
+_SetColor3:
+   sta $d020   ; border
+   stx $d021   ; background
 
    rts
--- Vice/Programming/Mine/Tutorial-05/foo.c
+++ Vice/Programming/Mine/Tutorial-05/foo.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 extern void SetColor(void);
+extern void SetColor2(char);
+extern void SetColor3(char, char);
 
 int main(void) {
     printf("yo");
-    SetColor();
+    // SetColor();
+    // SetColor2(13);
+    SetColor3(0,1);    // (x, a)
     return 0;
 }
