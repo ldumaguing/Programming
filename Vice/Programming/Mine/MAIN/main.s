3f3fb92234589a8f11638d280970d63b54b91219
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-09-17 23:17:19 -0400

    2025.09b

--- Vice/Programming/Mine/MAIN/main.s
+++ Vice/Programming/Mine/MAIN/main.s
@@ -1,4 +1,6 @@
 .include "defs.s"
+; first row, first column --- 1024
+; third row, first column --- 1104
 
 ; cl65 -o main -u __EXEHDR__ -t c64 -C ../c64-asm.cfg main.s && mv main ~/Vice/vicefs/
 ; LOAD"MAIN",9,1
