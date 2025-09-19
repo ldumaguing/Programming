a042056e06888a5396593c581fc0e273716472a5
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-09-18 23:43:23 -0400

    2025.09b

--- Vice/Programming/Mine/MAIN/defs.s
+++ Vice/Programming/Mine/MAIN/defs.s
@@ -9,5 +9,25 @@ MESSAGE    = $9d
 VIC_BANK   = $dd00
 MEM_SETUP  = $d018
 COLOR_RAM  = $d800
+
 SCREEN_CONTROL_1 = $d011
 SCREEN_CONTROL_2 = $d016
+
+BLACK       = 0
+WHITE       = 1
+RED         = 2
+CYAN        = 3
+PURPLE      = 4
+GREEN       = 5
+BLUE        = 6
+YELLOW      = 7
+ORANGE      = 8
+BROWN       = 9
+PINK        = 10
+DARK_GREY   = 11
+GREY        = 12
+LIGHT_GREEN = 13
+LIGHT_BLUE  = 14
+LIGHT_GREY  = 15
+
+
