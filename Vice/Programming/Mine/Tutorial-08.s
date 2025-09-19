87d625ea8160829a4fe5c814fe84759752e1c3ec
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-09-19 15:54:15 -0400

    Update Tutorial-08.s

--- Vice/Programming/Mine/Tutorial-08.s
+++ Vice/Programming/Mine/Tutorial-08.s
@@ -1,5 +1,5 @@
 ; cl65 -o test -u __EXEHDR__ -t c64 -C ../c64-asm.cfg Tutorial-08.s && mv test ~/Vice/vicefs/
-; LOAD"MAIN",9,1
+; LOAD"TEST",9,1
 
    ; ***** divided by 8; disregard remainder
    ldx #9
