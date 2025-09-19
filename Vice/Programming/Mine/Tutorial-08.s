6cc0f2b7d1e2c1ca513f5cf7f7e0017ac90bd1e2
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-09-19 15:52:39 -0400

    Add files via upload

---
+++ Vice/Programming/Mine/Tutorial-08.s
@@ -0,0 +1,18 @@
+; cl65 -o test -u __EXEHDR__ -t c64 -C ../c64-asm.cfg Tutorial-08.s && mv test ~/Vice/vicefs/
+; LOAD"MAIN",9,1
+
+   ; ***** divided by 8; disregard remainder
+   ldx #9
+   stx $c000
+
+   lda $c000
+   ror
+   clc
+   ror
+   clc
+   ror
+   sta $400
+
+
+   rts
+
