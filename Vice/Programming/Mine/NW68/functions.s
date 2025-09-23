2ba3af0cbf58363ef4bf6aef87948aa250a8c772
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-09-23 15:19:14 -0400

    Add files via upload

--- Vice/Programming/Mine/NW68/functions.s
+++ Vice/Programming/Mine/NW68/functions.s
@@ -3,43 +3,18 @@ fill_background:
    stx BACKGROUND
    sty BORDER
 
-   ldx #0
-   lda #0
-@loop:
-   sta $a000,x       ; $8000 + $2000
-   sta $a100,x
-   sta $a200,x
-   sta $a300,x
-   sta $a400,x
-   sta $a500,x
-   sta $a600,x
-   sta $a700,x
-   sta $a800,x
-   sta $a900,x
-   sta $aa00,x
-   sta $ab00,x
-   sta $ac00,x
-   sta $ad00,x
-   sta $ae00,x
-   sta $af00,x
+   lda #<SCREEN_MEM
+   sta 2
+   lda #>SCREEN_MEM
+   sta 3
 
-   sta $b000,x
-   sta $b100,x
-   sta $b200,x
-   sta $b300,x
-   sta $b400,x
-   sta $b500,x
-   sta $b600,x
-   sta $b700,x
-   sta $b800,x
-   sta $b900,x
-   sta $ba00,x
-   sta $bb00,x
-   sta $bc00,x
-   sta $bd00,x
-   sta $be00,x
-   sta $bf00,x
-   inx
-   bne @loop
+   ldy #0
+   lda #0
+   ldx #3
+loop:
+:
+   sta (2),y
+   iny
+   bne :-
 
    rts
