cbe117e92c4b91fc72138324a383b6ef84fb727a
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-09-28 21:40:03 -0400

    2025.09b

--- Vice/Programming/Mine/Math.s
+++ Vice/Programming/Mine/Math.s
@@ -9,9 +9,12 @@ BUFFER = 2                   ; zero page memory address 2
    lda #$01
    sta BUFFER+1
 
-   ; ***** set Y = 3
-   lda #3
+   ; ***** set Y = 4
+   lda #0
    sta BUFFER+2
+
+   ; ***** execute
+   jsr divide
 
    ; ***** print
    lda BUFFER
@@ -23,46 +26,45 @@ BUFFER = 2                   ; zero page memory address 2
 
    rts
 
-
 ; ***************************************************************************************
    ; ***** X / Y
    ; ***** X is 2-byte integer; +0, +1
    ; ***** Y is 1-byte integer; +2
    ; ***** store result in registers +0 & +1; low/high byte, respectively
    ; ***** counter is +3
+   ; ***** no remainder stored
 divide:
+   lda BUFFER+2
+   cmp #1
+   beq do_nothing
+zero:
+   cmp #0
+   beq zero                  ; divide by zero?  Infinite loop.
+
    lda #0
    sta BUFFER+3              ; clear counter
 
+   clc
    ldy BUFFER+2
-loop:
+:
    dec BUFFER
-   beq aaa
-   dey
-   bne loop
-aaa:
-   dec BUFFER+1
+   beq :+                   ; branch to aaa when BUFFER becomes 0
+   dey                       ; loop Y times
+   bne :-
+   inc BUFFER+3              ; counter++
+   ldy BUFFER+2
+   jmp :-
+:
+   dec BUFFER+1              ; decrement high byte
+   bmi done
    lda #$ff
    sta BUFFER
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   jmp :--
+done:
+   inc BUFFER+3
+   lda BUFFER+3
+   sta BUFFER
+do_nothing:
    rts
 
 
