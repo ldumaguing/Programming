8e6a7569e0b2b8520a9c89da2198f687794ff177
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-10-16 14:32:13 -0400

    Add files via upload

--- Vice/Programming/Mine/Line/quadrants.s
+++ Vice/Programming/Mine/Line/quadrants.s
@@ -16,10 +16,35 @@ do_q1_a:
    ldy y1
    sty old_Y
 
+   lda delta_X
+   lsr
+   cmp delta_Y
+   bpl :++
+:
+   ldx x1
+   ldy y1
+   lda #3
+   jsr put_dot
+   jsr redefine_Y1_a
+   inc x1
+   lda x1
+   cmp x2
+   bne :-
+
+
+   ldx x2
+   ldy y2
+   lda #2
+   jsr put_dot
 
 
 
 
+
+
+
+
+   rts
 
 :
    ldx x1
@@ -93,7 +118,47 @@ redefine_Y1:
 
    rts
 
+; =================================================================================================
+redefine_Y1_a:
+   ; A(10,10)
+   ; B(90,80)
 
+   ; ***** x1 - Ax
+   sec
+   lda x1
+   sbc old_X
+
+   ; ***** multiply by delta_Y
+   sta numer
+   lda #0
+   sta numer+1
+   sta multi+1
+   lda delta_Y
+   sta multi
+   jsr do_multiply
+
+   ; ***** divide by delta_X
+   lda delta_X
+   sta denom
+   lda #0
+   sta denom+1
+   jsr do_divide
+
+   lda numer
+   sta y1
+
+   ; ***** add Ay
+   clc
+   lda numer
+   adc old_Y
+   sta y1
+
+   inc y1
+
+   ;lda #15
+   ;sta y1
+
+   rts
 
 
 
