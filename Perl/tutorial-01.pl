c0350a0f7ee617756dae9299ea00a0608930a6d1
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-11-04 15:56:06 -0500

    Add files via upload

--- Perl/tutorial-01.pl
+++ Perl/tutorial-01.pl
@@ -19,13 +19,40 @@ while($count <= 10) {
    $count++;
 }
 
+say "";
+$count = 1;
+unless($count > 5) {   # not a loop.  Enter block if condition is false.
+   say $count;
+   $count++;
+}
+say $count;
 
+say "";
+$count = 1;
+do {
+   say $count;
+   $count++;
+} while($count <= 5);
 
+say "";
+for (my $i = 0; $i <= 10; $i++) {
+   next if ($i % 2 == 0);   # skip even numbers
+   say $i;
+}
 
+say "";
+for (my $i = 0; $i <= 10; $i++) {
+   last if ($i == 7);   # exit loop if 7
+   say $i;
+}
 
+# say "";
+# for (my $i = 0; $i <= 10; $i++) {
+#    redo if ($i == 7);   # do the loop one more time, but doesn't exit block.
+#    say $i;
+# }
 
-
-
-
-
-
+say "";
+for my $i (0..10) {
+   say $i;
+}
