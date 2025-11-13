89e1c48c83e094bbc942b8902dc9bb83033f6190
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-11-13 13:49:40 -0500

    Add files via upload

---
+++ RED RPG/roll_d20.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use diagnostics;
+
+use v5.40.3;
+
+# ***************************************************************************************
+
+say int(rand 20) + 1;
