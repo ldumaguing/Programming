8effd98ffcfb2796898b81f96656908b41519644
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-11-25 16:28:31 -0500

    Add files via upload

--- Perl/The Long Road/foo2.pl
+++ Perl/The Long Road/foo2.pl
@@ -9,13 +9,15 @@ use v5.42;
 use DBI;
 my $conn = DBI->connect("dbi:SQLite:dbname=TLR.db","","");
 
-my $stmt = $conn->prepare("SELECT * FROM img");
-$stmt->execute();
+my $stmt = $conn->prepare("SELECT * FROM img WHERE flag1 = ?");
+$stmt->execute("3");
 
 while (my @row = $stmt->fetchrow_array()) {
-   say $row[0] . ", " . $row[1];
+   say $row[0] . ", " . $row[1] . ", " . $row[2];
 }
 $stmt->finish();
 
+
+$conn->disconnect();
 
 
