dc598bb8a3149dd9c058b0a062147767a01cf0af
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-12-10 15:35:12 -0500

    Add files via upload

---
+++ Perl/test-args.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use diagnostics;
+
+use v5.40.3;
+
+# *************************************************************************************************
+my $arg1 = $ARGV[0];
+
+if (defined $arg1) {
+   say $arg1;
+} else {
+   say "empty";
+}
+
+
+
+# say $arg1;
+
