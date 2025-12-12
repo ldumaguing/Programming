414f2b974ff859594ac64cafad6b49b93f9b8220
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2025-12-12 14:06:59 -0500

    Add files via upload

--- Perl/The Long Road/pl/slurp_to_sql.pl
+++ Perl/The Long Road/pl/slurp_to_sql.pl
@@ -8,6 +8,7 @@ use v5.40.3;
 
 my $scenario_num = 0;
 my $name = "";
+my $map = "";
 
 # ***************************************************************************************
 use DBI;
@@ -28,20 +29,29 @@ sub slurp {
    while (my $line = <$fh>) {
      chomp $line; # Remove trailing newline character
      last if($line =~ /END/);
+
      if($line =~ /scenario_num/) {
         my @num = $line =~ /[0-9].*/g;
         $scenario_num = $num[0];
      }
      if($line =~ /name/) {
-        my @nm = $line =~ /\ [a-zA-Z0-9].*/g;
-        say $nm[0];
-        $name = $nm[0];
+        my @vals = $line =~ /\ [a-zA-Z0-9].*/g;
+        $name = $vals[0];
         $name =~ tr/ //d;
-        say $name;
+     }
+     if($line =~ /map/) {
+        my @vals = $line =~ /\ [a-zA-Z0-9].*/g;
+        $map = $vals[0];
+        my @A = $map =~ /[a-zA-Z0-9].*/g;
+        $map = $A[0];
      }
    }
-
+   
    close $fh;
+   
+   say "scenarion_num: " . $scenario_num;
+   say "name:          " . $name;
+   say "map:           " . $map;
 }
 
 
