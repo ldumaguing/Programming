1c2250d8706d9f02a2ca64b1723f642bc5c51f45
Lawrence Dumaguing <os2nlinux@gmail.com>
2025-12-14 18:02:12 -0500

    2025.12a

--- Perl/The Long Road/pl/generate-unit-list.pl
+++ Perl/The Long Road/pl/generate-unit-list.pl
@@ -7,7 +7,7 @@ use diagnostics;
 use v5.42;
 
 use DBI;
-my $conn = DBI->connect("dbi:SQLite:dbname=TLR.db","","");
+my $conn = DBI->connect("dbi:SQLite:dbname=db/TLR.db","","");
 
 my $stmt = $conn->prepare("SELECT * FROM v_unit_states order by flag1, name");
 $stmt->execute();
