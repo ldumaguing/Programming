4fcdbb07852eb01cc0c83ba00e0b56ff2080bc61
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-01-05 15:59:15 -0500

    Add files via upload

--- Perl/The Long Road/pl/GAME - create map.pl
+++ Perl/The Long Road/pl/GAME - create map.pl
@@ -9,14 +9,15 @@ use v5.42;
 # ***************************************************************************************
 my $conn = DBI->connect( "dbi:SQLite:dbname=db/TLR.db", "", "" );
 
-my $filename  = $ARGV[0];
-my $fname     = $filename;
-my $letterRef = ord('A') + 1;
-
+my $filename = $ARGV[0];
+my $fname    = $filename;
 $fname =~ s/scenario\///;
 $fname =~ s/\.txt//;
 
-say $fname;
+my $letterRef = ord('A') + 1;
+my $gameName  = "";
+my $map_dim   = "";
+
 open my $fh, '<', $filename or die "Cannot open $filename: $!";
 
 my $line = "";
@@ -25,11 +26,65 @@ while ( $line = <$fh> ) {
     last if ( $line =~ /^END/ );
     next if ( $line =~ /^$/ );
 
-    say $line;
+    if ( $line =~ /^name/ ) {
+        my @X = split /:/, $line;
+        $gameName = $X[1];
+        next;
+    }
+    if ( $line =~ /^map_dim/ ) {
+        my @X = split /:/, $line;
+        $map_dim = $X[1];
+        next;
+    }
+    if ( $line =~ /^maps/ ) {
+        my @X = split /:/, $line;
+        instantiate_map( $X[1] );
+        next;
+    }
 }
+
+# say $map_dim;
 
 close $fh;
 $conn->disconnect();
 
 # ***************************************************************************************
+sub instantiate_map {
+    my ($mapStr) = @_;
+    my @maps     = split /,/, $mapStr;
+    my $row      = 0;
+    my $col      = 0;
+    foreach my $letter (@maps) {
+
+        if ( $letter =~ /_/ ) {
+            $col = 0;
+            $row += 1;
+            next;
+        }
+
+        new_map( $col, $row, $letter );
+        $col += 1;
+    }
+}
+
+# *********************************************************
+sub new_map {
+    my ( $col, $row, $letter ) = @_;
+    return if ( $letter =~ /\./ );
+
+    my $mapFile = "Map " . $letter;
+
+    say $gameName . "," . $col . "," . $row . ": " . $mapFile;
+    my $sqlSTMT =
+      "SELECT loc_x, loc_y, flag1, flag2 FROM terrain WHERE mapFile = '"
+      . $mapFile . "'";
+    my $stmt = $conn->prepare($sqlSTMT);
+    $stmt->execute();
+
+    #while ( my @sqlROW = $stmt->fetchrow_array() ) {
+        #say $sqlROW[0] . "," . $sqlROW[1] . "," . $sqlROW[2] . "," . $sqlROW[3];
+    #}
+
+    $stmt->finish();
+}
 
