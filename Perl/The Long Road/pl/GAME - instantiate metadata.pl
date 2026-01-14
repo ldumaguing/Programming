cf868c22ac94237b68c276f75fc28ada1803f2d2
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-01-14 12:24:09 -0500

    Add files via upload

--- Perl/The Long Road/pl/GAME - instantiate metadata.pl
+++ Perl/The Long Road/pl/GAME - instantiate metadata.pl
@@ -10,7 +10,6 @@ use v5.42;
 
 # ***************************************************************************************
 my $argNum = @ARGV;
-say $argNum;
 if ( $argNum < 2 ) {
     say "pl/'GAME - instantiate combatant.pl'   scenario/1-Recon.txt   1";
     exit;
@@ -21,8 +20,7 @@ my $conn = DBI->connect( "dbi:SQLite:dbname=db/TLR.db", "", "" );
 my $filename    = $ARGV[0];
 my $scenario_id = $ARGV[1];
 
-my $gameName = "";
-my $stmt     = "";
+my $stmt = "";
 
 open my $fh, '<', $filename or die "Cannot open $filename: $!";
 
@@ -39,11 +37,25 @@ while ( $line = <$fh> ) {
 
     if ( $line =~ /^name/ ) {
         my @X = split /:/, $line;
-        $gameName = $X[1];
+        my $gameName = $X[1];
+        $stmt =
+            "INSERT INTO scenario (key, id, txt_val) VALUES ('name', "
+          . $scenario_id . ", '"
+          . $gameName . "')";
+        $rs = $conn->prepare($stmt);
+        $rs->execute();
+        $rs->finish();
         next;
     }
-
-    say $line . ", " . $gameName;
+    if ( $line =~ /^upperLeft/ ) {
+        my @X         = split /:/, $line;
+        my $upperLeft = $X[1];
+        my @Y         = split /,/, $upperLeft;
+        say "INSERT INTO scenario (key, id, num1, num2) VALUES ('upperLeft', "
+          . $scenario_id . ", "
+          . $Y[0] . ", "
+          . $Y[1] . ")";
+    }
 }
 
 close $fh;
