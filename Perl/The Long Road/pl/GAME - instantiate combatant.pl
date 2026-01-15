1c6ab2caf295c331788d9f70a7bbb87a05145f74
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-01-15 12:22:50 -0500

    Add files via upload

--- Perl/The Long Road/pl/GAME - instantiate combatant.pl
+++ Perl/The Long Road/pl/GAME - instantiate combatant.pl
@@ -10,7 +10,6 @@ use v5.42;
 
 # ***************************************************************************************
 my $argNum = @ARGV;
-say $argNum;
 if ( $argNum < 2 ) {
     say "pl/'GAME - instantiate combatant.pl'   scenario/1-Recon.txt   1";
     exit;
@@ -21,8 +20,10 @@ my $conn = DBI->connect( "dbi:SQLite:dbname=db/TLR.db", "", "" );
 my $filename    = $ARGV[0];
 my $scenario_id = $ARGV[1];
 
-my $gameName = "";
-my $stmt     = "";
+my $gameName    = "";
+my $stmt        = "";
+my $faction     = 0;      # _flag1_ field of the _unit_ table
+my $instance_id = 1000;
 
 open my $fh, '<', $filename or die "Cannot open $filename: $!";
 
@@ -30,6 +31,7 @@ my $line = "";
 while ( $line = <$fh> ) {
     chomp $line;
     last if ( $line =~ /^END/ );
+    next if ( $line =~ /^\*\*\*/ );
     next if ( $line =~ /^$/ );
 
     if ( $line =~ /^name/ ) {
@@ -37,11 +39,44 @@ while ( $line = <$fh> ) {
         $gameName = $X[1];
         next;
     }
+    if ( $line =~ /^Soviet/ ) {
+        $faction = 2;
+        next;
+    }
+    if ( $line =~ /^American/ ) {
+        $faction = 1;
+        next;
+    }
 
-    say $line . ", " . $gameName;
+    instantiate();
 }
 
 close $fh;
 $conn->disconnect();
 
 # ***************************************************************************************
+sub instantiate {
+    my @KeyVal = split /:/, $line;
+    my $unit   = $KeyVal[0];
+    my $count  = $KeyVal[1];
+
+    # ***** get unit's ID
+    my $stmt =
+        "SELECT id FROM unit WHERE name = '"
+      . $unit
+      . "' and (flag1 & "
+      . $faction . ")";
+    my $rs = $conn->prepare($stmt);
+    $rs->execute();
+
+    while ( my @ROW = $rs->fetchrow_array() ) {
+        say ">>> " . $ROW[0] . ", " . $unit . ": " . $count;
+        for ( my $i = 0 ; $i < $count ; $i++ ) {
+            say $instance_id;
+            $instance_id++;
+        }
+    }
+
+    $rs->finish();
+}
+
