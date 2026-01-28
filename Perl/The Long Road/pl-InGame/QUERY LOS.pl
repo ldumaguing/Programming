af9beaf178019c2171ec24e5558432a3cb4ff8ee
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-01-28 14:50:56 -0500

    Add files via upload

---
+++ Perl/The Long Road/pl-InGame/QUERY LOS.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use diagnostics;
+use DBI;
+use v5.42;
+
+use lib "pl-InGame/module";
+use Hexagon;
+
+# ***************************************************************************************
+my @args = @ARGV;
+
+my $x = @ARGV;
+if ( $x < 2 ) {
+    say "pl-InGame/QUERY\ LOS.pl '0,0'  '10,10'   hex locations";
+    say "                       '1234' '5678'    unit ids";
+    say "                       '1234' '10,10'   mix";
+    say "                       '0,0'  '5678'    mix";
+    exit;
+}
+
+my $scenario_id = 0;
+
+my $conn = DBI->connect( "dbi:SQLite:dbname=db/TLR.db", "", "" );
+
+my $stmt = "SELECT num1 FROM scenario WHERE key = 'currScenario'";
+my $rs   = $conn->prepare($stmt);
+$rs->execute();
+while ( my @ROW = $rs->fetchrow_array() ) {
+    $scenario_id = $ROW[0];
+}
+
+$rs->finish();
+$conn->disconnect();
+
+my @from = ();
+my @to   = ();
+
+# ***** from
+if ( $args[0] =~ /,/ ) {
+    my @Aye = split /,/, $args[0];
+    push @from, $Aye[0];
+    push @from, $Aye[1];
+}
+else {
+    @from = get_hex_loc( $args[0] );
+}
+
+# ***** to
+if ( $args[1] =~ /,/ ) {
+    my @Aye = split /,/, $args[1];
+    push @to, $Aye[0];
+    push @to, $Aye[1];
+}
+else {
+    @to = get_hex_loc( $args[1] );
+}
+
+my @path1 = get_path( $from[0], $from[1], $to[0], $to[1] );
+clear_path();
+my @path2 = get_path( $to[0], $to[1], $from[0], $from[1] );
+foreach my ( $i, $j ) (@path1) {
+    say $i . "," . $j;
+    next;    # increment one more iteration
+}
+say "---";
+foreach my ( $i1, $j1 ) (@path2) {
+    say $i1 . "," . $j1;
+    next;    # increment one more iteration
+}
+
+# ***************************************************************************************
+sub get_hex_loc {
+    my @args    = @_;
+    my @hex_loc = ();
+
+    my $stmt_1 =
+        "SELECT loc_x, loc_y FROM instance WHERE scenario_id = "
+      . $scenario_id
+      . " AND id = "
+      . $args[0];
+    my $rs1 = $conn->prepare($stmt_1);
+    $rs1->execute();
+    while ( my @ROW = $rs1->fetchrow_array() ) {
+        push @hex_loc, $ROW[0];
+        push @hex_loc, $ROW[1];
+    }
+    $rs1->finish();
+
+    $x = @hex_loc;
+    if ( $x == 0 ) {
+        say "unit id " . $args[0] . " doesn't exist";
+        exit;
+    }
+
+    return @hex_loc;
+}
+
--- Perl/The Long Road/pl-InGame/module/Hexagon.pm
+++ Perl/The Long Road/pl-InGame/module/Hexagon.pm
@@ -16,6 +16,7 @@ my $X_hex = cos( ( ( 4 * atan2( 1, 1 ) ) / 180.0 ) * 30.0 );
 
 my @args    = ();
 my $counter = 0;
+my @path    = ();
 
 # ***************************************************************************************
 sub get_cart_distance {
@@ -219,6 +220,98 @@ sub get_hex_distance {
     #say $hex_cursor[0] . " " . $hex_cursor[1];
 
     get_hex_distance( $hex_cursor[0], $hex_cursor[1], $hex_to[0], $hex_to[1] );
+}
+
+# *********************************************************
+sub clear_path {
+    @path = ();    # seems variables are static
+}
+
+# *********************************************************
+sub get_path {
+    @args = @_;
+
+    my @hex_cursor = ( $args[0], $args[1] );
+    my @hex_to     = ( $args[2], $args[3] );
+    if ( $hex_cursor[0] == $hex_to[0] ) {
+        if ( $hex_cursor[1] == $hex_to[1] ) {
+            push @path, $args[2];
+            push @path, $args[3];
+            return @path;
+        }
+    }
+
+    push @path, $args[0];
+    push @path, $args[1];
+
+    my $From_X = $args[0] * $X_hex;
+    my $To_X   = $args[2] * $X_hex;
+    my $From_Y = 0;
+    my $To_Y   = 0;
+    if ( $args[0] % 2 ) {    # is From_X odd
+        $From_Y = $args[1] + 0.5;
+    }
+    else {
+        $From_Y = $args[1];
+    }
+    if ( $args[2] % 2 ) {    # is To_X odd
+        $To_Y = $args[3] + 0.5;
+    }
+    else {
+        $To_Y = $args[3];
+    }
+
+    my $dir_ang = get_degrees( $args[0], $args[1], $args[2], $args[3] );
+
+    #say $dir_ang . " degs";
+
+    #my $degMOD90 = $dir_ang % 90.0;
+    #say $degMOD90 . " (degs % 90)";
+    my @three_hexes = ( 0, 0, 0 );
+    if ( ( 60 <= $dir_ang ) and ( 120 > $dir_ang ) ) {
+        @three_hexes = ( 5, 0, 1 );
+    }
+    if ( ( 0 <= $dir_ang ) and ( 60 > $dir_ang ) ) {
+        @three_hexes = ( 0, 1, 2 );
+    }
+    if ( ( 300 <= $dir_ang ) ) {
+        @three_hexes = ( 1, 2, 3 );
+    }
+    if ( ( 240 <= $dir_ang ) and ( 300 > $dir_ang ) ) {
+        @three_hexes = ( 2, 3, 4 );
+    }
+    if ( ( 180 <= $dir_ang ) and ( 240 > $dir_ang ) ) {
+        @three_hexes = ( 3, 4, 5 );
+    }
+    if ( ( 120 <= $dir_ang ) and ( 180 > $dir_ang ) ) {
+        @three_hexes = ( 4, 5, 0 );
+    }
+
+    my @adj_hex = ( 0, 0 );
+    my $ref_deg = $dir_ang % 180.0;
+
+    my $most_acute = 360;
+    foreach my $adjacent_hex_dir (@three_hexes) {
+        @adj_hex = get_adjacent_hex( $args[0], $args[1], $adjacent_hex_dir );
+
+        #print $adj_hex[0] . ", " . $adj_hex[1] . " --- ";
+        my $delta = abs(
+            (
+                ( get_degrees( $args[0], $args[1], $adj_hex[0], $adj_hex[1] ) )
+                % 180
+            ) - $ref_deg
+        );
+
+        if ( $most_acute > $delta ) {
+            $most_acute = $delta;
+            @hex_cursor = ( $adj_hex[0], $adj_hex[1] );
+        }
+    }
+
+    #say "most acute: " . $most_acute;
+    #say $hex_cursor[0] . " " . $hex_cursor[1];
+
+    get_path( $hex_cursor[0], $hex_cursor[1], $hex_to[0], $hex_to[1] );
 }
 
 # ***************************************************************************************
