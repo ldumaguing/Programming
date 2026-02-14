cfa2acf6bcb6cfb1c459e29658b1d8e0f74b70fe
Lawrence Dumaguing <os2nlinux@gmail.com>
2026-02-14 01:08:06 -0500

    2026.02a

--- The Long Road/py/slurp Map.py
+++ The Long Road/py/slurp Map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import re
+import sqlite3
 
 # ***************************************************************************************
 def save_info(line, map_mode):
@@ -16,18 +17,60 @@ def save_info(line, map_mode):
     if re.search("^BRIDGE", map_mode):
         return
 
+    line = line.upper()
     hexagons = line.split(",")
-    print(hexagons)
-    X = re.search("[0-9]+", hexagons[0])
-    print(X[0])
+    A_ref = ord("A")
+    for hexagon in hexagons:
+        alpha = re.search("[A-Z]+", hexagon)
+        X = ord(alpha[0]) - A_ref
+        Y = re.search("[0-9]+", hexagon)
 
-# ***************************************************************************************
+        print(str(X) + " " + Y[0])
+
+    # print(hexagons)
+    # X = re.search("[0-9]+", hexagons[0])
+    # print(X[0])
+
+
+# *************************************************************************************** main
 if len(sys.argv) < 2:
     print("py/'slurp Map.py'  db/'Map A.txt'")
     exit()
 
+
+conn = sqlite3.connect("db/TLR.db")
+cursor = conn.cursor()
+
+filename = sys.argv[1]
+filename = filename.replace("db/", "")
+filename = filename.replace(".txt", "")
+stmt = "DELETE FROM terrain WHERE mapFile = '" + filename + "'"
+cursor.execute(stmt)
+conn.commit()
+
+
+for loc_x in range(19):
+    for y in range(13):
+        loc_y = y - 1;
+        if (loc_x % 2) & (loc_y < 0):
+            pass
+        else:
+            print(str(loc_x) + ", " + str(loc_y))
+
+
+
+
+
+
+
+
+
+
+conn.close()
+
+
 map_mode = ""
-with open(sys.argv[1], 'r') as file:
+with open(sys.argv[1], "r") as file:
     for line in file:
         line = line.strip()
         if re.search("^$", line):
@@ -58,4 +101,3 @@ with open(sys.argv[1], 'r') as file:
                 map_mode = "BRIDGE"
 
             save_info(line, map_mode)
-
