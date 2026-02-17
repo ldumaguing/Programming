da831036f6fc729f9a85dd247ccb817ebe9a08d6
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-02-17 13:33:31 -0500

    Add files via upload

--- The Long Road/py/instanciate Game Map.py
+++ The Long Road/py/instanciate Game Map.py
@@ -6,18 +6,67 @@ import sqlite3
 missionName = ""
 
 
+def plate_0x0(map_plate, row, col, conn):
+    print("plate 0x0")
+    stmt = "INSERT INTO terrain_instance (gameName, loc_x, loc_y, flag1, flag2) "
+    stmt += "SELECT '" + missionName + "', loc_x, loc_y, flag1, flag2 FROM "
+    stmt += "terrain WHERE mapFile = '" + map_plate + "'"
+    cursor = conn.cursor()
+    cursor.execute(stmt)
+    conn.commit()
+
+
+def plate_0x1(map_plate, row, col, conn):
+    print("plate 0x1")
+    col_shift = 18 * col
+    stmt = "INSERT INTO terrain_instance (gameName, loc_x, loc_y, flag1, flag2) "
+    stmt += "SELECT '" + missionName + "', loc_x + " + str(col_shift) + ", "
+    stmt += "loc_y, flag1, flag2 FROM "
+    stmt += "terrain WHERE mapFile = '" + map_plate + "' AND loc_x > 0"
+    # print(stmt)
+    cursor = conn.cursor()
+    cursor.execute(stmt)
+    conn.commit()
+
+
+def plate_1x0(map_plate, row, col, conn):
+    print("plage 1x0")
+    row_shift = 12 * row
+    stmt = "INSERT INTO terrain_instance (gameName, loc_x, loc_y, flag1, flag2) "
+    stmt += "SELECT '" + missionName + "', loc_x, loc_y + "
+    stmt += str(row_shift) + ", flag1, flag2 FROM terrain WHERE mapFile = '"
+    stmt += map_plate + "' AND loc_y >= 0"
+    # print(stmt)
+    cursor = conn.cursor()
+    cursor.execute(stmt)
+    conn.commit()
+
+
 def put_plate(letter, row, col, conn):
     map_plate = "Map " + letter
-    print("---" + map_plate + "," + str(row) + "," + str(col))
+
+    if row == 0 and col == 0:
+        plate_0x0(map_plate, row, col, conn)
+        return
+
+    if row == 0 and col >= 1:
+        plate_0x1(map_plate, row, col, conn)
+        return
+
+    if row >= 1 and col == 0:
+        plate_1x0(map_plate, row, col, conn)
+        return
 
     for i in range(19):
-        print(i)
+        # print(i)
         if i % 2:
             for j in range(12):
-                print(" " + str(j))
+                pass
+                # print(" " + str(j))
         else:
             for j in range(13):
-                print(" " + str(j-1))
+                pass
+                # print(" " + str(j-1))
 
 
 def instanciate_terrain(line, conn):
@@ -60,6 +109,7 @@ with open(sys.argv[1], "r") as file:
                 missionName = define_mission_name(line)
                 stmt = "DELETE FROM terrain_instance WHERE gameName = '" \
                     + missionName + "'"
+                print(stmt)
                 cursor = conn.cursor()
                 cursor.execute(stmt)
                 conn.commit()
