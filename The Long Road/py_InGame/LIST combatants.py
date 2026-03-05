57f36550133a964e8e99a9ac2bbf66579d188b0c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-05 14:15:20 -0500

    Add files via upload

--- The Long Road/py_InGame/LIST combatants.py
+++ The Long Road/py_InGame/LIST combatants.py
@@ -8,8 +8,13 @@ stmt = "SELECT num1 FROM scenario WHERE key = 'currScenario' AND "
 stmt += "id = 0"
 scenario_id = SQL.get_field_value(CONN, stmt)
 
-stmt = "SELECT id, unit_name"
-
-
+stmt = "SELECT id, unit_name, faction, loc_x, loc_y FROM instance "
+stmt += f"WHERE scenario_id = {scenario_id} ORDER BY faction, "
+stmt += "unit_name"
+cursor = CONN.cursor()
+cursor.execute(stmt)
+rows = cursor.fetchall()
+for row in rows:
+    print(f"{row[2]: <10}{row[1]: <13}{row[0]: <5}")
 
 CONN.close()
