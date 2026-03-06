b23c2441abc38b2ffd2b86cd987ec1c793866b90
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-06 12:06:09 -0500

    Update MOVE combatant.py

--- The Long Road/py_InGame/MOVE combatant.py
+++ The Long Road/py_InGame/MOVE combatant.py
@@ -27,38 +27,46 @@ stmt = "SELECT loc_x, loc_y FROM instance WHERE "
 stmt += f"scenario_id = {scenario_id} AND "
 stmt += f"id = {unit_id}"
 rs = SQL.get_2columns(CONN, stmt)[0]
-x = rs[0]
-y = rs[1]
+x = int(rs[0])
+y = int(rs[1])
+direction = int(sys.argv[1])
 if x%2: # x is odd
-    if sys.argv[1] == 1:
+    if direction == 1:
         y -= 1
-    elif sys.argv[1] == 2:
+    elif direction == 2:
         x += 1
         y -= 1
-    elif sys.argv[1] == 3:
+    elif direction == 3:
         x += 1
-    elif sys.argv[1] == 4:
+    elif direction == 4:
         y += 1
-    elif sys.argv[1] == 5:
+    elif direction == 5:
         x -= 1
     else:
         x -= 1
         y -= 1
 else:
-    if sys.argv[1] == 1:
+    if direction == 1:
         y -= 1
-    elif sys.argv[1] == 2:
+    elif direction == 2:
         x += 1
-    elif sys.argv[1] == 3:
+    elif direction == 3:
         x += 1
         y += 1
-    elif sys.argv[1] == 4:
+    elif direction == 4:
         y += 1
-    elif sys.argv[1] == 5:
+    elif direction == 5:
         x -= 1
         y -= 1
     else:
         x -= 1
+
+stmt = "UPDATE instance SET "
+stmt += f"loc_x = {x}, "
+stmt += f"loc_y = {y} WHERE "
+stmt += f"id = {unit_id} AND "
+stmt += f"scenario_id = {scenario_id}"
+print(stmt)
 
 CONN.close()
 
