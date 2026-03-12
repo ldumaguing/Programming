8bb9ec406acad066bb44bb30ef74a7cbf3366512
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-12 15:17:49 -0400

    Add files via upload

--- The Long Road/TUI/MAIN.py
+++ The Long Road/TUI/MAIN.py
@@ -5,19 +5,42 @@ from textual.widgets import Footer, Header
 
 
 class TUIApp(App):
-    BINDINGS = [("w", "move_forward", "Toggle dark mode")]
+    BINDINGS = [
+        ("w", "move_forward", ""),
+        ("a", "turn_left", ""),
+        ("d", "turn_right", ""),
+        ("s", "about_face", ""),
+    ]
+    CONN = sqlite3.connect("db/TLR.db")
 
     def compose(self):
         yield Header()
         yield Footer()
 
+    def action_about_face(self):
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+        cmd.about_face(self.CONN)
+
+    def action_turn_right(self):
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+        cmd.turn_right(self.CONN)
+
+    def action_turn_left(self):
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+        cmd.turn_left(self.CONN)
+
     def action_move_forward(self):
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
-        CONN = sqlite3.connect("db/TLR.db")
-        cmd.move_forward(CONN)
-        CONN.close()
+        cmd.move_forward(self.CONN)
+
 
 if __name__ == "__main__":
     app = TUIApp()
--- The Long Road/TUI/py_package/COMMANDS.py
+++ The Long Road/TUI/py_package/COMMANDS.py
@@ -2,6 +2,76 @@ import sys
 import py_package.SQL as SQL
 
 
+def about_face(CONN):
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'unitFocus'"
+    unit_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'currScenario'"
+    scenario_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT status FROM instance WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    X = int(SQL.get_field_value(CONN, stmt))
+    status = X & 8191  # 1FFF
+    facing = X >> 13
+    facing += 3
+    facing %= 6
+    facing = facing << 13
+    status = facing | status
+    stmt = f"UPDATE instance SET status = {status} WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    SQL.execute_sql(CONN, stmt)
+
+
+def turn_right(CONN):
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'unitFocus'"
+    unit_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'currScenario'"
+    scenario_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT status FROM instance WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    X = int(SQL.get_field_value(CONN, stmt))
+    status = X & 8191  # 1FFF
+    facing = X >> 13
+    facing += 1
+    facing %= 6
+    facing = facing << 13
+    status = facing | status
+    stmt = f"UPDATE instance SET status = {status} WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    SQL.execute_sql(CONN, stmt)
+
+
+def turn_left(CONN):
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'unitFocus'"
+    unit_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT num1 FROM scenario WHERE "
+    stmt += "key = 'currScenario'"
+    scenario_id = SQL.get_field_value(CONN, stmt)
+
+    stmt = "SELECT status FROM instance WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    X = int(SQL.get_field_value(CONN, stmt))
+    status = X & 8191  # 1FFF
+    facing = X >> 13
+    facing -= 1
+    if facing < 0:
+        facing = 5
+    facing = facing << 13
+    status = facing | status
+    stmt = f"UPDATE instance SET status = {status} WHERE "
+    stmt += f"id = {unit_id} AND scenario_id = {scenario_id}"
+    SQL.execute_sql(CONN, stmt)
+
+
 def move_forward(CONN):
     stmt = "SELECT num1 FROM scenario WHERE "
     stmt += "key = 'unitFocus'"
@@ -23,7 +93,7 @@ def move_forward(CONN):
     print(facing)
     direction = facing + 1
     print(direction)
-    if x%2: # x is odd
+    if x % 2:  # x is odd
         print("odd")
         if direction == 1:
             y -= 1
