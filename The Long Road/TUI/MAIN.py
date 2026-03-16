b76c6f23a98bc0136f5ad20da9d291df21da4089
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-16 16:36:28 -0400

    Add files via upload

--- The Long Road/TUI/MAIN.py
+++ The Long Road/TUI/MAIN.py
@@ -10,36 +10,52 @@ class TUIApp(App):
         ("a", "turn_left", ""),
         ("d", "turn_right", ""),
         ("s", "about_face", ""),
+        ("backspace", "set_zero", ""),
     ]
 
     CONN = sqlite3.connect("db/TLR.db")
+    count = 0
 
     def compose(self):
         yield Header()
         yield Footer()
         yield Log()
 
-    def action_about_face(self):
+    def action_set_zero(self):
+        self.count = 0
         log = self.query_one(Log)
-        log.write_line("about face")
+        log.clear()
+
+    def action_about_face(self):
+        self.count += 1
+        log = self.query_one(Log)
+        foo = f"{self.count}"
+        log.write_line(foo + " about face")
         # cmd.about_face(self.CONN)
 
     def action_turn_right(self):
+        self.count += 1
         log = self.query_one(Log)
-        log.write_line("turn right")
+        foo = f"{self.count}"
+        log.write_line(foo + " turn right")
         # cmd.turn_right(self.CONN)
 
     def action_turn_left(self):
+        self.count += 1
         log = self.query_one(Log)
-        log.write_line("turn left")
+        foo = f"{self.count}"
+        log.write_line(foo + " turn left")
         # cmd.turn_left(self.CONN)
 
     def action_move_forward(self):
+        self.count += 1
         log = self.query_one(Log)
-        log.write_line("move forward")
+        foo = f"{self.count}"
+        log.write_line(foo + " move forward")
         # cmd.move_forward(self.CONN)
 
 
 if __name__ == "__main__":
     app = TUIApp()
     app.run()
+
