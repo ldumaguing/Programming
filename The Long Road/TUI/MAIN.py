c0a2530be5c111bf86c93ec008efe1fcc22edb3c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-16 13:47:39 -0400

    Add files via upload

--- The Long Road/TUI/MAIN.py
+++ The Long Road/TUI/MAIN.py
@@ -1,7 +1,7 @@
 import py_package.COMMANDS as cmd
 import sqlite3
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Log
 
 
 class TUIApp(App):
@@ -11,35 +11,33 @@ class TUIApp(App):
         ("d", "turn_right", ""),
         ("s", "about_face", ""),
     ]
+
     CONN = sqlite3.connect("db/TLR.db")
 
     def compose(self):
         yield Header()
         yield Footer()
+        yield Log()
 
     def action_about_face(self):
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
-        cmd.about_face(self.CONN)
+        log = self.query_one(Log)
+        log.write_line("about face")
+        # cmd.about_face(self.CONN)
 
     def action_turn_right(self):
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
-        cmd.turn_right(self.CONN)
+        log = self.query_one(Log)
+        log.write_line("turn right")
+        # cmd.turn_right(self.CONN)
 
     def action_turn_left(self):
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
-        cmd.turn_left(self.CONN)
+        log = self.query_one(Log)
+        log.write_line("turn left")
+        # cmd.turn_left(self.CONN)
 
     def action_move_forward(self):
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
-        cmd.move_forward(self.CONN)
+        log = self.query_one(Log)
+        log.write_line("move forward")
+        # cmd.move_forward(self.CONN)
 
 
 if __name__ == "__main__":
