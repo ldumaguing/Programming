6532eaaf8e83ccac62dd6fe01b9965d5c0b33cf2
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-26 14:48:40 -0400

    Add files via upload

--- The Long Road/foo.py
+++ The Long Road/foo.py
@@ -3,31 +3,36 @@ import sqlite3
 
 print ("""<!DOCTYPE html>
 <html>
-   <head>
-      <meta charset="UTF-8">
-      <title>The Long Road</title>
-   </head>
-   <body>
-      <table>"""
+\t<head>
+\t\t<meta charset="UTF-8">
+\t\t<title>The Long Road</title>
+\t</head>
+\t<body>
+\t\t<table>"""
 )
 
 CONN = sqlite3.connect("db/TLR.db")
 
-stmt = "SELECT * FROM img WHERE flag1 = 0"
+stmt = "SELECT * FROM img WHERE (flag1 & 1) or (flag3 & (1<<6)) ORDER BY id"
 cursor = CONN.cursor()
 cursor.execute(stmt)
 rows = cursor.fetchall()
 for row in rows:
-    html_row = f"\t\t<tr><td>{row[0]}</td>"
-    html_row += f"<td>{row[1]}</td>"
+    html_row = f"\t\t\t<tr><td>{row[0]}</td>"
+    # html_row += f"<td>{row[1]}</td>" # file
     html_row += f"<td><img src=\"TLR/{row[1]}\"></td>"
+    html_row += f"<td>{hex(row[2])}</td>"
+    html_row += f"<td>{hex(row[9])}</td>"
+    html_row += f"<td>{hex(row[11])}</td>"
+    html_row += f"<td>{hex(row[12])}</td>"
+    html_row += f"<td>{hex(row[13])}</td>"
     html_row += "</tr>"
     print(html_row)
 
 CONN.close()
 
-print("""      </table>
-   </body>
+print("""\t\t</table>
+\t</body>
 </html>
 """
 )
