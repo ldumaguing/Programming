0b8bda452b50cccca9cbf5177aea6ea9d1152a3b
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-03-26 11:26:21 -0400

    Add files via upload

---
+++ The Long Road/HTML img.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+print ("""<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>The Long Road</title>
+   </head>
+   <body>
+      <table>"""
+)
+
+CONN = sqlite3.connect("db/TLR.db")
+
+stmt = "SELECT * FROM img"
+cursor = CONN.cursor()
+cursor.execute(stmt)
+rows = cursor.fetchall()
+for row in rows:
+    html_row = f"\t\t<tr><td>{row[0]}</td>"
+    html_row += f"<td><img src=\"TLR/{row[1]}\"></td>"
+    html_row += "</tr>"
+    print(html_row)
+
+CONN.close()
+
+print("""      </table>
+   </body>
+</html>
+"""
+)
