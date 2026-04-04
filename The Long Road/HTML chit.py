986f190e51b89887c385029a744a545e90a8ce1c
Lawrence Dumaguing <os2nlinux@gmail.com>
2026-04-03 21:28:36 -0400

    2026.04a

---
+++ The Long Road/HTML chit.py
@@ -0,0 +1,38 @@
+import sqlite3
+import re
+
+
+# ******************************************************************************
+print ("""<!DOCTYPE html>
+<html>
+\t<head>
+\t\t<meta charset="UTF-8">
+\t\t<title>The Long Road</title>
+\t</head>
+\t<body>
+\t\t<table>"""
+)
+
+CONN = sqlite3.connect("db/TLR.db")
+
+stmt = "SELECT * FROM v_chit "
+stmt += "ORDER BY id"
+cursor = CONN.cursor()
+cursor.execute(stmt)
+rows = cursor.fetchall()
+for row in rows:
+    html_row = "<tr>"
+    html_row += f"<td>{row[0]}</td>"
+    html_row += f"<td><img src=\"TLR/{row[3]}\"</td>"
+    html_row += f"<td><img src=\"TLR/{row[4]}\"</td>"
+    html_row += f"<td>{row[1]}</td>"
+    html_row += "</tr>"
+    print(html_row)
+
+CONN.close()
+
+print("""\t\t</table>
+\t</body>
+</html>
+"""
+)
