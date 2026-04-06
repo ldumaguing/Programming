75afd6c89e5cbf584f2e094209f2c4f47914f555
Lawrence Dumaguing <os2nlinux@gmail.com>
2026-04-05 21:36:46 -0400

    2026.04a

---
+++ The Long Road/py/2 - instanciate combatants.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import py_package.combatants as cmbat
+import sys
+import re
+import sqlite3
+
+
+scenario_id = 0
+
+
+def get_scenario_id(line):
+    X = line.split(":")
+    return int(X[1])
+
+
+# ************************************************************************ main
+if len(sys.argv) < 2:
+    print("append <scenario file>")
+    exit()
+
+faction = ""
+
+conn = sqlite3.connect("db/TLR.db")
+
+
+with open(sys.argv[1], "r") as file:
+    for line in file:
+        line = line.strip()
+        if re.search("^END", line):
+            conn.close()
+            exit()
+
+        if re.search("^id", line):
+            scenario_id = get_scenario_id(line)
+            print(scenario_id)
+            stmt = f"DELETE FROM instance WHERE scenario_id = {scenario_id}"
+            cmbat.sql_exec_stmt(conn, stmt)
+            continue
+
+        if re.search("^$", line) or re.search("^[\\*/#;\\-=]+", line):
+            faction = ""
+            continue
+        else:
+            if re.search("^American", line):
+                if re.search(faction, line) != 0:
+                    faction = "American"
+                    continue
+            if re.search("^Soviet", line):
+                if re.search(faction, line) != 0:
+                    faction = "Soviet"
+                    continue
+            cmbat.place(conn, faction, line, scenario_id)
---
+++ The Long Road/py/py_package/combatants.py
@@ -0,0 +1,58 @@
+import re
+
+
+def sql_exec_stmt(conn, stmt):
+    cursor = conn.cursor()
+    cursor.execute(stmt)
+    conn.commit()
+    cursor.close()
+
+
+# *****************************************************************************
+def place(conn, faction, line, scenario_id):
+    if re.search("^$", faction):
+        return
+
+    X = line.split(";")
+
+    infos = get_unit_infos(conn, X[1], faction)
+
+    stmt = "INSERT INTO instance (id, scenario_id, unit_id, unit_name, "
+    stmt += "img_id, faction) VALUES ("
+    stmt += f"{X[0]}, {scenario_id}, {infos[0]}, '{X[1]}', {infos[1]}, "
+    stmt += f"'{faction}')"
+    sql_exec_stmt(conn, stmt)
+
+
+# *************************************
+def get_unit_infos(conn, unit_name, faction):
+    infos = ()
+    stmt = ""
+    if re.search("Infantry", unit_name):
+        stmt = get_stmt_infantry_faction_id(conn, faction)
+    else:
+        stmt = f"SELECT id, front FROM unit WHERE name = '{unit_name}'"
+
+    print(stmt)
+    cursor = conn.cursor()
+    cursor.execute(stmt)
+    conn.commit()
+    row = cursor.fetchone()
+    infos = row
+    cursor.close()
+
+    return infos
+
+
+# *****************
+def get_stmt_infantry_faction_id(conn, faction):
+    stmt = ""
+    bit = ""
+    if re.search("^Americ", faction):
+        bit = "(flag1 & (1<<0))"
+    else:
+        bit = "(flag1 & (1<<1))"
+    stmt = "SELECT id, front FROM unit WHERE name = 'Infantry' AND "
+    stmt += f"{bit}"
+
+    return stmt
