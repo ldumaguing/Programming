ea8af5e20c52ec77f1ee97e6bf55ed44f99f95e6
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-04-10 12:13:41 -0400

    Add files via upload

---
+++ The Long Road/HTML unit.py
@@ -0,0 +1,40 @@
+import sqlite3
+import re
+
+
+# ******************************************************************************
+print ("""<!DOCTYPE html>
+<html>
+\t<head>
+\t\t<meta charset="UTF-8">
+\t\t<title>Unit</title>
+\t</head>
+\t<body>
+\t\t<table>"""
+)
+
+CONN = sqlite3.connect("db/TLR.db")
+
+stmt = "SELECT * FROM v_mv_unit "
+stmt += "ORDER BY descrip"
+cursor = CONN.cursor()
+cursor.execute(stmt)
+rows = cursor.fetchall()
+for row in rows:
+    html_row = "<tr>"
+    html_row += f"<td>{row[0]}</td>"
+    html_row += f"<td><img src=\"../TLR/{row[2]}\"</td>"
+    html_row += f"<td><img src=\"../TLR/{row[3]}\"</td>"
+    html_row += f"<td><img src=\"../TLR/{row[4]}\"</td>"
+    html_row += f"<td><img src=\"../TLR/{row[5]}\"</td>"
+    html_row += f"<td>{row[1]}</td>"
+    html_row += "</tr>"
+    print(html_row)
+
+CONN.close()
+
+print("""\t\t</table>
+\t</body>
+</html>
+"""
+)
