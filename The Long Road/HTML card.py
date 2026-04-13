049087d2ea364021e6236946f92df72cacb8489e
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-04-13 16:04:14 -0400

    Add files via upload

---
+++ The Long Road/HTML card.py
@@ -0,0 +1,37 @@
+import sqlite3
+import re
+
+
+# ******************************************************************************
+print ("""<!DOCTYPE html>
+<html>
+\t<head>
+\t\t<meta charset="UTF-8">
+\t\t<title>Card</title>
+\t</head>
+\t<body>
+\t\t<table>"""
+)
+
+CONN = sqlite3.connect("db/TLR.db")
+
+# stmt = "SELECT * FROM card "
+stmt = "select * from card where flag1 != (flag1 | (1<<7)) "
+stmt += "ORDER BY id"
+cursor = CONN.cursor()
+cursor.execute(stmt)
+rows = cursor.fetchall()
+for row in rows:
+    html_row = "\t\t\t<tr>"
+    html_row += f"<td>{row[0]}</td>"
+    html_row += f"<td><img src=\"../TLR/{row[1]}\"</td>"
+    html_row += "</tr>"
+    print(html_row)
+
+CONN.close()
+
+print("""\t\t</table>
+\t</body>
+</html>
+"""
+)
