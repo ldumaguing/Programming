668bd5e7757cf369996692e486bbcc17848863ac
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-06-23 14:20:42 -0400

    Update main.zig

--- Zig/learing/a1_args/src/main.zig
+++ Zig/learing/a1_args/src/main.zig
@@ -3,8 +3,8 @@ const print = std.debug.print;
 
 pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
-
     const args = try init.minimal.args.toSlice(arena);
+
     for (args) |arg| {
         std.log.info("arg: {s}", .{arg});
         print("---> {s}\n", .{arg});
