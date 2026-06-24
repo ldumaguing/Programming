6cdbf8209408650d8c9d771d2c1aa05eb3941819
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-06-24 14:38:05 -0400

    Add files via upload

---
+++ Zig/sqlite3/test1/src/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+const c = @cImport({
+    @cInclude("sqlite3.h");
+});
+
+pub fn main() !void {
+    var db: ?*c.sqlite3 = undefined;
+    var stmt: ?*c.sqlite3_stmt = undefined;
+
+    _ = c.sqlite3_open("fish.db", &db);
+    if (db == null) {
+        print("failed to open\n", .{});
+        return;
+    }
+
+    _ = c.sqlite3_prepare_v2(db, "select * from laptop", -1, &stmt, null);
+    var count: i32 = 0;
+    while (c.sqlite3_step(stmt) != c.SQLITE_DONE) {
+        count += 1;
+        print("{d}\n", .{count});
+    }
+}
