b9d01765b287bba7ae99d13d0442fe57d5bb4878
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-06-24 16:47:57 -0400

    Add files via upload

---
+++ Zig/learing/a2_allocator/src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+const Allocator = std.mem.Allocator;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const result = try concat(allocator, "foo123", "bar456");
+    print("--> {s}\n", .{result});
+}
+
+fn concat(allocator: Allocator, a: []const u8, b: []const u8) ![]u8 {
+    const result = try allocator.alloc(u8, a.len + b.len);
+    @memcpy(result[0..a.len], a);
+    @memcpy(result[a.len..], b);
+    return result;
+}
