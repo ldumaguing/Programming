9ddd960f6b6dd6864b460861c9bd8dd872075309
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-07-15 15:57:17 -0400

    Add files via upload

--- Zig/The Long Road/html_Catalog.zig
+++ Zig/The Long Road/html_Catalog.zig
@@ -44,7 +44,8 @@ fn show_imgs(db: ?*c.sqlite3) void {
     const query =
         \\SELECT id, file, flag1, apf_val, apf_rng,
         \\apf2_val, apf2_rng,
-        \\hef_val, hef_rng
+        \\hef_val, hef_rng,
+        \\hef2_val, hef2_rng
         \\FROM img WHERE mf_val >= 0
     ;
 
@@ -66,6 +67,8 @@ fn show_imgs(db: ?*c.sqlite3) void {
         const apf2_rng = c.sqlite3_column_text(stmt, 6);
         const hef_val = c.sqlite3_column_int64(stmt, 7);
         const hef_rng = c.sqlite3_column_text(stmt, 8);
+        const hef2_val = c.sqlite3_column_int64(stmt, 9);
+        const hef2_rng = c.sqlite3_column_text(stmt, 10);
 
         print("   <tr>", .{});
         print("<td>{d}</td><td><img src=\"TLR/{s}\"></td>", .{ id, fname });
@@ -80,41 +83,52 @@ fn show_imgs(db: ?*c.sqlite3) void {
 
         if (hef_val >= 0) {
             print("<td valign=\"top\" style=\"border: 1px solid #ccc\">", .{});
-            process_HEF(hef_val, hef_rng);
+            process_HEF(flag1, hef_val, hef_rng);
+            if (hef2_val >= 0)
+                process_HEF2(hef2_val, hef2_rng);
             print("</td>", .{});
         }
 
         print("</tr>\n", .{});
-
-        // const string_p = c.sqlite3_column_text(stmt, 1);
-        // if (string_p == null) {
-        //     std.debug.print("{d}\n", .{img_id});
-        // } else {
-        //     const fname = std.mem.span(string_p);
-        //     std.debug.print("{s}\n", .{fname});
-        // }
     }
 }
 
-fn process_HEF(hef_val: i64, hef_rng: [*c]const u8) void {
-    print("HE: {d}", .{hef_val});
+fn process_HEF2(hef2_val: i64, hef2_rng: [*c]const u8) void {
+    print("<b>HE2:</b> {d}", .{hef2_val});
+    print(" ({s})<br>", .{hef2_rng});
+}
+
+fn process_HEF(flag1: i64, hef_val: i64, hef_rng: [*c]const u8) void {
+    print("<b>HE:</b> {d}", .{hef_val});
+    if ((flag1 & (3 << 6)) > 0) {
+        if ((flag1 & (1 << 6)) > 0)
+            print("<sup>-1</sup>", .{})
+        else
+            print("<sup>+1</sup>", .{});
+    }
     print(" ({s})<br>", .{hef_rng});
+    if ((flag1 & (1 << 8)) > 0) {
+        print("May damage Armor <= 4.<br>", .{});
+    }
+    if ((flag1 & (1 << 9)) > 0) {
+        print("Minimum range of 2.<br>", .{});
+    }
+    if ((flag1 & (1 << 10)) > 0) {
+        print("Range effects do not apply.<br>", .{});
+    }
 }
 
 fn process_APF2(apf2_val: i64, apf2_rng: [*c]const u8) void {
-    print("AP2: {d}", .{apf2_val});
+    print("<b>AP2:</b> {d}", .{apf2_val});
     print(" ({s})<br>", .{apf2_rng});
-    //    print("</td>", .{});
 }
 
 fn process_APF(flag1: i64, apf_val: i64, apf_rng: [*c]const u8) void {
     if ((flag1 & (1 << 2)) > 0)
-        print("Wired Guided Missiles<br>", .{});
-    //print("asdf", .{});
-    //print("{d},{d},{s}", .{ flag1, apf_val, apf_rng });
+        print("Wired guided missiles.<br>", .{});
     if ((flag1 & (1 << 5)) > 0)
         print("Range effects do not apply.<br>", .{});
-    print("AP: {d}", .{apf_val});
+    print("<b>AP:</b> {d}", .{apf_val});
     if ((flag1 & (3 << 3)) > 0) {
         if ((flag1 & (1 << 3)) > 0)
             print("<sup>-1</sup>", .{})
