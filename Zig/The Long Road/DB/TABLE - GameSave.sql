6f04c497056685dc9c7c37a579cee2d7f0e5aa95
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-07-21 16:04:45 -0400

    Update TABLE - GameSave.sql

--- Zig/The Long Road/DB/TABLE - GameSave.sql
+++ Zig/The Long Road/DB/TABLE - GameSave.sql
@@ -1,11 +1,16 @@
 DROP TABLE IF EXISTS GameSave;
 CREATE TABLE GameSave (
-    id       TEXT NOT NULL,
-    attrib   TEXT NOT NULL,
-    val_num  INTEGER DEFAULT 0,
-    val_real REAL DEFAULT 0.0,
-    val_text TEXT DEFAULT ''
+    id        TEXT NOT NULL,
+    attrib    TEXT NOT NULL,
+    val_int0  INTEGER DEFAULT 0,
+    val_int1  INTEGER DEFAULT 0,
+    val_int2  INTEGER DEFAULT 0,
+    val_real0 REAL DEFAULT 0.0,
+    val_real1 REAL DEFAULT 0.0,
+    val_real2 REAL DEFAULT 0.0,
+    val_text  TEXT DEFAULT ''
 );
+
 
 
 
