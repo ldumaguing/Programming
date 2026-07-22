f7cb97ffc4bb955df4e92bfd558f3316cfece8e0
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-07-22 10:26:34 -0400

    Add files via upload

--- Zig/The Long Road/DB/TABLE - COMBATANT.sql
+++ Zig/The Long Road/DB/TABLE - COMBATANT.sql
@@ -21,7 +21,7 @@ CREATE TABLE COMBATANT (
 --    DDD.file1,
 --    DDD.file2,
 --    DDD.file3,
---    img.file       wreck,
+--    IMG.file            wreck,
 --    DDD.faction
 -- from (
 -- select
@@ -30,7 +30,7 @@ CREATE TABLE COMBATANT (
 --    CCC.file0,
 --    CCC.file1,
 --    CCC.file2,
---    img.file       file3,
+--    IMG.file            file3,
 --    CCC.wreck,
 --    CCC.faction
 -- from (
@@ -39,7 +39,7 @@ CREATE TABLE COMBATANT (
 --    BBB.descrip,
 --    BBB.file0,
 --    BBB.file1,
---    img.file       file2,
+--    IMG.file            file2,
 --    BBB.state3,
 --    BBB.wreck,
 --    BBB.faction
@@ -48,46 +48,45 @@ CREATE TABLE COMBATANT (
 --    AAA.id,
 --    AAA.descrip,
 --    AAA.file0,
---    img.file       file1,
+--    IMG.file            file1,
 --    AAA.state2,
 --    AAA.state3,
 --    AAA.wreck,
 --    AAA.faction
 -- from (
 -- select
---    combatant.id,
---    combatant.descrip,
---    img.file       file0,
---    combatant.state1,
---    combatant.state2,
---    combatant.state3,
---    combatant.wreck,
---    combatant.faction
+--    COMBATANT.id,
+--    COMBATANT.descrip,
+--    IMG.file            file0,
+--    COMBATANT.state1,
+--    COMBATANT.state2,
+--    COMBATANT.state3,
+--    COMBATANT.wreck,
+--    COMBATANT.faction
 -- from
---    combatant, img
+--    COMBATANT, IMG
 -- where
---    combatant.state0 = img.id
--- ) AAA, img
+--    COMBATANT.state0 = IMG.id
+-- ) AAA, IMG
 -- where
---    AAA.state1 = img.id
--- ) BBB, img
+--    AAA.state1 = IMG.id
+-- ) BBB, IMG
 -- where
---    BBB.state2 = img.id
--- ) CCC, img
+--    BBB.state2 = IMG.id
+-- ) CCC, IMG
 -- where
---    CCC.state3 = img.id
--- ) DDD, img
+--    CCC.state3 = IMG.id
+-- ) DDD, IMG
 -- where
---    DDD.wreck = img.id
+--    DDD.wreck = IMG.id
 -- ;
 
 
--- 
--- 
--- 
--- 
--- 
--- 
+
+
+
+
+
 -- .mode csv
 -- .import chit.csv chit
 -- 
