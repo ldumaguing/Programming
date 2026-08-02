55a79fa12fd12bce4b6d51e104c625d6400c4d9f
Lawrence Dumaguing <os2nlinux@gmail.com>
2026-08-01 21:14:40 -0400

    2026.08a

--- Zig/The Long Road/viewMap/src/main.zig
+++ Zig/The Long Road/viewMap/src/main.zig
@@ -1,15 +1,28 @@
-// port of raylib example https://github.com/raysan5/raylib/blob/master/examples/core/core_2d_camera_mouse_zoom.c
-
 const rl = @import("raylib");
+const std = @import("std");
+const print = std.debug.print;
+
+const sqlite = @import("lib/SQLite.zig");
+const c64 = @import("lib/C64.zig");
 
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const sql = sqlite.SQLite.init();
+    defer sql.close();
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - 2d camera mouse zoom");
+    print("{d}\n", .{sql.currSession});
+
+    const screenWidth = 1227;
+    const screenHeight = 690;
+    const hex_width = screenWidth / 18;
+    const hex_height = screenHeight / 12;
+    const halfY: i32 = hex_height / 2;
+
+    print("{d},{d},{d}\n", .{ hex_width, hex_height, halfY });
+
+    rl.initWindow(screenWidth, screenHeight, "View Map");
     defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(1); // Set our game to run at 60 frames-per-second
 
     var camera = rl.Camera2D{
         .target = .{ .x = 0, .y = 0 },
@@ -18,21 +31,10 @@ pub fn main() anyerror!void {
         .rotation = 0,
     };
 
-    var zoomMode: i32 = 0; // 0-Mouse Wheel, 1-Mouse Move
-
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    rl.setTargetFPS(6); // Set our game to run at 60 frames-per-second
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        if (rl.isKeyPressed(.one)) {
-            zoomMode = 0;
-        } else if (rl.isKeyPressed(.two)) {
-            zoomMode = 1;
-        }
-
         // Translate based on mouse right click
         if (rl.isMouseButtonDown(.right)) {
             var delta = rl.getMouseDelta();
@@ -40,79 +42,45 @@ pub fn main() anyerror!void {
             camera.target = rl.math.vector2Add(camera.target, delta);
         }
 
-        if (zoomMode == 0) {
-            // Zoom based on mouse wheel
-            const wheel = rl.getMouseWheelMove();
-            if (wheel != 0) {
-                // Get the world point that is under the mouse
-                const mouseWorldPos = rl.getScreenToWorld2D(rl.getMousePosition(), camera);
+        // Zoom based on mouse wheel
+        const wheel = rl.getMouseWheelMove();
+        if (wheel != 0) {
+            const mouseWorldPos = rl.getScreenToWorld2D(rl.getMousePosition(), camera);
 
-                // Set the offset to where the mouse is
-                camera.offset = rl.getMousePosition();
+            camera.offset = rl.getMousePosition();
 
-                // Set the target to match, so that the camera maps the world space point
-                // under the cursor to the screen space point under the cursor at any zoom
-                camera.target = mouseWorldPos;
+            camera.target = mouseWorldPos;
 
-                // Zoom increment
-                var scaleFactor = 1.0 + (0.25 * @abs(wheel));
-                if (wheel < 0) {
-                    scaleFactor = 1.0 / scaleFactor;
-                }
-                camera.zoom = rl.math.clamp(camera.zoom * scaleFactor, 0.125, 64.0);
+            // Zoom increment
+            var scaleFactor = 1.0 + (0.25 * @abs(wheel));
+            if (wheel < 0) {
+                scaleFactor = 1.0 / scaleFactor;
             }
-        } else {
-            // Zoom based on left click
-            if (rl.isMouseButtonPressed(.left)) {
-                // Get the world point that is under the mouse
-                const mouseWorldPos = rl.getScreenToWorld2D(rl.getMousePosition(), camera);
-
-                // Set the offset to where the mouse is
-                camera.offset = rl.getMousePosition();
-
-                // Set the target to match, so that the camera maps the world space point
-                // under the cursor to the screen space point under the cursor at any zoom
-                camera.target = mouseWorldPos;
-            }
-            if (rl.isMouseButtonDown(.left)) {
-                // Zoom increment
-                const deltaX = rl.getMouseDelta().x;
-                var scaleFactor = 1.0 + (0.01 * @abs(deltaX));
-                if (deltaX < 0) {
-                    scaleFactor = 1.0 / scaleFactor;
-                }
-                camera.zoom = rl.math.clamp(camera.zoom * scaleFactor, 0.125, 64.0);
-            }
+            camera.zoom = rl.math.clamp(camera.zoom * scaleFactor, 0.125, 64.0);
         }
-        //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(.ray_white);
+        rl.clearBackground(c64.colors[7]);
 
         {
             camera.begin();
             defer camera.end();
 
-            rl.gl.rlPushMatrix();
-            rl.gl.rlTranslatef(0, 25 * 50, 0);
-            rl.gl.rlRotatef(90, 1, 0, 0);
-            rl.drawGrid(100, 50);
-            rl.gl.rlPopMatrix();
-
-            rl.drawCircle(screenWidth / 2, screenHeight / 2, 50, .maroon);
+            for (0..19) |x| {
+                const fooX: i32 = @intCast(x * hex_width);
+                for (0..13) |y| {
+                    var fooY: i32 = @intCast(y * hex_height);
+                    if (@mod(x, 2) != 0) fooY -= halfY;
+                    rl.drawCircle(fooX, fooY, 10, c64.colors[14]);
+                    if (sql.is_hill(x, y)) {
+                        rl.drawCircle(fooX, fooY, halfY + 5, c64.colors[9]);
+                    }
+                }
+            }
         }
-
-        rl.drawText("[1][2] Select mouse zoom mode (Wheel or Move)", 20, 20, 20, .dark_gray);
-        if (zoomMode == 0) {
-            rl.drawText("Mouse right button drag to move, mouse wheel to zoom", 20, 50, 20, .dark_gray);
-        } else {
-            rl.drawText("Mouse right button drag to move, mouse press and move to zoom", 20, 50, 20, .dark_gray);
-        }
-
-        //----------------------------------------------------------------------------------
     }
 }
