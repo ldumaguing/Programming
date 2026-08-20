98d4a0c2323c1f3731ab7a59c6d0e6dae7277766
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-08-20 16:18:56 -0400

    Add files via upload

--- Zig/The Long Road/TheGame/src/main.zig
+++ Zig/The Long Road/TheGame/src/main.zig
@@ -58,23 +58,62 @@ pub fn main() anyerror!void {
     // ==========================================================
     rl.setTargetFPS(12);
 
+    var camera = rl.Camera2D{
+        .target = .{ .x = 0, .y = 0 },
+        .offset = .{ .x = 0, .y = 0 },
+        .zoom = 0.5,
+        .rotation = 0,
+    };
+
     while (!rl.windowShouldClose()) {
+        // Translate based on mouse right click
+        if (rl.isMouseButtonDown(.right)) {
+            var delta = rl.getMouseDelta();
+            delta = rl.math.vector2Scale(delta, -1.0 / camera.zoom);
+            camera.target = rl.math.vector2Add(camera.target, delta);
+        }
+
+        // Zoom based on mouse wheel
+        const wheel = rl.getMouseWheelMove();
+        if (wheel != 0) {
+            const mouseWorldPos = rl.getScreenToWorld2D(rl.getMousePosition(), camera);
+
+            camera.offset = rl.getMousePosition();
+
+            camera.target = mouseWorldPos;
+
+            // Zoom increment
+            var scaleFactor = 1.0 + (0.25 * @abs(wheel));
+            if (wheel < 0) {
+                scaleFactor = 1.0 / scaleFactor;
+            }
+            camera.zoom = rl.math.clamp(camera.zoom * scaleFactor, 0.125, 64.0);
+        }
+
+        // ==============================
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        // rl.clearBackground(.white);
-        rl.drawTexture(mapTile, 0, 0, .white);
-        rl.drawTexture(unit_1, 50, 50, .white);
+        rl.clearBackground(.black);
 
-        // Control frames speed
-        if (rl.isKeyPressed(.right)) {
-            mapTile = try rl.loadTextureFromImage(Assets.items.ptr[Tiles.items.ptr[0].index]);
-            unit_1 = try rl.loadTextureFromImage(Assets.items.ptr[Unit_1f.index]);
-        } else if (rl.isKeyPressed(.left)) {
-            mapTile = try rl.loadTextureFromImage(Assets.items.ptr[Tiles.items.ptr[1].index]);
-            unit_1 = try rl.loadTextureFromImage(Assets.items.ptr[Unit_1b.index]);
+        {
+            camera.begin();
+            defer camera.end();
+
+            // rl.clearBackground(.white);
+            rl.drawTexture(mapTile, 0, 0, .white);
+            rl.drawTexture(unit_1, 50, 50, .white);
+
+            // Control frames speed
+            if (rl.isKeyPressed(.right)) {
+                mapTile = try rl.loadTextureFromImage(Assets.items.ptr[Tiles.items.ptr[0].index]);
+                unit_1 = try rl.loadTextureFromImage(Assets.items.ptr[Unit_1f.index]);
+            } else if (rl.isKeyPressed(.left)) {
+                mapTile = try rl.loadTextureFromImage(Assets.items.ptr[Tiles.items.ptr[1].index]);
+                unit_1 = try rl.loadTextureFromImage(Assets.items.ptr[Unit_1b.index]);
+            }
+
+            rl.drawText("Congrats! You created your first window!", 190, 200, 20, .black);
         }
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .black);
     }
 }
