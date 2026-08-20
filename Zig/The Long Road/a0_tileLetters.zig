89c58a85c7561054a628984a43d4e87a08b69746
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-08-20 12:58:28 -0400

    Add files via upload

--- Zig/The Long Road/a0_tileLetters.zig
+++ Zig/The Long Road/a0_tileLetters.zig
@@ -8,15 +8,35 @@ pub fn main() !void {
     // const tileLetters = "b_,C";
     print("{s}\n", .{tileLetters});
 
+    var GMap: [4][4]i32 = [4][4]i32{
+        [4]i32{ -1, -1, -1, -1 },
+        [4]i32{ -1, -1, -1, -1 },
+        [4]i32{ -1, -1, -1, -1 },
+        [4]i32{ -1, -1, -1, -1 },
+    };
+
     var it_Row = std.mem.splitScalar(u8, tileLetters, '_');
     var locCR: [2]i32 = .{ 0, 0 };
     while (it_Row.next()) |Row| {
         var it_Col = std.mem.splitScalar(u8, Row, ',');
         while (it_Col.next()) |Col| {
-            print(" {d},{d}: {s}\n", .{ locCR[0], locCR[1], Col });
+            if (Col.len < 1) {
+                locCR[0] += 1;
+                continue;
+            }
+            GMap[@intCast(locCR[0])][@intCast(locCR[1])] = @intCast(Col[0]);
             locCR[0] += 1;
         }
         locCR[0] = 0;
         locCR[1] += 1;
+    }
+
+    print("**************************\n", .{});
+    for (0..4) |row| {
+        for (0..4) |col| {
+            print("({d},{d})", .{ col, row });
+            print("{d},", .{GMap[@intCast(col)][@intCast(row)]});
+        }
+        print("\n", .{});
     }
 }
