11d301ae7655b34835894cac719bea58838d0937
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-08-21 11:55:06 -0400

    Update GameMap.zig

--- Zig/The Long Road/TheGame/src/lib/GameMap.zig
+++ Zig/The Long Road/TheGame/src/lib/GameMap.zig
@@ -7,8 +7,6 @@ pub const GameMap = struct {
     GMap: [4][4]i32,
 
     pub fn init(tileLetters: []const u8) GameMap {
-        print("{s}\n", .{tileLetters});
-
         var GMap: [4][4]i32 = [4][4]i32{
             [4]i32{ -1, -1, -1, -1 },
             [4]i32{ -1, -1, -1, -1 },
@@ -39,25 +37,11 @@ pub const GameMap = struct {
 
     // ********************************************************************************************
     pub fn modify_GMap(self: *GameMap, tiles: *std.ArrayList(tile.Tile)) void {
-        print("modifying....................\n", .{});
-
-        //self.GMap[0][0] = get_tile_id(tiles);
-
-        //print("{d}\n", .{tiles.items.len});
-        //for (0..tiles.items.len) |x| {
-        //    print("{d}:{d}, ", .{ tiles.items[x].id, tiles.items[x].rotation });
-        //}
-        //print("\n", .{});
-
         for (0..4) |row| {
             for (0..4) |col| {
-                //        print("({d},{d})", .{ col, row });
-                //        print("{d},", .{self.GMap[@intCast(col)][@intCast(row)]});
                 const X = get_tile_id(tiles, self.GMap[@intCast(col)][@intCast(row)]);
-                //print("> {d}\n", .{X});
                 self.GMap[@intCast(col)][@intCast(row)] = X;
             }
-            //    print("\n", .{});
         }
     }
 
@@ -65,7 +49,6 @@ pub const GameMap = struct {
     fn get_tile_id(tiles: *std.ArrayList(tile.Tile), letter: i32) i32 {
         const ref_A: i32 = 'A';
         const ref_a: i32 = 'a';
-        //print("{d}:{d}\n", .{ ref_A, ref_a });
 
         var rotation: f32 = 0.0;
         var ref = letter - ref_A;
@@ -76,14 +59,12 @@ pub const GameMap = struct {
 
         var tile_id: i32 = -1;
         for (0..tiles.items.len) |x| {
-            // print("{d}:{d}, ", .{ tiles.items[x].id, tiles.items[x].rotation });
             if (ref >= 0) {
-                //print("{d}...{d}:{d} --- {d}:{d}\n", .{ x, ref, rotation, tiles.items[x].id, tiles.items[x].rotation });
                 if ((ref == tiles.items[x].id) and (rotation == tiles.items[x].rotation))
                     tile_id = @intCast(x);
             }
         }
-        //print("{d}\n", .{letter});
+
         return tile_id;
     }
 
