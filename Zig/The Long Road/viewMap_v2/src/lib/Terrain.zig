2b6eeee0bdf89e31955657dfb02b62ba8c24d9b8
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-08-27 12:27:10 -0400

    Add files via upload

--- Zig/The Long Road/viewMap_v2/src/lib/Terrain.zig
+++ Zig/The Long Road/viewMap_v2/src/lib/Terrain.zig
@@ -1,14 +1,28 @@
+const rl = @import("raylib");
+
 pub const Hill = struct {
     x: i32,
     y: i32,
     h: i32, // height
 
     pub fn init(x: i32, y: i32, h: i32) Hill {
-        // ======================================================
         return Hill{
             .x = x,
             .y = y,
             .h = h,
+        };
+    }
+};
+
+pub const WholeHex = struct {
+    x: i32,
+    y: i32,
+    c: rl.Color,
+
+    pub fn init(x: i32, y: i32) WholeHex {
+        return WholeHex{
+            .x = x,
+            .y = y,
         };
     }
 };
