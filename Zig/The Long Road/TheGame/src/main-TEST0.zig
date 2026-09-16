039b3448ee41bc9609958acdc2d8fcc3b2e84788
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-09-16 13:03:29 -0400

    Delete Zig/The Long Road/TheGame/src/main-TEST0.zig

--- Zig/The Long Road/TheGame/src/main-TEST0.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const print = std.debug.print;
-
-const card = @import("lib/Card.zig");
-
-pub fn main() !void {
-    const foo = card.Card.init("Larry was here");
-    print("{s}\n", .{foo.name});
-}
