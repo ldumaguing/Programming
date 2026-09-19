d09806ebec72f073bea34ad607a84da35803034a
Lawrence Dumaguing <os2nlinux@gmail.com>
2026-09-18 21:07:48 -0400

    2026.09b

--- Zig/The Long Road/TheGame/src/lib/Card.zig
+++ Zig/The Long Road/TheGame/src/lib/Card.zig
@@ -44,39 +44,23 @@ pub const PlayerAid = struct {
 
         var turns: i32 = db.get_int_vals_session("turns").@"0";
         turns -= 1;
-        print("------------------------ {d}\n", .{turns});
+        
         const upper_left: [2]i32 = [_]i32{ 17, 333 };
         const grid_dim: [2]i32 = [_]i32{ 978, 781 };
         const rect_dim: [2]i32 = [_]i32{ @divTrunc(grid_dim[0], 5), @divTrunc(grid_dim[1], 4) };
-        print("1 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ rect_dim[0], rect_dim[1] });
+
         const turn_X: i32 = @mod(turns, 5);
         const turn_Y: i32 = @divTrunc(turns, 4);
-        print("2 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ turn_X, turn_Y });
+
         const grid_X: i32 = rect_dim[0] * turn_X;
         const grid_Y: i32 = rect_dim[1] * turn_Y;
-        print("3 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ grid_X, grid_Y });
+
         const loc_X: i32 = grid_X + upper_left[0];
         const loc_Y: i32 = grid_Y + upper_left[1];
-        print("4 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ loc_X, loc_Y });
+
         const card_dim: [2]i32 = [_]i32{ 1504, 1131 };
         const adj_X: i32 = loc_X - card_dim[0] + 15;
-        const adj_Y: i32 = pxY + grid_Y + upper_left[1] + 20;
-
-        //
-        //
-        //const lower_right: [2]i32 = [_]i32{ 973, 780 };
-        //const l_r: [2]i32 = [_]i32{ lower_right[0] - upper_left[0], lower_right[1] - upper_left[1] };
-        //print("1 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ l_r[0], l_r[1] });
-
-        //const rect_X: i32 = @divTrunc(l_r[0], 5);
-        //const rect_Y: i32 = @divTrunc(l_r[1], 4);
-        //print("3 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ rect_X, rect_Y });
-        //const X: i32 = rect_X * turn_X;
-        //const Y: i32 = rect_Y * turn_Y;
-        //print("4 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ X, Y });
-        //const mrk_X: i32 = X + upper_left[0];
-        //const mrk_Y: i32 = Y + upper_left[1];
-        //print("5 >>>>>>>>>>>>>>>>>>>>>>> {d},{d}\n", .{ mrk_X, mrk_Y });
+        const adj_Y: i32 = loc_Y + pxY + 18;
 
         return PlayerAid{
             .pxX = pxX,
@@ -89,6 +73,3 @@ pub const PlayerAid = struct {
         };
     }
 };
-
-// 5x4
-// const new_slice = try std.fmt.allocPrintZ(allocator, "{s}", .{old_slice});
