68bd2387e2166f865ec1c1998982d90d7efe0653
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-09-23 16:08:52 -0400

    Add files via upload

--- Zig/The Long Road/TheGame/src/lib/Asset.zig
+++ Zig/The Long Road/TheGame/src/lib/Asset.zig
@@ -26,7 +26,7 @@ pub const Combatant = struct {
     // imagefile: [64]u8,
     // imagefile_len: i32,
 
-    pub fn init(instanceID: i32, hex_x: i32, hex_y: i32, id: i32, currState: i32, c_str: []const u8) Combatant {
+    pub fn init(instanceID: i32, hex_x: i32, hex_y: i32, id: i32, currState: i32, descrip: []const u8) Combatant {
         return Combatant{
             .instanceID = instanceID,
             .hex_x = hex_x,
@@ -37,7 +37,7 @@ pub const Combatant = struct {
             .currState = currState,
             .imgIndex = 0,
             .is_visible = true,
-            .descrip = c_str,
+            .descrip = descrip,
             // .imagefile = undefined,
             // .imagefile_len = 0,
         };
