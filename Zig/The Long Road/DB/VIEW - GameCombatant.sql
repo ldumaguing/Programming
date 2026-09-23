e69681dfb87114c79d00064a4532aeeec47704cc
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2026-09-23 10:45:09 -0400

    Add files via upload

--- Zig/The Long Road/DB/VIEW - GameCombatant.sql
+++ Zig/The Long Road/DB/VIEW - GameCombatant.sql
@@ -8,26 +8,7 @@ SELECT
    GameCombatant.hex_z,
    GameCombatant.flag1,
    GameCombatant.id,
-   IMG.file AS filename
-FROM
-   IMG, GameCombatant
-WHERE
-   IMG.id = GameCombatant.currState
-;
-
-v_combatant(id,descrip,file0,file1,file2,file3,wreck,faction) 
-
-****************************************************************************
-DROP VIEW IF EXISTS v_gamecombatant;
-CREATE VIEW v_gamecombatant AS
-SELECT
-   GameCombatant.sessionID,
-   GameCombatant.instanceID,
-   GameCombatant.hex_x,
-   GameCombatant.hex_y,
-   GameCombatant.hex_z,
-   GameCombatant.flag1,
-   GameCombatant.id,
+   GameCombatant.currState,
    IMG.file AS filename,
    COMBATANT.descrip,
    COMBATANT.faction
